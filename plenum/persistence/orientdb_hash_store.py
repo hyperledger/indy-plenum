231925a8a4685b564bf1fae6cda522527149773a
Joseph Kiran <joseph.kiran@evernym.us>
2016-04-25 19:38:07 +0530

    Created OrientDbHashStore and tests. One failing test.

--- plenum/persistence/orientdb_hash_store.py
+++ plenum/persistence/orientdb_hash_store.py
@@ -1,43 +1,89 @@
-import pyorient
+from base64 import b64encode, b64decode
 
 from ledger.immutable_store.store import F
 from ledger.immutable_store.stores.hash_store import HashStore
 from plenum.persistence.orientdb_store import OrientDbStore
 
 
-class OrientDbHashStore(HashStore, OrientDbStore):
+class OrientDbHashStore(HashStore):
     """
     Uses OrientDB to store leaf hashes and node hashes
     """
 
-    def __init__(self, user, password, dbName, host="localhost", port=2424,
-                 dbType=pyorient.DB_TYPE_DOCUMENT,
-                 storageType=pyorient.STORAGE_TYPE_MEMORY):
-        super(OrientDbHashStore, self).__init__(user, password, dbName,
-                                                host, port, dbType,
-                                                storageType)
-        self.leafHashClass = "LeafHashes"
-        self.nodeHashClass = "NodeHashes"
-        self.createClasses(self.classesNeeded())
+    def __init__(self, store: OrientDbStore):
+        self.store = store
+        self.leafHashClass = "LeafHashStore"
+        self.nodeHashClass = "NodeHashStore"
+        self.store.createClasses(self.classesNeeded())
+        self.lastSerialNo = 0
 
-    def writeLeaf(self, leaf):
-        # append the leaf to the leaf store
-        pass
+    def writeLeaf(self, leafHash):
+        """append the leaf to the leaf hash store"""
+        clusterId = self._getClusterId(self.leafHashClass)
+        rec = {
+            '@' + self.leafHashClass: {
+                F.serialNo.name: self.lastSerialNo + 1,
+                F.leafHash.name: self._tob64(leafHash)
+            }
+        }
+        self.store.client.record_create(clusterId, rec)
+        self.lastSerialNo += 1
 
     def writeNode(self, node):
-        pass
+        start, height, nodeHash = node
+        serialNo = self.getNodePosition(start, height)
+        clusterId = self._getClusterId(self.nodeHashClass)
+        rec = {
+            '@' + self.nodeHashClass: {
+                F.serialNo.name: serialNo,
+                F.nodeHash.name: self._tob64(nodeHash)
+            }
+        }
+        rec_position = self.store.client.record_create(clusterId, rec)
+        return rec_position
 
-    def readLeaf(self, pos):
-        pass
+    def _tob64(self, hash):
+        return b64encode(hash).decode('utf_8')
 
-    def readNode(self, pos):
-        pass
+    def _fromb64(self, hash):
+        return b64decode(hash.encode('utf_8'))
 
-    def readLeafs(self, startpos, endpos):
-        pass
+    def readLeaf(self, serialNo):
+        return self._readOne(serialNo, self.leafHashClass, F.leafHash.name)
 
-    def readNodes(self, startpos, endpos):
-        pass
+    def readNode(self, serialNo):
+        return self._readOne(serialNo, self.nodeHashClass, F.nodeHash.name)
+
+    def _readOne(self, pos, hashClass, attrib):
+        self._validatePos(pos)
+        resultSet = self.store.client.command(
+            "select * from {} where serialNo={} limit 1".format(
+                hashClass, pos))
+        return self._fromb64(resultSet[0].oRecordData[attrib])
+
+    def readLeafs(self, start, end):
+        return self._readMultiple(start, end, self.leafHashClass,
+                                  F.leafHash.name)
+
+    def readNodes(self, start, end):
+        return self._readMultiple(start, end, self.nodeHashClass,
+                                  F.nodeHash.name)
+
+    def _readMultiple(self, start, end, hashClass, attrib):
+        self._validatePos(start, end)
+        resultSet = self.store.client.command(
+            "select * from {} where serialNo between {} and {}".format(
+                hashClass, start, end))
+        return [self._fromb64(r.oRecordData[attrib]) for r in resultSet]
+
+    @staticmethod
+    def _validatePos(start, end=None):
+        if end:
+            assert start < end, "start index must be less than end index"
+        if start < 1:
+            raise IndexError(
+                "serialNo starts from 1, index requested: {}".
+                    format(start))
 
     def createLeafHashClass(self):
         self._createHashClass(self.leafHashClass, {
@@ -47,17 +93,20 @@ class OrientDbHashStore(HashStore, OrientDbStore):
 
     def createNodeHashClass(self):
         self._createHashClass(self.nodeHashClass, {
-            F.rowNo.name: "integer",
+            F.serialNo.name: "integer",
             F.nodeHash.name: "string"
-        }, F.rowNo.name)
+        }, F.serialNo.name)
 
     def _createHashClass(self, className, attributes, index):
-        self.createClass(className)
-        self.createClassProperties(className, attributes)
-        self.createIndexOnClass(className, index)
+        self.store.createClass(className)
+        self.store.createClassProperties(className, attributes)
+        self.store.createIndexOnClass(className, index, "unique")
 
     def classesNeeded(self):
         return [(self.leafHashClass, self.createLeafHashClass),
                 (self.nodeHashClass, self.createNodeHashClass)]
 
-
+    # TODO Store clusterId in class state
+    def _getClusterId(self, clusterName: str):
+        return self.store.client.get_class_position(
+            bytes(clusterName, 'utf_8'))
--- plenum/test/storage/test_orientdb_hash_store.py
+++ plenum/test/storage/test_orientdb_hash_store.py
@@ -1,6 +1,7 @@
 import pyorient
 import pytest
 
+from plenum.persistence.orientdb_store import OrientDbStore
 from plenum.persistence.orientdb_hash_store import OrientDbHashStore
 from ledger.immutable_store.test.test_file_hash_store import nodesLeaves
 
@@ -8,7 +9,13 @@ from ledger.immutable_store.test.test_file_hash_store import nodesLeaves
 @pytest.fixture(scope="module")
 def db():
     # TODO Read setup info from a config file
-    return OrientDbHashStore(user="root", password="password", dbName="test")
+    db = OrientDbHashStore(
+        OrientDbStore(user="root", password="password", dbName="test"))
+    for cls in [db.nodeHashClass, db.leafHashClass]:
+        if db.store.classExists(cls):
+            db.store.client.command("Truncate class {}".format(cls))
+    return db
+
 
 """
 Tests:
@@ -20,19 +27,31 @@ Tests:
 """
 
 
-def testOrientDbSetup():
-    db = OrientDbHashStore(user="root", password="password", dbName="test")
-    assert db.client.db_exists("test", pyorient.STORAGE_TYPE_MEMORY)
-    assert db.classExists(db.leafHashClass)
-    assert db.classExists(db.nodeHashClass)
+def testOrientDbSetup(db):
+    store = db.store
+    # This seems to be a bug in pyorient. Reported. Bug #186
+    # assert store.client.db_exists("test", pyorient.STORAGE_TYPE_MEMORY)
+    assert store.classExists(db.leafHashClass)
+    assert store.classExists(db.nodeHashClass)
 
 
-def testOneByOne(db: OrientDbHashStore, nodesLeaves):
+def testUniqueConstraint(db: OrientDbHashStore, nodesLeaves):
+    nodes, leafs = nodesLeaves
+    with pytest.raises(Exception):
+        db.writeLeaf(leafs[0])
+        db.writeLeaf(leafs[0])
+
+
+def testIndexFrom1(db: OrientDbHashStore):
+    with pytest.raises(IndexError):
+        db.readLeaf(0)
+
+
+def testReadWrite(db: OrientDbHashStore, nodesLeaves):
     nodes, leafs = nodesLeaves
     for node in nodes:
         db.writeNode(node)
     for leaf in leafs:
         db.writeLeaf(leaf)
-
-
-
+    assert db.readLeaf(1)
+    # TODO Read multiple and read one at a time give the same result.
