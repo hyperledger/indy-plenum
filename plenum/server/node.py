6820ceae1d3be4e4e6895e74144dfab886c49047
Lovesh <lovesh@evernym.us>
2016-07-14 23:44:40 +0530

    Revert "adding code for credit"
    This reverts commit e2bc28bb8c9b1667a83fa77a877f62334d5ad801.

--- plenum/server/node.py
+++ plenum/server/node.py
@@ -34,9 +34,6 @@ from plenum.common.raet import isLocalKeepSetup
 from plenum.common.stacked import NodeStack, ClientStack
 from plenum.common.startable import Status, Mode
 from plenum.common.txn import TXN_TYPE, TXN_ID, TXN_TIME, POOL_TXN_TYPES
-from plenum.common.txn import TXN_TYPE, TXN_ID, TXN_TIME, POOL_TXN_TYPES, ORIGIN, \
-    CREDIT, AMOUNT, DATA, GET_ALL_TXNS, GET_BAL, TARGET_NYM, SUCCESS, BALANCE, \
-    ALL_TXNS
 from plenum.common.types import Request, Propagate, \
     Reply, Nomination, OP_FIELD_NAME, TaggedTuples, Primary, \
     Reelection, PrePrepare, Prepare, Commit, \
@@ -245,12 +242,6 @@ class Node(HasActionQueue, Motor,
         self.addReplicas()
 
         self.msgsForFutureReplicas = {}
-
-        # Balances of all client
-        self.balances = {}  # type: Dict[str, int]
-
-        # Txns of all clients, each txn is a tuple like (from, to, amount)
-        self.txns = []  # type: List[Tuple]
 
     def __repr__(self):
         return self.name
@@ -1491,29 +1482,6 @@ class Node(HasActionQueue, Motor,
 
         for processor in self.reqProcessors:
             result.update(processor.process(req))
-
-        if req.operation.get(TXN_TYPE) in (CREDIT, GET_BAL, GET_ALL_TXNS):
-            frm = req.identifier
-            if frm not in self.balances:
-                self.balances[frm] = 1000
-            if req.operation.get(TXN_TYPE) == CREDIT:
-                to = req.operation[TARGET_NYM]
-                if to not in self.balances:
-                    self.balances[to] = 1000
-                amount = req.operation[DATA][AMOUNT]
-                if amount > self.balances[frm]:
-                    result[SUCCESS] = False
-                else:
-                    result[SUCCESS] = True
-                    self.balances[to] += amount
-                    self.balances[frm] -= amount
-                    self.txns.append((frm, to, amount))
-            elif req.operation.get(TXN_TYPE) == GET_BAL:
-                result[SUCCESS] = True
-                result[BALANCE] = self.balances.get(frm, 0)
-            elif req.operation.get(TXN_TYPE) == GET_ALL_TXNS:
-                result[SUCCESS] = True
-                result[ALL_TXNS] = [txn for txn in self.txns if frm in txn]
 
         return Reply(result)
 
