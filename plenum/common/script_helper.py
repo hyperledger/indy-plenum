e4b0960278433f6e458ba8454f5a8f42caf6d8c8
Lovesh <lovesh@evernym.us>
2016-07-30 19:30:06 +0530

    removing the need of extra seed from some scripts

--- plenum/common/script_helper.py
+++ plenum/common/script_helper.py
@@ -23,6 +23,7 @@ def isHexKey(key):
         print(ex)
         exit()
 
+
 def buildKeepDirIfNotExists(baseDir):
     keepDir = os.path.expanduser(baseDir)
     if not os.path.exists(keepDir):
@@ -30,23 +31,25 @@ def buildKeepDirIfNotExists(baseDir):
 
 
 def isNodeType(baseDir, name):
-    filepath = os.path.join(os.path.expanduser(baseDir), name + CLIENT_STACK_SUFFIX)
+    filepath = os.path.join(os.path.expanduser(baseDir),
+                            name + CLIENT_STACK_SUFFIX)
     if os.path.exists(filepath):
         return True
     else:
         return False
 
 
-def getLedger(baseDir, dbName, storeHash=True, isLineNoKey: bool=False):
+def getLedger(baseDir, dbName, storeHash=True, isLineNoKey: bool = False):
     return TextFileStore(
-        dbDir = baseDir,
-        dbName = dbName,
-        storeContentHash = storeHash,
-        isLineNoKey = isLineNoKey)
+        dbDir=baseDir,
+        dbName=dbName,
+        storeContentHash=storeHash,
+        isLineNoKey=isLineNoKey)
 
 
 def storeToFile(baseDir, dbName, value, key, storeHash=True, isLineNoKey=False):
-    ledger = getLedger(baseDir, dbName, storeHash=storeHash, isLineNoKey=isLineNoKey)
+    ledger = getLedger(baseDir, dbName, storeHash=storeHash,
+                       isLineNoKey=isLineNoKey)
     if key is None:
         ledger.put(value)
     else:
@@ -54,14 +57,19 @@ def storeToFile(baseDir, dbName, value, key, storeHash=True, isLineNoKey=False):
 
 
 def getNodeInfo(baseDir, nodeName):
-    ledger = getLedger(baseDir, NodeInfoFile, storeHash=False, isLineNoKey=False)
+    ledger = getLedger(baseDir, NodeInfoFile, storeHash=False,
+                       isLineNoKey=False)
     rec = ledger.get(nodeName)
     return json.loads(rec)
 
 
-def storeNodeInfo(baseDir, nodeName, steward, nodeip, nodeport, clientip, clientport):
+def storeNodeInfo(baseDir, nodeName, steward, nodeip, nodeport, clientip,
+                  clientport):
     data = {}
-    vnodeip, vnodeport, vclientip, vclientport = getAddGenesisHAs(nodeip, nodeport, clientip, clientport)
+    vnodeip, vnodeport, vclientip, vclientport = getAddGenesisHAs(nodeip,
+                                                                  nodeport,
+                                                                  clientip,
+                                                                  clientport)
     nodeAddr = vnodeip + ":" + str(vnodeport)
     clientAddr = vclientip + ":" + str(vclientport)
     data['steward'] = steward
@@ -69,27 +77,33 @@ def storeNodeInfo(baseDir, nodeName, steward, nodeip, nodeport, clientip, client
     data['clientAddr'] = clientAddr
     newJsonData = json.dumps(data)
 
-    ledger = getLedger(baseDir, NodeInfoFile, storeHash=False, isLineNoKey=False)
+    ledger = getLedger(baseDir, NodeInfoFile, storeHash=False,
+                       isLineNoKey=False)
     storedJsonData = ledger.get(nodeName)
     if not storedJsonData:
-        storeToFile(baseDir, NodeInfoFile, newJsonData, nodeName, storeHash=False, isLineNoKey=False)
+        storeToFile(baseDir, NodeInfoFile, newJsonData, nodeName,
+                    storeHash=False, isLineNoKey=False)
     elif not storedJsonData == newJsonData:
         newRec = []
-        for key, jsonValue in ledger.iterator(includeKey=True, includeValue=True):
+        for key, jsonValue in ledger.iterator(includeKey=True,
+                                              includeValue=True):
             if key != nodeName:
                 newRec.append((key, jsonValue))
         newRec.append((nodeName, newJsonData))
         ledger.reset()
         for key, value in newRec:
-            storeToFile(baseDir, NodeInfoFile, value, key, storeHash=False, isLineNoKey=False)
+            storeToFile(baseDir, NodeInfoFile, value, key, storeHash=False,
+                        isLineNoKey=False)
 
 
 def storeExportedTxns(baseDir, txn):
-    storeToFile(baseDir, ExportedTxnFile, txn, None, storeHash=False, isLineNoKey=True)
+    storeToFile(baseDir, ExportedTxnFile, txn, None, storeHash=False,
+                isLineNoKey=True)
 
 
 def storeGenTxns(baseDir, txn):
-    storeToFile(baseDir, GenTxnFile, txn, None, storeHash=False, isLineNoKey=True)
+    storeToFile(baseDir, GenTxnFile, txn, None, storeHash=False,
+                isLineNoKey=True)
 
 
 def initKeep(baseDir, name, sigseed, override=False):
@@ -111,26 +125,37 @@ def getAddGenesisHAs(nodeip, nodeport, clientip, clientport):
     return vnodeip, vnodeport, vclientip, vclientport
 
 
-def getAddNewGenNodeCommand(name, verkey, pubkey, stewardkey, nodeip, nodeport, clientip, clientport):
-    vnodeip, vnodeport, vclientip, vclientport = getAddGenesisHAs(nodeip, nodeport, clientip, clientport)
+def getAddNewGenNodeCommand(name, verkey, stewardkey, nodeip, nodeport,
+                            clientip, clientport):
+    vnodeip, vnodeport, vclientip, vclientport = getAddGenesisHAs(nodeip,
+                                                                  nodeport,
+                                                                  clientip,
+                                                                  clientport)
     nodeAddr = vnodeip + ":" + vnodeport
     clientAddr = vclientip + ":" + vclientport
 
-    return 'add genesis transaction NEW_NODE with data {"' + name + '": {"verkey": ' + verkey + ',"pubkey": "' + pubkey + '",' \
-        '"node_address": "' + nodeAddr + '", "client_address": "' + clientAddr + '"},' \
-        '"by": "' + stewardkey + '"}'
+    return 'add genesis transaction NEW_NODE with data {"' + name + '": {"verkey": ' + verkey + \
+           '"node_address": "' + nodeAddr + '", "client_address": "' + clientAddr + '"},' \
+                                                                                    '"by": "' + stewardkey + '"}'
 
 
-def getOldAddNewGenNodeCommand(name, verkey, pubkey, stewardverkey, nodeip, nodeport, clientip, clientport):
-    vnodeip, vnodeport, vclientip, vclientport = getAddGenesisHAs(nodeip, nodeport, clientip, clientport)
+def getOldAddNewGenNodeCommand(name, verkey, stewardverkey, nodeip, nodeport,
+                               clientip, clientport):
+    vnodeip, vnodeport, vclientip, vclientport = getAddGenesisHAs(nodeip,
+                                                                  nodeport,
+                                                                  clientip,
+                                                                  clientport)
     return 'add genesis transaction NEW_NODE for ' + verkey + ' by ' + stewardverkey + ' with data {"node_ip": "' + \
-    vnodeip + '", "node_port": ' + vnodeport + ', "client_ip": "' + vclientip + '", "client_port": ' + \
-    vclientport + ', "pubkey": "' + pubkey + '", "alias": "' + name + '"}'
+           vnodeip + '", "node_port": ' + vnodeport + ', "client_ip": "' + vclientip + '", "client_port": ' + \
+           vclientport + '", "alias": "' + name + '"}'
 
 
-def generateNodeGenesisTxn(baseDir, displayTxn, name, verkey, pubkey, stewardverkey, nodeip, nodeport, clientip, clientport):
-    storeNodeInfo(baseDir, name, stewardverkey, nodeip, nodeport, clientip, clientport)
-    txn = getOldAddNewGenNodeCommand(name, verkey, pubkey, stewardverkey, nodeip, nodeport, clientip, clientport)
+def generateNodeGenesisTxn(baseDir, displayTxn, name, verkey, stewardverkey,
+                           nodeip, nodeport, clientip, clientport):
+    storeNodeInfo(baseDir, name, stewardverkey, nodeip, nodeport, clientip,
+                  clientport)
+    txn = getOldAddNewGenNodeCommand(name, verkey, stewardverkey, nodeip,
+                                     nodeport, clientip, clientport)
     storeGenTxns(baseDir, txn)
     printGenTxn(txn, displayTxn)
 
@@ -139,12 +164,12 @@ def getAddNewGenStewardCommand(name, verkey):
     return 'add genesis transaction NEW_STEWARD with data {"' + name + '": {"verkey": "' + verkey + '"}}'
 
 
-def getOldAddNewGenStewardCommand(name, verkey, pubkey):
-    return 'add genesis transaction NEW_STEWARD for ' + verkey + ' with data {"alias": "' + name + '", "pubkey": "' + pubkey + '"}'
+def getOldAddNewGenStewardCommand(name, verkey):
+    return 'add genesis transaction NEW_STEWARD for ' + verkey + ' with data {"alias": "' + name + '"}'
 
 
-def generateStewardGenesisTxn(baseDir, displayTxn, name, verkey, pubkey):
-    txn = getOldAddNewGenStewardCommand(name, verkey, pubkey)
+def generateStewardGenesisTxn(baseDir, displayTxn, name, verkey):
+    txn = getOldAddNewGenStewardCommand(name, verkey)
     storeGenTxns(baseDir, txn)
     printGenTxn(txn, displayTxn)
 
@@ -165,12 +190,11 @@ def getPubKeyFromName(baseDir, roleName):
 def exportNodeGenTxn(baseDir, displayTxn, name):
     nodeInfo = getNodeInfo(baseDir, name)
     nodeVerKey = getVerKeyFromName(baseDir, name)
-    nodePubKey = getPubKeyFromName(baseDir, name)
     stewardKey = nodeInfo.get('steward')
     nodeAddr = nodeInfo.get('nodeAddr')
     clientAddr = nodeInfo.get('clientAddr')
 
-    txn = 'add genesis transaction NEW_NODE with data {"' + name + '": {"verkey": "' + nodeVerKey + '", "pubkey": "' + nodePubKey + \
+    txn = 'add genesis transaction NEW_NODE with data {"' + name + '": {"verkey": "' + nodeVerKey + \
           '", "node_address": "' + nodeAddr + \
           '", "client_address": "' + clientAddr + '"}, "by":"' + stewardKey + '"}'
     storeExportedTxns(baseDir, txn)
@@ -179,6 +203,6 @@ def exportNodeGenTxn(baseDir, displayTxn, name):
 
 def exportStewardGenTxn(baseDir, displayTxn, name):
     verkey = getLocalVerKey(name, baseDir)
-    txn = 'add genesis transaction NEW_STEWARD with data  {"' +name + '": {"verkey": "' + verkey + '"}}'
+    txn = 'add genesis transaction NEW_STEWARD with data  {"' + name + '": {"verkey": "' + verkey + '"}}'
     storeExportedTxns(baseDir, txn)
     printGenTxn(txn, displayTxn)
