6f752527a547fa274cb28c5f7f8df9da3c8a17bf
Lovesh <lovesh@evernym.us>
2016-09-13 13:54:31 +0530

    adding scripts to setup

--- setup.py
+++ setup.py
@@ -54,8 +54,8 @@ setup(
     packages=find_packages(exclude=['test', 'test.*', 'docs', 'docs*']) + [
         'data', ],
     package_data={
-        '':       ['*.txt',  '*.md', '*.rst', '*.json', '*.conf', '*.html',
-                   '*.css', '*.ico', '*.png', 'LICENSE', 'LEGAL', 'plenum']},
+        '': ['*.txt', '*.md', '*.rst', '*.json', '*.conf', '*.html',
+             '*.css', '*.ico', '*.png', 'LICENSE', 'LEGAL', 'plenum']},
     include_package_data=True,
     data_files=[(
         (BASE_DIR, ['data/pool_transactions', ])
@@ -69,15 +69,19 @@ setup(
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'pytest-xdist'],
     scripts=['scripts/plenum', 'scripts/init_plenum_raet_keep',
-             'scripts/start_plenum_node', 'scripts/generate_plenum_pool_transactions',
-             'scripts/gen_steward_key', 'scripts/gen_node', 'scripts/export-gen-txns']
+             'scripts/start_plenum_node',
+             'scripts/generate_plenum_pool_transactions',
+             'scripts/gen_steward_key', 'scripts/gen_node',
+             'scripts/export-gen-txns', 'scripts/get_keys']
 )
 
 if not os.path.exists(CONFIG_FILE):
     with open(CONFIG_FILE, 'w') as f:
-        msg = "# Here you can create config entries according to your needs.\n " \
+        msg = "# Here you can create config entries according to your " \
+              "needs.\n " \
               "# For help, refer config.py in the sovrin package.\n " \
-              "# Any entry you add here would override that from config example\n"
+              "# Any entry you add here would override that from config " \
+              "example\n"
         f.write(msg)
 
 DATA_DIR = os.path.dirname(data.__file__)
