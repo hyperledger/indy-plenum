94c12876c1a55a52c2b8363ade89558a0e1e87fb
Neelkanth Sachdeva <neelkanthsachdeva@gmail.com>
2016-09-20 17:44:00 +0530

    [#130086787] Updated the help command

--- plenum/cli/cli.py
+++ plenum/cli/cli.py
@@ -628,20 +628,42 @@ class Cli:
         else:
             self.print(record.msg, Token)
 
+#     def printHelp(self):
+#         self.print("""{}-CLI, a simple command-line interface for a
+#         {} sandbox.
+# Commands:
+#     help - Shows this help message
+#     help <command> - Shows the help message of <command>
+#     new - creates one or more new nodes or clients
+#     keyshare - manually starts key sharing of a node
+#     status - Shows general status of the sandbox
+#     status <node_name>|<client_name> - Shows specific status
+#     list - Shows the list of commands you can run
+#     license - Show the license
+#     exit - exit the command-line interface ('quit' also works)""".
+#                    format(self.properName, self.fullName))
+
     def printHelp(self):
-        self.print("""{}-CLI, a simple command-line interface for a
-        {} sandbox.
-Commands:
-    help - Shows this help message
-    help <command> - Shows the help message of <command>
-    new - creates one or more new nodes or clients
-    keyshare - manually starts key sharing of a node
-    status - Shows general status of the sandbox
-    status <node_name>|<client_name> - Shows specific status
-    list - Shows the list of commands you can run
-    license - Show the license
-    exit - exit the command-line interface ('quit' also works)""".
-                   format(self.properName, self.fullName))
+            self.print("""{}-CLI, a simple command-line interface for a
+            {} sandbox.
+    Commands:
+        help - Shows this help message
+        help <command> - Shows the help message of <command>
+        new - creates one or more new nodes or clients
+        keyshare - manually starts key sharing of a node
+        status - Shows general status of the sandbox
+        status <node_name>|<client_name> - Shows specific status
+        list - Shows the list of commands you can run
+        license - Show the license
+        exit - exit the command-line interface ('quit' also works)
+        prompt <principal name> - Changes the prompt to <principal name>
+        principals (a person like Alice, an organization like Faber College, or an IoT-style thing)
+        load <invitation filename> - Creates the link, generates Identifier and signing keys
+        show <invitation filename> - Shows the info about the link invitation
+        show link <name> - Shows link info in case of one matching link, otherwise shows all the matching link <names>
+        connect <test> |<live> - Let's you connect to the respective environment
+        sync <link name> - Synchronizes the link between the endpoints""".
+                       format(self.properName, self.fullName))
 
     def printCmdHelper(self, command=None):
         self.functionMappings[command]()
