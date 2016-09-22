a418ed7388e90a8c2b515865d4d30eb809962e11
Neelkanth Sachdeva <neelkanthsachdeva@gmail.com>
2016-09-22 09:30:56 +0530

    [#130086787] Updated the help command-minor refactoring after review

--- plenum/test/cli/test_help_command.py
+++ plenum/test/cli/test_help_command.py
@@ -3,25 +3,24 @@ def testDefaultHelp(cli):
     Testing `help` command
     """
     cli.enterCmd("help")
-    defaultHelpMsg = """Plenum-CLI, a simple command-line interface for a
-            Plenum protocol sandbox.
-    Commands:
-        help - Shows this help message
-        help <command> - Shows the help message of <command>
-        new - creates one or more new nodes or clients
-        keyshare - manually starts key sharing of a node
-        status - Shows general status of the sandbox
-        status <node_name>|<client_name> - Shows specific status
-        list - Shows the list of commands you can run
-        license - Show the license
-        exit - exit the command-line interface ('quit' also works)
-        prompt <principal name> - Changes the prompt to <principal name>
-        principals (a person like Alice, an organization like Faber College, or an IoT-style thing)
-        load <invitation filename> - Creates the link, generates Identifier and signing keys
-        show <invitation filename> - Shows the info about the link invitation
-        show link <name> - Shows link info in case of one matching link, otherwise shows all the matching link <names>
-        connect <test> |<live> - Let's you connect to the respective environment
-        sync <link name> - Synchronizes the link between the endpoints"""
+    defaultHelpMsg = """Plenum-CLI, a simple command-line interface for a Plenum protocol sandbox.
+        Commands:
+            help - Shows this help message
+            help <command> - Shows the help message of <command>
+            new - creates one or more new nodes or clients
+            keyshare - manually starts key sharing of a node
+            status - Shows general status of the sandbox
+            status <node_name>|<client_name> - Shows specific status
+            list - Shows the list of commands you can run
+            license - Show the license
+            exit - exit the command-line interface ('quit' also works)
+            prompt <principal name> - Changes the prompt to <principal name>
+            principals (a person like Alice, an organization like Faber College, or an IoT-style thing)
+            load <invitation filename> - Creates the link, generates Identifier and signing keys
+            show <invitation filename> - Shows the info about the link invitation
+            show link <name> - Shows link info in case of one matching link, otherwise shows all the matching link <names>
+            connect <test> |<live> - Let's you connect to the respective environment
+            sync <link name> - Synchronizes the link between the endpoints"""
 
     msg = cli.lastPrintArgs['msg']
     assert msg == defaultHelpMsg
