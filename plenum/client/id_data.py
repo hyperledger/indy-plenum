25926089ae18d9ff7a211cf85eab83131ab352b7
Victor Muzychenko <mzkvct@gmail.com>
2016-09-29 13:40:50 +0300

    create StoredIdData - child of IdData that works with provided storage

--- plenum/client/id_data.py
+++ plenum/client/id_data.py
@@ -1,14 +1,15 @@
 from binascii import unhexlify
 
 from plenum.client.signer import Signer, SimpleSigner
-
+from plenum.persistence.client_req_rep_store import ClientReqRepStore
 
 class IdData:
+
     def __init__(self,
                  signer: Signer=None,
                  lastReqId: int=0):
         self.signer = signer
-        self.lastReqId = lastReqId
+        self._lastReqId = lastReqId
 
     def __getstate__(self):
         return {
@@ -18,4 +19,20 @@ class IdData:
 
     def __setstate__(self, obj):
         self.signer = SimpleSigner(seed=unhexlify(obj['key'].encode()))
-        self.lastReqId = obj['lastReqId']
+        self._lastReqId = obj['lastReqId']
+
+    @property
+    def lastReqId(self):
+        return self._lastReqId
+
+class StoredIdData(IdData):
+
+    def __init__(self,
+                 reqRepStore: ClientReqRepStore,
+                 signer: Signer=None):
+        id = reqRepStore.lastReqId # type: int
+        super().__init__(signer, id)
+
+    @property
+    def lastReqId(self):
+        return self._lastReqId
