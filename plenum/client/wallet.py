0268087774d4b3e3fc406a7d32f11b93505bbfe5
Lovesh <lovesh@evernym.us>
2016-10-04 08:24:56 +0530

    changing owner and group of base directory to logged in user

--- plenum/client/wallet.py
+++ plenum/client/wallet.py
@@ -100,7 +100,8 @@ class Wallet:
         req.identifier = idr
         idData = self._getIdData(idr)
         if not idData:
-            raise RuntimeError("Identifier not present in wallet, cannot sign")
+            raise RuntimeError("Identifier {} not present in wallet, "
+                               "cannot sign".format(idr))
         req.reqId = idData.lastReqId + 1
         req.signature = self.signMsg(msg=req.getSigningState(),
                                      identifier=idr,
