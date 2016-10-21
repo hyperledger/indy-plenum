6aaf26c40333102afc18ebb43a8a193a72e11e8f
Rajesh Kalaria <rajesh.kalaria@gmail.com>
2016-10-21 16:15:18 +0530

    added method to get size of object

--- plenum/common/util.py
+++ plenum/common/util.py
@@ -20,6 +20,7 @@ from typing import TypeVar, Iterable, Mapping, Set, Sequence, Any, Dict, \
     Tuple, Union, List, NamedTuple
 
 import libnacl.secret
+import sys
 from ledger.util import F
 from libnacl import crypto_hash_sha256
 from six import iteritems, string_types
@@ -561,3 +562,24 @@ def getTimeBasedId():
 
 def randomSeed(size=32):
     return ''.join(random.choice(string.hexdigits) for _ in range(size)).encode()
+
+
+def get_size(obj, seen=None):
+    """Recursively finds size of objects"""
+    size = sys.getsizeof(obj)
+    if seen is None:
+        seen = set()
+    obj_id = id(obj)
+    if obj_id in seen:
+        return 0
+    # Important mark as seen *before* entering recursion to gracefully handle
+    # self-referential objects
+    seen.add(obj_id)
+    if isinstance(obj, dict):
+        size += sum([get_size(v, seen) for v in obj.values()])
+        size += sum([get_size(k, seen) for k in obj.keys()])
+    elif hasattr(obj, '__dict__'):
+        size += get_size(obj.__dict__, seen)
+    elif hasattr(obj, '__iter__') and not isinstance(obj, (str, bytes, bytearray)):
+        size += sum([get_size(i, seen) for i in obj])
+    return size
