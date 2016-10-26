93bf2bdc3c028712fc096f834e25f2609818a9e0
Rajesh Kalaria <rajesh.kalaria@gmail.com>
2016-10-26 14:53:25 +0530

    initial changes for change ha script

--- plenum/cli/constants.py
+++ plenum/cli/constants.py
@@ -1,6 +1,18 @@
 import re
 import os
+from typing import NamedTuple
 
+Environment = NamedTuple("Environment", [
+    ("poolLedger", str),
+    ("domainLedger", str)
+])
+
+ENVS = {
+    "test": Environment("pool_transactions_sandbox",
+                        "transactions_sandbox"),
+    "live": Environment("pool_transactions_live",
+                        "transactions_live")
+}
 
 def relist(seq):
     return '(' + '|'.join(seq) + ')'
--- plenum/common/script_helper.py
+++ plenum/common/script_helper.py
@@ -1,10 +1,23 @@
+import argparse
 import os
+from ast import literal_eval
 
 from jsonpickle import json
 
 from ledger.stores.text_file_store import TextFileStore
+from plenum.cli.constants import ENVS
+from plenum.client.client import Client
+from plenum.client.wallet import Wallet
+from plenum.common.looper import Looper
+from plenum.common.port_dispenser import genHa
 from plenum.common.raet import initLocalKeep, getLocalVerKey, getLocalPubKey
-from plenum.common.types import CLIENT_STACK_SUFFIX
+from plenum.common.signer_simple import SimpleSigner
+from plenum.common.txn import TXN_TYPE, CHANGE_HA, TARGET_NYM, DATA, NODE_IP, \
+    NODE_PORT, CLIENT_IP, CLIENT_PORT, ALIAS, NEW_NODE
+from plenum.common.types import CLIENT_STACK_SUFFIX, HA
+from plenum.common.util import getConfig, getMaxFailures
+from plenum.test.eventually import eventually
+from plenum.test.helper import checkSufficientRepliesRecvd
 
 NodeInfoFile = "node-info"
 GenTxnFile = "genesis_txn"
@@ -206,3 +219,96 @@ def exportStewardGenTxn(baseDir, displayTxn, name):
                                                                        '"verkey": "' + verkey + '"}} role=STEWARD'
     storeExportedTxns(baseDir, txn)
     printGenTxn(txn, displayTxn)
+
+
+def submitNodeIpChange(client, stewardWallet, name: str, nym: str,
+                       nodeStackHa: HA, clientStackHa: HA):
+    (nodeIp, nodePort), (clientIp, clientPort) = nodeStackHa, clientStackHa
+    txn = {
+        TXN_TYPE: CHANGE_HA,
+        TARGET_NYM: nym,
+        DATA: {
+            NODE_IP: nodeIp,
+            NODE_PORT: int(nodePort),
+            CLIENT_IP: clientIp,
+            CLIENT_PORT: int(clientPort),
+            ALIAS: name
+        }
+    }
+    signedOp = stewardWallet.signOp(txn, stewardWallet.defaultId)
+    req, = client.submitReqs(signedOp)
+    return req
+
+
+def changeHA(basedirpath):
+    parser = argparse.ArgumentParser(
+        description="Change Ip and/or Port of existing node")
+
+    parser.add_argument('--env', required=True, type=str,
+                        help='Environment, options are: test, live')
+
+    parser.add_argument('--name', required=True, type=str,
+                        help='node name')
+
+    parser.add_argument('--nodeseed', required=True, type=str,
+                        help="node's seed")
+
+    parser.add_argument('--stewardseed', required=True, type=str,
+                        help="stewards's seed")
+
+    parser.add_argument('--nodestacknewha', required=True, type=str,
+                        help="node stack's new HA, format=> ip:port")
+
+    parser.add_argument('--clientstacknewha', required=False, type=str,
+                        help="client stack's new HA, format=> ip:port")
+
+    args = parser.parse_args()
+
+    env = args.env
+    nodeName = args.name
+    nodeStackNewHA = args.nodestacknewha
+    clientStackNewHA = args.clientstacknewha
+    nodesNewIp, nodesNewPort = tuple(nodeStackNewHA.split(':'))
+    nodeStackNewHA = (nodesNewIp, nodesNewPort)
+    if not clientStackNewHA:
+        clientStackNewHA = (nodesNewIp, str(int(nodesNewPort)+1))
+
+    nodesSeed = bytes(args.nodeseed, 'utf-8')
+    nodeSigner = SimpleSigner(seed=nodesSeed)
+
+    stewardsSeed = bytes(args.stewardseed, 'utf-8')
+    stewardSigner = SimpleSigner(seed=stewardsSeed)
+
+    stewardWallet = Wallet("StewardWallet")
+    stewardWallet.addIdentifier(signer=stewardSigner)
+
+    client_address = ('0.0.0.0', 9761)
+
+    poolTransactionsFile = ENVS[env].poolLedger
+    poolTransFile = open(os.path.join(basedirpath, poolTransactionsFile), 'r')
+    nodeReg = {}
+    for line in poolTransFile:
+        txn = json.loads(line)
+        data = txn[DATA]
+        if txn[TXN_TYPE] == NEW_NODE:
+            nodeReg[data[ALIAS]+CLIENT_STACK_SUFFIX] = HA(data[CLIENT_IP],
+                                                             data[CLIENT_PORT])
+
+    client = Client("steward1",
+                    ha=client_address,
+                    nodeReg=nodeReg,
+                    basedirpath=basedirpath)
+
+    looper = Looper(debug=True)
+
+    looper.add(client)
+
+    looper.runFor(3)
+
+    req = submitNodeIpChange(client, stewardWallet, nodeName, nodeSigner.verkey,
+                       nodeStackNewHA, clientStackNewHA)
+
+    f = getMaxFailures(len(nodeReg))
+    looper.run(eventually(checkSufficientRepliesRecvd, client.inBox, req.reqId
+                          , f, retryWait=1, timeout=15))
+    looper.runFor(15)
