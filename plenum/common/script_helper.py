d68c7ee21a4ac339c84a8d61169719788919f9a5
Alexander Shekhovtsov <alexander.shekhovcov@dsr-company.com>
2016-11-01 13:26:47 +0300

    update change_ha command

--- plenum/common/script_helper.py
+++ plenum/common/script_helper.py
@@ -1,23 +1,17 @@
-import argparse
 import os
-from ast import literal_eval
+import random
 
 from jsonpickle import json
-
 from ledger.stores.text_file_store import TextFileStore
-from plenum.cli.constants import ENVS
+
 from plenum.client.client import Client
 from plenum.client.wallet import Wallet
-from plenum.common.looper import Looper
-from plenum.common.port_dispenser import genHa
 from plenum.common.raet import initLocalKeep, getLocalVerKey, getLocalPubKey
 from plenum.common.signer_simple import SimpleSigner
 from plenum.common.txn import TXN_TYPE, CHANGE_HA, TARGET_NYM, DATA, NODE_IP, \
-    NODE_PORT, CLIENT_IP, CLIENT_PORT, ALIAS, NEW_NODE
+    NODE_PORT, CLIENT_IP, CLIENT_PORT, ALIAS
 from plenum.common.types import CLIENT_STACK_SUFFIX, HA
-from plenum.common.util import getConfig, getMaxFailures
 from plenum.test.eventually import eventually
-from plenum.test.helper import checkSufficientRepliesRecvd
 
 NodeInfoFile = "node-info"
 GenTxnFile = "genesis_txn"
@@ -240,64 +234,32 @@ def submitNodeIpChange(client, stewardWallet, name: str, nym: str,
     return req
 
 
-def changeHA(basedirpath):
-    parser = argparse.ArgumentParser(
-        description="Change Ip and/or Port of existing node")
-
-    parser.add_argument('--env', required=True, type=str,
-                        help='Environment, options are: test, live')
-
-    parser.add_argument('--name', required=True, type=str,
-                        help='node name')
-
-    parser.add_argument('--nodeverkey', required=True, type=str,
-                        help="node's ver key")
-
-    parser.add_argument('--stewardseed', required=True, type=str,
-                        help="stewards's seed")
-
-    parser.add_argument('--nodestacknewha', required=True, type=str,
-                        help="node stack's new HA, format=> ip:port")
-
-    parser.add_argument('--clientstacknewha', required=False, type=str,
-                        help="client stack's new HA, format=> ip:port")
-
-    args = parser.parse_args()
-
-    env = args.env
-    nodeName = args.name
-    nodeStackNewHA = args.nodestacknewha
-    clientStackNewHA = args.clientstacknewha
-    nodeVerKey = args.nodeverkey
-    stewardsSeed = bytes(args.stewardseed, 'utf-8')
-    looper = Looper(debug=True)
-    config = getConfig()
-    changeHACore(looper, basedirpath, config, env, nodeName, nodeVerKey,
-                 stewardsSeed, nodeStackNewHA, clientStackNewHA)
+def __checkClientConnected(cli, ):
+    assert cli.hasSufficientConnections
 
 
-def changeHACore(looper, basedirpath, config, env, nodeName, nodeVerKey,
-                 stewardsSeed, nodeStackNewHA, clientStackNewHA=None):
+def changeHA(looper, config, nodeName, nodeSeed, newNodeHA,
+                 stewardsName, stewardsSeed, newClientHA=None):
 
-    nodesNewIp, nodesNewPort = tuple(nodeStackNewHA.split(':'))
-    nodeStackNewHA = (nodesNewIp, nodesNewPort)
-    if not clientStackNewHA:
-        clientStackNewHA = (nodesNewIp, str(int(nodesNewPort) + 1))
+    if not newClientHA:
+        newClientHA = HA(newNodeHA.host, newNodeHA.port + 1)
+
+    # prepare Steward
     stewardSigner = SimpleSigner(seed=stewardsSeed)
-    stewardWallet = Wallet("StewardWallet")
+    stewardWallet = Wallet(stewardsName)
     stewardWallet.addIdentifier(signer=stewardSigner)
-    client_address = ('0.0.0.0', 9761)
-
-    config.poolTransactionsFile = ENVS[env].poolLedger
-    config.domainTransactionsFile = ENVS[env].domainLedger
-    client = Client("changehasteward",
-                    ha=client_address,
-                    config=config,
-                    basedirpath=basedirpath)
-
-
+    randomClientPort = random.randint(9700, 9799)
+    client = Client(stewardsName,
+                    ha=('0.0.0.0', randomClientPort), config=config)
     looper.add(client)
-    looper.runFor(20)
+    looper.run(eventually(__checkClientConnected, client,
+                          retryWait=1, timeout=5))
+    # looper.runFor(3)
+    # if nodeName + 'C' in client.nodestack.conns:
+    #     raise Exception("Node '{}' must be stopped "
+    #                     "before".format(nodeName))
+    nodeVerKey = SimpleSigner(seed=nodeSeed).verkey
+    # send request
     req = submitNodeIpChange(client, stewardWallet, nodeName, nodeVerKey,
-                             nodeStackNewHA, clientStackNewHA)
+                             newNodeHA, newClientHA)
     return client, req
--- plenum/test/script/test_change_node_ha.py
+++ plenum/test/script/test_change_node_ha.py
@@ -1,12 +1,13 @@
 import pytest
 from plenum.common.looper import Looper
 from plenum.common.port_dispenser import genHa
-from plenum.common.script_helper import changeHACore
+from plenum.common.script_helper import changeHA
 from plenum.common.signer_simple import SimpleSigner
+from plenum.common.types import HA
 
 from plenum.common.util import getMaxFailures
 from plenum.test.eventually import eventually
-from plenum.test.helper import checkSufficientRepliesRecvd
+from plenum.test.helper import checkSufficientRepliesRecvd, TestNode, checkNodesConnected, genTestClient
 
 
 @pytest.yield_fixture(scope="module")
@@ -15,22 +16,36 @@ def looper():
         yield l
 
 
-def testChangeNodeHa(looper, txnPoolNodeSet, tdir, poolTxnData,
-                     poolTxnStewardData, conf):
-    nodeName = txnPoolNodeSet[0].name
-    nodeSeed = poolTxnData["seeds"][nodeName]
-    nodeVerkey = SimpleSigner(seed=bytes(nodeSeed, 'utf-8')).verkey
-    _, stewardsSeed = poolTxnStewardData
+def testChangeNodeHa(looper, txnPoolNodeSet, tdirWithPoolTxns,
+                     tdir, poolTxnData, poolTxnStewardData, tconf):
+    subjectedNode = txnPoolNodeSet[0]
+    nodeSeed = poolTxnData["seeds"][subjectedNode.name].encode()
+    stewardName, stewardsSeed = poolTxnStewardData
     ip, port = genHa()
-    nodeStackNewHA = "{}:{}".format(ip, port)
+    nodeStackNewHA = HA(ip, port)
+    clientStackNewHA = HA(ip, port + 1)
 
-    client, req = changeHACore(looper, tdir, conf, "test", nodeName,
-                               nodeVerkey, stewardsSeed, nodeStackNewHA)
+    # stop node which HA will be changed
+    subjectedNode.stop()
+    # change HA
+    client, req = changeHA(looper, tconf, subjectedNode.name, nodeSeed,
+                           nodeStackNewHA, stewardName, stewardsSeed)
     f = getMaxFailures(len(client.nodeReg))
-    looper.run(eventually(checkSufficientRepliesRecvd, client.inBox, req.reqId
-                          , f, retryWait=1, timeout=8))
-    looper.runFor(10)
+    looper.run(eventually(checkSufficientRepliesRecvd, client.inBox, req.reqId,
+                          f, retryWait=1, timeout=8))
+    # start node with new HA
+    restartedNode = TestNode(subjectedNode.name, basedirpath=tdirWithPoolTxns,
+                             config=tconf, ha=nodeStackNewHA,
+                             cliha=clientStackNewHA)
+    looper.add(restartedNode)
+    looper.run(checkNodesConnected(txnPoolNodeSet))
 
+    # start client and check the node HA
+    anotherClient, _ = genTestClient(tmpdir=tdir, usePoolLedger=True)
+    looper.add(anotherClient)
+    looper.run(anotherClient.ensureConnectedToNodes())
+
+    pass
     # TODO: Once it is sure, that node ha is changed, following is pending
     # 1. Restart nodes with new node ha
     # 2. Start a new client (should have different tdir)
@@ -38,3 +53,20 @@ def testChangeNodeHa(looper, txnPoolNodeSet, tdir, poolTxnData,
     # 3. Check that client's master pool txn file
     # gets updated (corresponding code needs to be written)
     # 4. Any other tests we can think of to thoroughly test it
+
+
+def testStopScriptIfNodeIsRunning(looper, txnPoolNodeSet, poolTxnData,
+                                  poolTxnStewardData, tconf):
+    nodeName = txnPoolNodeSet[0].name
+    nodeSeed = poolTxnData["seeds"][nodeName].encode()
+    stewardName, stewardsSeed = poolTxnStewardData
+    ip, port = genHa()
+    nodeStackNewHA = HA(ip, port)
+
+    # the node `nodeName` is not stopped here
+
+    # change HA
+    with pytest.raises(Exception, message="Node '{}' must be stopped "
+                                          "before".format(nodeName)):
+        changeHA(looper, tconf, nodeName, nodeSeed, nodeStackNewHA,
+                 stewardName, stewardsSeed)
