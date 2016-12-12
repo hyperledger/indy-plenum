70895ab8039ef993641e8a4298cb589ea80bdb8d
Lovesh <lovesh.harchandani@evernym.com>
2016-12-12 13:31:58 +0530

    updating method call

--- plenum/test/plugin/test_bank_req_processor_plugin.py
+++ plenum/test/plugin/test_bank_req_processor_plugin.py
@@ -120,6 +120,6 @@ def testBankTransactions(nodeSet, up, looper, apps):
     jason.sendMoney(john.wallet.defaultId, 100, nodeSet)
     txnReqJl = jason.checkTxns()
     txnReqJb = john.checkTxns()
-    txnJason = jason.client.hasConsensus(*txnReqJl.reqKey)[ALL_TXNS]
-    txnJohn = john.client.hasConsensus(*txnReqJb.reqKey)[ALL_TXNS]
+    txnJason = jason.client.hasConsensus(*txnReqJl.key)[ALL_TXNS]
+    txnJohn = john.client.hasConsensus(*txnReqJb.key)[ALL_TXNS]
     assert txnJason == txnJohn
