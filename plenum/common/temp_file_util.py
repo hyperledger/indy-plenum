b2624cf20f35f6274d3fe03823ce880802a3cd31
Nikita Spivachuk <nikita.spivachuk@dsr-company.com>
2016-12-29 14:49:58 +0300

    Made SafeTemporaryDirectory to be a subclass of tempfile.TemporaryDirectory.

--- plenum/common/temp_file_util.py
+++ plenum/common/temp_file_util.py
@@ -1,32 +1,19 @@
 import shutil
 import warnings
-import weakref
-from tempfile import mkdtemp
+from tempfile import TemporaryDirectory
 
-class SafeTemporaryDirectory(object):
-    """Version of tempfile.TemporaryDirectory that works on Windows 10
+
+class SafeTemporaryDirectory(TemporaryDirectory):
+    """TemporaryDirectory that works on Windows 10
     """
 
-    def __init__(self, suffix=None, prefix=None, dir=None):
-        self.name = mkdtemp(suffix, prefix, dir)
-        self._finalizer = weakref.finalize(
-            self, self._cleanup, self.name,
-            warn_message="Implicitly cleaning up {!r}".format(self))
+    def __init__(self, *args, **kwargs):
+        super(SafeTemporaryDirectory, self).__init__(*args, **kwargs)
 
     @classmethod
     def _cleanup(cls, name, warn_message):
         shutil.rmtree(name, ignore_errors=True)
         warnings.warn(warn_message, ResourceWarning)
-
-
-    def __repr__(self):
-        return "<{} {!r}>".format(self.__class__.__name__, self.name)
-
-    def __enter__(self):
-        return self.name
-
-    def __exit__(self, exc, value, tb):
-        self.cleanup()
 
     def cleanup(self):
         if self._finalizer.detach():
