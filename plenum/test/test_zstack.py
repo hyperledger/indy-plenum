1d41ef54b71f23cf93bbbeb61888f639486a3f98
Lovesh <lovesh.harchandani@evernym.com>
2017-02-25 04:02:01 +0530

    completing test

--- plenum/test/test_zstack.py
+++ plenum/test/test_zstack.py
@@ -2,17 +2,42 @@ import os
 import shutil
 from distutils.dir_util import copy_tree
 
-from plenum.common.looper import Prodable
+import asyncio
+import pytest
+import zmq.asyncio
+
+from plenum.common.eventually import eventually
+from plenum.common.looper import Prodable, Looper
 from plenum.common.motor import Motor
 from plenum.common.port_dispenser import genHa
+from plenum.common.temp_file_util import SafeTemporaryDirectory
 from plenum.common.z_util import generate_certificates
 from plenum.common.zstack import ZStack
-from plenum.test.test_node_connection import tdirAndLooper
 
 
-def printer(m):
-    print('Printing...')
-    print(m)
+# Need this loop to be passed in the looper, can make a module fixture
+# since need to use it with function fixture below
+loop = zmq.asyncio.ZMQEventLoop()
+loop.set_debug(True)
+
+
+@pytest.yield_fixture()
+def tdirAndLooper():
+    asyncio.set_event_loop(loop)
+
+    with SafeTemporaryDirectory() as td:
+        with Looper(loop=loop, debug=True) as looper:
+            yield td, looper
+
+
+class Printer:
+    def __init__(self):
+        self.printeds = []
+
+    def print(self, m):
+        print('Printing...')
+        print(m)
+        self.printeds.append(m)
 
 
 class SMotor(Motor):
@@ -39,6 +64,10 @@ def genKeys(directory, names):
             copy_tree(os.path.join(directory, kd), os.path.join(d, kd))
 
 
+def chkPrinted(p, m):
+    assert m in [_[0] for _ in p.printeds]
+
+
 def testRestricted2ZStackCommunication(tdirAndLooper):
     """
     Create 2 ZStack and make them send and receive messages.
@@ -47,28 +76,28 @@ def testRestricted2ZStackCommunication(tdirAndLooper):
     """
     tdir, looper = tdirAndLooper
     names = ['Alpha', 'Beta']
-    # generate_certificates(tdir, *names, clean=True)
-    # for n in names:
-    #     d = os.path.join(tdir, n)
-    #     os.makedirs(d, exist_ok=True)
-    #     for kd in ZStack.keyDirNames():
-    #         copy_tree(os.path.join(tdir, kd), os.path.join(d, kd))
     genKeys(tdir, names)
-    alpha = ZStack(names[0], ha=genHa(), basedirpath=tdir, msgHandler=printer,
+    alphaP = Printer()
+    betaP = Printer()
+
+    alpha = ZStack(names[0], ha=genHa(), basedirpath=tdir, msgHandler=alphaP.print,
                    restricted=True)
-    beta = ZStack(names[1], ha=genHa(), basedirpath=tdir, msgHandler=printer,
+    beta = ZStack(names[1], ha=genHa(), basedirpath=tdir, msgHandler=betaP.print,
                   restricted=True)
+
     aMotor = SMotor(alpha)
     bMotor = SMotor(beta)
     looper.add(aMotor)
     looper.add(bMotor)
-    looper.runFor(1)
+    looper.runFor(.5)
     alpha.connect(beta.name, beta.ha, beta.verKey, beta.publicKey)
     beta.connect(alpha.name, alpha.ha, alpha.verKey, alpha.publicKey)
-    looper.runFor(1.5)
+    looper.runFor(.5)
     alpha.send({'greetings': 'hi'}, beta.name)
     beta.send({'greetings': 'hello'}, alpha.name)
-    looper.runFor(1.5)
+
+    looper.run(eventually(chkPrinted, alphaP, {'greetings': 'hello'}))
+    looper.run(eventually(chkPrinted, betaP, {'greetings': 'hi'}))
 
 
 def testUnrestricted2ZStackCommunication(tdirAndLooper):
@@ -80,18 +109,23 @@ def testUnrestricted2ZStackCommunication(tdirAndLooper):
     tdir, looper = tdirAndLooper
     names = ['Alpha', 'Beta']
     genKeys(tdir, names)
-    alpha = ZStack(names[0], ha=genHa(), basedirpath=tdir, msgHandler=printer,
+    alphaP = Printer()
+    betaP = Printer()
+    alpha = ZStack(names[0], ha=genHa(), basedirpath=tdir, msgHandler=alphaP.print,
                    restricted=False)
-    beta = ZStack(names[1], ha=genHa(), basedirpath=tdir, msgHandler=printer,
+    beta = ZStack(names[1], ha=genHa(), basedirpath=tdir, msgHandler=betaP.print,
                   restricted=False)
     aMotor = SMotor(alpha)
     bMotor = SMotor(beta)
     looper.add(aMotor)
     looper.add(bMotor)
-    looper.runFor(1)
+    looper.runFor(.5)
     alpha.connect(beta.name, beta.ha, beta.verKey, beta.publicKey)
     beta.connect(alpha.name, alpha.ha, alpha.verKey, alpha.publicKey)
-    looper.runFor(1.5)
+    looper.runFor(.5)
     alpha.send({'greetings': 'hi'}, beta.name)
     beta.send({'greetings': 'hello'}, alpha.name)
-    looper.runFor(1.5)
+    looper.runFor(.5)
+
+    looper.run(eventually(chkPrinted, alphaP, {'greetings': 'hello'}))
+    looper.run(eventually(chkPrinted, betaP, {'greetings': 'hi'}))
