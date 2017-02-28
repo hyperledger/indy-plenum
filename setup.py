f7904e47cf40f27f881807512e968d3c43d02360
Alexander Shekhovtsov <alexander.shekhovcov@dsr-company.com>
2017-02-28 18:17:41 +0300

    checkout setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ if not os.path.exists(BASE_DIR):
     os.makedirs(BASE_DIR)
 
 setup(
-    name='plenum-dev',
+    name='plenum',
     version=__version__,
     description='Plenum Byzantine Fault Tolerant Protocol',
     long_description='Plenum Byzantine Fault Tolerant Protocol',
@@ -61,7 +61,7 @@ setup(
     data_files=[(
         (BASE_DIR, ['data/pool_transactions_sandbox', ])
     )],
-    install_requires=['ledger-dev==0.0.86',
+    install_requires=['ledger==0.0.22',
                       'raet', 'jsonpickle', 'portalocker==0.5.7',
                       'prompt_toolkit==0.57', 'pyorient', 'pygments',
                       'ioflo==1.5.4', 'semver', 'base58', 'orderedset',
