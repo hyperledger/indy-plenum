0a970a7c56ffa0372724d442cc395fb8ad9860b1
Lovesh <lovesh.harchandani@evernym.com>
2017-03-14 01:48:20 +0530

    cleaning up before keys

--- plenum/common/init_util.py
+++ plenum/common/init_util.py
@@ -7,14 +7,15 @@ def cleanup_environment(name, base_dir):
     HasFileStorage.wipeDataLocation(dataLocation)
 
 
-def initialize_node_environment(name, base_dir, sigseed=None, override=False):
+def initialize_node_environment(name, base_dir, sigseed=None,
+                                override_keep=False):
     # Question: Does this comment belong here? I dont think so.
     """
     transport-agnostic method; in the future when the transport protocol is
     abstracted a bit more, this function and the one below will be the same
     and likely a method of an interface
     """
-    _, vk = initLocalKeep(name=name, baseDir=base_dir, sigseed=sigseed,
-                          override=override)
     cleanup_environment(name, base_dir)
+    _, vk = initLocalKeep(name=name, baseDir=base_dir, sigseed=sigseed,
+                          override=override_keep)
     return vk
