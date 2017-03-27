294a106ddf56f5ca196f3a266317c6e75cce31e7
andrey.goncharov <andrey.goncharov@dsr-company.com>
2017-03-28 02:02:00 +0300

    final

--- plenum/__init__.py
+++ plenum/__init__.py
@@ -6,11 +6,5 @@ from __future__ import absolute_import, division, print_function
 
 import sys
 
-import plenum
-
 if sys.version_info < (3, 5, 0):
     raise ImportError("Python 3.5.0 or later required.")
-
-
-import importlib
-from .__metadata__ import *
