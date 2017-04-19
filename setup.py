fce975975599c36addb0aa1621b241121c104683
ashcherbakov <alexander.sherbakov@dsr-company.com>
2017-04-19 19:38:27 +0300

    increment stp version

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(
     data_files=[(
         (BASE_DIR, ['data/pool_transactions_sandbox', ])
     )],
-    install_requires=['ledger-3pc-batch==0.2.7', 'stp-3pc-batch==0.1.5',
+    install_requires=['ledger-3pc-batch==0.2.7', 'stp-3pc-batch==0.1.8',
                       'evernym-state==0.1.4', 'jsonpickle',
                       'portalocker==0.5.7', 'prompt_toolkit==0.57', 'pygments',
                       'ioflo==1.5.4', 'semver', 'base58', 'orderedset',
