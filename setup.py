fe5ea4bbc0627d053d78d8e9a4a5130fb074b7b1
Victor Muzychenko <mzkvct@gmail.com>
2017-05-12 18:37:15 +0300

    update versions of ledger and stp

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
     data_files=[(
         (BASE_DIR, ['data/pool_transactions_sandbox', ])
     )],
-    install_requires=['ledger-3pc-batch==0.2.16', 'stp-3pc-batch==0.1.15',
+    install_requires=['ledger-dev==0.2.11', 'stp-dev==0.1.47',
                       'state-trie==0.1.14', 'jsonpickle',
                       'prompt_toolkit==0.57', 'pygments',
                       'ioflo==1.5.4', 'semver', 'base58', 'orderedset',
