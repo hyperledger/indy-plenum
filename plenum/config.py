594526c6fb410fb279d932ecbc02669ee20a0505
keenondrums <frantic1993@gmail.com>
2017-05-24 17:15:36 +0300

    Reverted timeouts

--- plenum/config.py
+++ plenum/config.py
@@ -133,7 +133,7 @@ enableStdOutLogging=True
 # OPTIONS RELATED TO TESTS
 
 # todo test 60sec after https://evernym.atlassian.net/browse/SOV-995 closed
-TestRunningTimeLimitSec = 150
+TestRunningTimeLimitSec = 100
 
 # Expected time for one stack to get connected to another
 ExpectedConnectTime = 3.3 if sys.platform == 'win32' else 2
