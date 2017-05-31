dcaa72878445b3728e78fae0d78cc79aaaab3639
Victor Muzychenko <mzkvct@gmail.com>
2017-05-31 19:53:47 +0300

    refactor test for timestamp

--- plenum/test/input_validation/fields_validation/test_timestamp_field.py
+++ plenum/test/input_validation/fields_validation/test_timestamp_field.py
@@ -1,19 +1,20 @@
 import pytest
 from plenum.common.messages.fields import TimestampField
-from plenum.test.input_validation.utils import *
 from datetime import datetime
 
+validator = TimestampField()
+timestamp = datetime.now().timestamp()
 
-def test_value_range_validation():
-    validator = TimestampField()
-    timestamp = datetime.now().timestamp()
 
-    assert_valid(validator.validate(timestamp))
+def test_valid_value():
+    assert not validator.validate(timestamp)
 
     # This is needed because timestamp is usually multiplied
     # by 1000 to "make it compatible to JavaScript Date()"
-    assert_valid(validator.validate(round(timestamp * 1000)))
+    assert not validator.validate(round(timestamp * 1000))
 
-    assert_invalid(validator.validate(-1))
+
+def test_invalid_value():
+    assert validator.validate(-1)
 
 
