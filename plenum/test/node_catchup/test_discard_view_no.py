90c5e3d989adc4c53a382406415197c4deb66676
Andrey Kononykhin <andkononykhin@gmail.com>
2017-06-01 15:57:15 +0300

    enabled and updated testNodeDiscardMessageFromUnknownView

--- plenum/test/node_catchup/test_discard_view_no.py
+++ plenum/test/node_catchup/test_discard_view_no.py
@@ -6,20 +6,19 @@ import pytest
 from stp_core.loop.eventually import eventually
 from plenum.common.types import Nomination, PrePrepare
 from plenum.common.util import randomString
+from plenum.common.constants import DOMAIN_LEDGER_ID
 from plenum.test.delayers import delayNonPrimaries
 from plenum.test.helper import sendReqsToNodesAndVerifySuffReplies, \
     waitForViewChange, checkDiscardMsg
 from plenum.test.node_catchup.helper import waitNodeDataEquality
 from plenum.test.pool_transactions.helper import addNewStewardAndNode
 from plenum.test.test_node import checkNodesConnected, \
-    checkProtocolInstanceSetup
+    checkProtocolInstanceSetup, getPrimaryReplica
 from plenum.test import waits
 
 
 whitelist = ['found legacy entry']  # warnings
 
-
-@pytest.mark.skip(reason='INDY-90')
 def testNodeDiscardMessageFromUnknownView(txnPoolNodeSet,
                                           nodeSetWithNodeAddedAfterSomeTxns,
                                           newNodeCaughtUp, tdirWithPoolTxns,
@@ -34,9 +33,32 @@ def testNodeDiscardMessageFromUnknownView(txnPoolNodeSet,
 
     # Delay processing of PRE-PREPARE from all non primary replicas of master
     # so master's performance falls and view changes
-    delayNonPrimaries(txnPoolNodeSet, 0, 10)
-    sendReqsToNodesAndVerifySuffReplies(looper, wallet, client, 4)
-    waitForViewChange(looper, txnPoolNodeSet, expectedViewNo=viewNo+1)
+
+    def forceViewChange(delay):
+        try:
+            cancelDelays = delayNonPrimaries(txnPoolNodeSet, 0,delay)
+            sendReqsToNodesAndVerifySuffReplies(looper, wallet, client, 4)
+            waitForViewChange(looper, txnPoolNodeSet, expectedViewNo=viewNo+1)
+        finally:
+            cancelDelays()
+
+    # TODO it's a workaround until view change related issues are resolved
+    # the goal is to force view change BUT after all responses are passed
+    # to client
+    delay = 0.1
+    numTries = 3
+    while True:
+        numTries -= 1
+        try:
+            forceViewChange(delay)
+        except AssertionError:
+            if numTries > 0:
+                delay += 0.1
+                pass
+            else:
+                raise
+        else:
+            break
 
     newStewardName = "testClientSteward" + randomString(3)
     nodeName = "Theta"
@@ -50,22 +72,24 @@ def testNodeDiscardMessageFromUnknownView(txnPoolNodeSet,
     looper.run(checkNodesConnected(txnPoolNodeSet))
     looper.run(client.ensureConnectedToNodes())
     waitNodeDataEquality(looper, nodeTheta, *txnPoolNodeSet[:-1])
-    checkProtocolInstanceSetup(looper, txnPoolNodeSet, retryWait=1,
-                               timeout=10)
+    checkProtocolInstanceSetup(looper, txnPoolNodeSet, retryWait=1)
     electMsg = Nomination(nodeX.name, 0, viewNo,
                           nodeX.replicas[0].lastOrderedPPSeqNo)
+
+    primaryRepl = getPrimaryReplica(txnPoolNodeSet)
     threePMsg = PrePrepare(
             0,
             viewNo,
             10,
-            wallet.defaultId,
-            wallet._getIdData().lastReqId+1,
+            time.time(),
+            [[wallet.defaultId, wallet._getIdData().lastReqId+1]],
+            1,
             "random digest",
-            time.time()
+            DOMAIN_LEDGER_ID,
+            primaryRepl.stateRootHash(DOMAIN_LEDGER_ID),
+            primaryRepl.txnRootHash(DOMAIN_LEDGER_ID),
             )
     ridTheta = nodeX.nodestack.getRemote(nodeTheta.name).uid
-    nodeX.send(electMsg, ridTheta)
-    nodeX.send(threePMsg, ridTheta)
     nodeX.send(electMsg, ridTheta)
 
     messageTimeout = waits.expectedNodeToNodeMessageDeliveryTime()
