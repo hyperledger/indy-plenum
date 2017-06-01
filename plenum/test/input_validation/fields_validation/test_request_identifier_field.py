617e66cef30bd43e0cf6c2bc80ef5cb17c91d64b
Victor Muzychenko <mzkvct@gmail.com>
2017-06-01 17:43:13 +0300

    update test for request identifier field

--- plenum/test/input_validation/fields_validation/test_request_identifier_field.py
+++ plenum/test/input_validation/fields_validation/test_request_identifier_field.py
@@ -3,38 +3,47 @@ from plenum.common.messages.fields import RequestIdentifierField
 
 validator = RequestIdentifierField()
 
+valid_client_id_chars = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+
+MIN_LENGTH_SHORT = 15
+MAX_LENGTH_SHORT = 25
+MIN_LENGTH_LONG = 43
+MAX_LENGTH_LONG = 45
+
 # Request id consists of client identifier (base56 string 16/32 long) and
 # some number (for now it is current timestamp, but can be any number)
-valid_request_id_long = ("123456789ABCDEFGHJKLMNPQRSTUVWXY", 11111)
-valid_request_id_short = ("123456789ABCDEFG", 11111)
+valid_request_id = (valid_client_id_chars[:MIN_LENGTH_SHORT], 11111)
 
 
 def test_valid_request_id():
-    assert not validator.validate(valid_request_id_long)
-    assert not validator.validate(valid_request_id_short)
+    all_valid_length = \
+        list(range(MIN_LENGTH_SHORT, MAX_LENGTH_SHORT + 1)) + \
+        list(range(MIN_LENGTH_LONG, MAX_LENGTH_LONG + 1))
+    for length in all_valid_length:
+        assert not validator.validate((valid_client_id_chars[:length], 11111))
 
 
 def test_invalid_order():
-    s, t = valid_request_id_short
+    s, t = valid_request_id
     assert validator.validate((t, s))
 
 
 def test_empty_client_id():
-    assert validator.validate(("", valid_request_id_short[1]))
-    assert validator.validate((None, valid_request_id_short[1]))
+    assert validator.validate(("", valid_request_id[1]))
+    assert validator.validate((None, valid_request_id[1]))
 
 
 def test_empty_number():
-    assert validator.validate((valid_request_id_short[0], None))
+    assert validator.validate((valid_request_id[0], None))
 
 
 def test_invalid_char():
-    invalid_client_id = valid_request_id_short[0][:-1] + "0"
-    invalid_request = (invalid_client_id, valid_request_id_short[1])
+    invalid_client_id = valid_request_id[0][:-1] + "0"
+    invalid_request = (invalid_client_id, valid_request_id[1])
     assert validator.validate(invalid_request)
 
 
 def test_invalid_length():
-    invalid_client_id = valid_request_id_short[0] + "1"
-    invalid_request = (invalid_client_id, valid_request_id_short[1])
+    invalid_client_id = valid_request_id[:-1]
+    invalid_request = (invalid_client_id, valid_request_id[1])
     assert validator.validate(invalid_request)
