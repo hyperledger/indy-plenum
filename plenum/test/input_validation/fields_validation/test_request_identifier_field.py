a89c71b260f882f90a32bc81b808f9fa16eb8e94
Victor Muzychenko <mzkvct@gmail.com>
2017-06-01 15:42:27 +0300

    update test for request identifier

--- plenum/test/input_validation/fields_validation/test_request_identifier_field.py
+++ plenum/test/input_validation/fields_validation/test_request_identifier_field.py
@@ -3,8 +3,10 @@ from plenum.common.messages.fields import RequestIdentifierField
 from datetime import datetime
 validator = RequestIdentifierField()
 
-# Request id consists of non-empty string and timestamp
-valid_request_id = ("some_string", datetime.now().timestamp())
+# Request id consists of client identifier and
+# some number (for now it is current timestamp, but can be any number)
+
+valid_request_id = ("11111111111111111111111111111111", 11111)
 
 
 def test_valid_request_id():
@@ -16,6 +18,10 @@ def test_invalid_order():
     assert validator.validate((t, s))
 
 
-def test_empty_string():
+def test_empty_client_id():
     assert validator.validate(("", valid_request_id[1]))
     assert validator.validate((None, valid_request_id[1]))
+
+
+def test_empty_number():
+    assert validator.validate((valid_request_id[0], None))
