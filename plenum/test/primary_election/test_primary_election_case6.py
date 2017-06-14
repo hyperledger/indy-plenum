871c08f01f82326ed77bcf74d773deb00acc9b30
Lovesh <lovesh.harchandani@evernym.com>
2017-06-15 00:50:42 +0530

    update test with more checks

--- plenum/test/primary_election/test_primary_election_case6.py
+++ plenum/test/primary_election/test_primary_election_case6.py
@@ -2,12 +2,13 @@ import pytest
 
 from plenum.common.types import Primary, Nomination, Reelection
 from plenum.test.delayers import delay
+from plenum.test.helper import sendReqsToNodesAndVerifySuffReplies
 from plenum.test.test_node import checkNodesConnected, \
     checkProtocolInstanceSetup
 from stp_core.loop.eventually import eventually
 
 
-@pytest.fixture()
+@pytest.fixture(scope='module')
 def case_6_setup(startedNodes):
     A, B, C, D = startedNodes.nodes.values()
 
@@ -16,17 +17,13 @@ def case_6_setup(startedNodes):
         delay(m, frm=B, to=A, howlong=120)
 
     # A will get Primary earlier than Nominates
-    delay(Nomination, frm=(C, D), to=A, howlong=5)
+    delay(Nomination, frm=(C, D), to=A, howlong=10)
 
 
 # noinspection PyIncorrectDocstring
-def test_primary_election_case6(case_6_setup, looper, keySharedNodes):
-    """
-    A is disconnected with B so A does not get any Nomination/Primary from
-    B (simulated by a large delay). A gets Nominations delayed due to which is
-    sends Primary only after it has received Primary from other 2 nodes.
-    A should still be able to select a primary and the pool should function.
-    """
+@pytest.fixture(scope='module')
+def elections_done(case_6_setup, looper, keySharedNodes):
+    # Make sure elections are done successfully
     nodeSet = keySharedNodes
     A, B, C, D = nodeSet.nodes.values()
     looper.run(checkNodesConnected(nodeSet))
@@ -47,7 +44,21 @@ def test_primary_election_case6(case_6_setup, looper, keySharedNodes):
         }
 
         for i in inst_ids:
-            assert primary_send_times[i] > primary_recv_times[i]
+            assert primary_send_times[i][0] > max(primary_recv_times[i])
 
-    looper.run(eventually(chk, retryWait=1, timeout=10))
+    looper.run(eventually(chk, retryWait=1, timeout=15))
     checkProtocolInstanceSetup(looper=looper, nodes=nodeSet, retryWait=1)
+    
+    for i in inst_ids:
+        assert B.replicas[i].name not in A.elector.nominations[i]
+        assert B.replicas[i].name not in A.elector.primaryDeclarations[i]
+
+
+def test_primary_election_case6(elections_done, looper, client1, wallet1):
+    """
+    A is disconnected with B so A does not get any Nomination/Primary from
+    B (simulated by a large delay). A gets Nominations delayed due to which is
+    sends Primary only after it has received Primary from other 2 nodes.
+    A should still be able to select a primary and the pool should function.
+    """
+    sendReqsToNodesAndVerifySuffReplies(looper, wallet1, client1, 5)
