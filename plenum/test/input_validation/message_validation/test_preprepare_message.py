bbab343a9936f31111630a849d1e8c1e76dc4ab6
Victor Muzychenko <mzkvct@gmail.com>
2017-06-14 14:15:17 +0300

    add tests for Prepare and PrePrepare messages

---
+++ plenum/test/input_validation/message_validation/test_preprepare_message.py
@@ -0,0 +1,34 @@
+import pytest
+from plenum.common.types import PrePrepare
+from collections import OrderedDict
+from plenum.common.messages.fields import NonNegativeNumberField, \
+    LedgerIdField, MerkleRootField, IterableField, NonEmptyStringField, \
+    TimestampField, HexField
+
+EXPECTED_ORDERED_FIELDS = OrderedDict([
+    ("instId", NonNegativeNumberField),
+    ("viewNo", NonNegativeNumberField),
+    ("ppSeqNo", NonNegativeNumberField),
+    ("ppTime", TimestampField),
+    ("reqIdr", IterableField),
+    ("discarded", NonNegativeNumberField),
+    ("digest", NonEmptyStringField),
+    ("ledgerId", LedgerIdField),
+    ("stateRootHash", HexField),
+    ("txnRootHash", HexField),
+])
+
+
+def test_hash_expected_type():
+    assert PrePrepare.typename == "PREPREPARE"
+
+
+def test_has_expected_fields():
+    actual_field_names = OrderedDict(PrePrepare.schema).keys()
+    assert actual_field_names == EXPECTED_ORDERED_FIELDS.keys()
+
+
+def test_has_expected_validators():
+    schema = dict(PrePrepare.schema)
+    for field, validator in EXPECTED_ORDERED_FIELDS.items():
+        assert isinstance(schema[field], validator)
