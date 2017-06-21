7ab632263733c707bdc66051d2da3f402affc0c3
ashcherbakov <alexander.sherbakov@dsr-company.com>
2017-06-21 18:31:51 +0300

    fix tests

--- plenum/test/view_change/test_queueing_req_from_future_view.py
+++ plenum/test/view_change/test_queueing_req_from_future_view.py
@@ -11,7 +11,7 @@ from plenum.test.delayers import ppDelay, icDelay
 from plenum.test.helper import sendRandomRequest, \
     sendReqsToNodesAndVerifySuffReplies
 from plenum.test.test_node import TestReplica, getNonPrimaryReplicas, \
-    checkViewChangeInitiatedForNode
+    checkViewChangeInitiatedForNode, get_last_master_non_primary_node
 
 nodeCount = 7
 
@@ -24,31 +24,17 @@ def testQueueingReqFromFutureView(delayed_perf_chk, looper, nodeSet, up,
     """
     Test if every node queues 3 Phase requests(PRE-PREPARE, PREPARE and COMMIT)
     that come from a view which is greater than the current view.
-    - Slow down the primary node of master protocol instance
-       => view change
     - Delay reception and processing of view change messages by a non primary for master instance
        => it starts receiving 3 phase commit messages for next view
     """
 
-    nprs = getNonPrimaryReplicas(nodeSet, 0)
-    lagging_node = nprs[0].node
+    lagging_node = get_last_master_non_primary_node(nodeSet)
     old_view_no = lagging_node.viewNo
 
     # Delay processing of instance change on a node
     delay_ic = 60
     lagging_node.nodeIbStasher.delay(icDelay(delay_ic))
     logger.debug('{} will delay its view change'.format(lagging_node))
-
-    # Delay processing of PRE-PREPARE from all non primary replicas of master
-    # so master's throughput falls and view changes
-    delay_pp = 5
-    pp_delayer = ppDelay(delay_pp, 0)
-    for r in nprs:
-        r.node.nodeIbStasher.delay(pp_delayer)
-
-    timeout = waits.expectedTransactionExecutionTime(len(nodeSet)) + delay_pp
-    sendReqsToNodesAndVerifySuffReplies(looper, wallet1, client1, 5,
-                                        customTimeoutPerReq=timeout)
 
     def chk_fut_view(view_no, is_empty):
         length = len(lagging_node.msgsForFutureViews.get(view_no, ()))
@@ -68,16 +54,14 @@ def testQueueingReqFromFutureView(delayed_perf_chk, looper, nodeSet, up,
                        [n for n in nodeSet if n != lagging_node],
                        [lagging_node])
 
-    for node in nodeSet:
-        node.reset_delays_and_process_delayeds()
-
-    sendReqsToNodesAndVerifySuffReplies(looper, wallet1, client1, 3,
-                                        customTimeoutPerReq=timeout)
-
-    # Messages queued for future view
+    # send more requests that will be queued for the lagged node
+    sendReqsToNodesAndVerifySuffReplies(looper, wallet1, client1, 3)
     l = chk_fut_view(old_view_no + 1, is_empty=False)
     logger.debug('{} has {} messages for future views'
                  .format(lagging_node, l))
+
+    # reset delays for the lagging_node node so that it finally makes view change
+    lagging_node.reset_delays_and_process_delayeds()
 
     # Eventually no messages queued for future view
     looper.run(eventually(chk_fut_view, old_view_no + 1, True,
