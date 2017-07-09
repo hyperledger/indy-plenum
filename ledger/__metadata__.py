35b9c6a89022cf6c6527acfdbf737ffe4023e486
andrey.goncharov <andrey.goncharov@dsr-company.com>
2017-07-09 11:03:20 +0300

    [WIP] Repo merge
    Test fixes

--- ledger/__metadata__.py
+++
@@ -1,9 +0,0 @@
-"""
-Ledger package metadata
-"""
-__version_info__ = (0, 3)
-__version__ = '.'.join(map(str, __version_info__))
-__author__ = "Evernym, Inc."
-__license__ = "Apache 2.0"
-
-__all__ = ['__version_info__', '__version__', '__author__', '__license__']
