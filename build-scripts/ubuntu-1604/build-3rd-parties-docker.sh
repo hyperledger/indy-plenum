4dbc035b56929f8dc1bce0fc0271c27f049e6c3f
Alexander Shekhovtsov <alexander.shekhovcov@dsr-company.com>
2017-07-14 16:06:22 +0300

    Refactor build-scripts

--- build-scripts/ubuntu-1604/build-3rd-parties-docker.sh
+++ build-scripts/ubuntu-1604/build-3rd-parties-docker.sh
@@ -3,19 +3,13 @@
 set -x
 set -e
 
-PKG_SOURCE_PATH=$1
-PKG_NAME=indy-plenum
-
-if [ -z ${PKG_SOURCE_PATH} ]; then
-    echo "Usage: $0 path-to-package-sources"
-    exit 1;
-fi
-
-if [ -z $2 ]; then
-    CMD="/root/build-indy-plenum.sh /input /output"
+if [ -z $1 ]; then
+    CMD="/root/build-3rd-parties.sh /output"
 else
-    CMD=$2
+    CMD=$1
 fi
+
+PKG_NAME=indy-plenum
 
 docker build -t indy-plenum-build-u1604 -f Dockerfile .
 
@@ -25,9 +19,6 @@ docker volume create --name ${OUTPUT_VOLUME_NAME}
 docker run \
     -i \
     --rm \
-    -v ${PKG_SOURCE_PATH}:/input \
     -v ${OUTPUT_VOLUME_NAME}:/output \
-    -e PKG_NAME=${PKG_NAME} \
     indy-plenum-build-u1604 \
     $CMD
-
