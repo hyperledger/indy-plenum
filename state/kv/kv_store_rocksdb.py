27a5e02b7f79ff2c1e76bdcc8d2e58de840dc6b8
Lovesh <lovesh.harchandani@evernym.com>
2017-07-18 11:57:42 +0530

    rocksdb integer comparator

--- state/kv/kv_store_rocksdb.py
+++ state/kv/kv_store_rocksdb.py
@@ -1,7 +1,28 @@
 from typing import Iterable, Tuple
 from state.kv.kv_store import KeyValueStorage
 
+try:
+    import rocksdb
+    from rocksdb.interfaces import Comparator
+except ImportError:
+    print('Cannot import rocksdb, please install')
+
 # TODO: WIP below
+
+class IntegerComparator(Comparator):
+    def compare(self, a, b):
+        a = int(a)
+        b = int(b)
+        if (a < b):
+            return -1
+        if (a > b):
+            return 1
+        if (a == b):
+            return 0
+
+    def name(self):
+        return b'IntegerComparator'
+
 class KeyValueStorageRocksdb(KeyValueStorage):
     def set(self, key, value):
         raise NotImplementedError
