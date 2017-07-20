5f9e3bd86355f613ced1058df65c4a6f9eaccc84
Victor Muzychenko <mzkvct@gmail.com>
2017-07-20 21:02:48 +0300

    add count function for counting elements in list

--- plenum/common/func_utils.py
+++ plenum/common/func_utils.py
@@ -2,7 +2,7 @@
 
 def flatten(some_list):
     """
-    [[1,2],[3,4]] => [1,2,3,4]
+    [[a,b],[c,d]] => [a,b,c,d]
     """
     for inner_list in some_list:
         for item in inner_list:
@@ -11,8 +11,18 @@ def flatten(some_list):
 
 def decons(some_list):
     """
-    [1,2,3,4] => (1, [2,3,4])
+    [a,b,c,d] => (a, [b,c,d])
     """
     if len(some_list) < 2:
         return None
     return some_list[0], some_list[1:]
+
+
+def count(some_list):
+    """
+    [a,b,c,a,b,b] => {a: 2, b: 3, c: 1}
+    """
+    votes = {}
+    for item in some_list:
+        votes[item] = 1 if item not in votes else votes[item] + 1
+    return sorted(votes.items(), key=lambda item: item[1], reverse=True)
