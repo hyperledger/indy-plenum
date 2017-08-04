846430997c1dba9671226bc537116c24fc9d7ead
Victor Muzychenko <mzkvct@gmail.com>
2017-08-04 18:37:16 +0300

    create Replicas class for replica management

---
+++ plenum/server/replicas.py
@@ -0,0 +1,22 @@
+from plenum.server.replica import Replica
+from typing import List
+
+
+class Replicas:
+
+    def __init__(self):
+        self.replicas = []  # type: List[Replica]
+
+    def add_replica(self, instance_id):
+        """
+        Creates replica for specified instance id
+        """
+        is_master = __
+
+        pass
+
+    def del_replica(self, instnace_id):
+        pass
+
+
+
