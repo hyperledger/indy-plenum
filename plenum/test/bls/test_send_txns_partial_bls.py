6a3d70ce0b4c7720a859db6a6b17e6720cb0ff7f
Victor Muzychenko <mzkvct@gmail.com>
2017-09-04 14:06:29 +0300

    rename test_send_txns_no_bls to test_send_txns_partial_bls to make it match file name and purpose

--- plenum/test/bls/test_send_txns_partial_bls.py
+++ plenum/test/bls/test_send_txns_partial_bls.py
@@ -5,7 +5,8 @@ from plenum.test.pool_transactions.conftest import looper, clientAndWallet1, \
 nodeCount = 4
 nodes_wth_bls = 2
 
-def test_send_txns_no_bls(tconf, looper, txnPoolNodeSet,
+
+def test_send_txns_partial_bls(tconf, looper, txnPoolNodeSet,
                           client1, client1Connected, wallet1):
     reqs = sendRandomRequests(wallet1, client1, 10)
     waitForSufficientRepliesForRequests(looper, client1, requests=reqs)
