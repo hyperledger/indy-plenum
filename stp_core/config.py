a43c34eea19532c09d9a9f49bb142e940e9657cb
Andrey Kononykhin <andkononykhin@gmail.com>
2017-09-06 18:28:21 +0300

    Rc 2.1 (#373)
    * Node status tool base support
    
    * Improve node status tool support
    
    * Add OOP, renaming
    
    * Remove unsupported fields
    
    * Renaming
    
    * Add dynamic tests
    
    * Make node status tool safe
    
    * Fix primary selection after primary demotion (INDY-463) (#354)
    
    * modified rank routine, added test cases for primary demotion
    
    * fixed pep8
    
    * fixed nodes oreder initialization
    
    * added checks that pool is functional after recofigurations
    
    * added additional view change to ensure original master is skipped
    
    * Merged hotfixes from stable for wallet and genesis pool transactions file migration and readme file.
    
    * Fix validator info tool (#365)
    
    * Don't add ip addresses, one-shot dump on start
    
    * Parameterize zmq network protocol
    
    * Cherry-pick cb7d068 8642be5 c37e835 from master

--- stp_core/config.py
+++ stp_core/config.py
@@ -44,6 +44,4 @@ ZMQ_INTERNAL_QUEUE_SIZE = 0  # messages (0 - no limit)
 
 
 # All messages exceeding the limit will be rejected without processing
-# TODO: need to think about a lower value; currently low value leads to
-# problems with batching on transport level
-MSG_LEN_LIMIT = 128 * 1024 * 1024
+MSG_LEN_LIMIT = 128 * 1024
