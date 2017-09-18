3743882a07a8c338cb052e0b269e45ca3c71db72
Dmitry Surnin <dmitry.surnin@dsr-company.com>
2017-09-18 11:51:55 +0300

    Add indy-crypto factory

--- plenum/bls/bls.py
+++ plenum/bls/bls.py
@@ -9,6 +9,8 @@ from plenum.bls.bls_key_manager_file import BlsKeyManagerFile
 from plenum.bls.bls_key_register_pool_ledger import BlsKeyRegisterPoolLedger
 from plenum.bls.bls_store import BlsStore
 
+from crypto.bls.indy_crypto.bls_crypto_indy_crypto import BlsCryptoIndyCrypto, BlsGroupParamsLoaderIndyCrypto
+
 
 class BlsFactoryCharm(BlsFactory):
     def __init__(self, basedir=None, node_name=None, bls_store: BlsStore = None):
@@ -28,9 +30,34 @@ class BlsFactoryCharm(BlsFactory):
         return BlsCryptoCharm
 
     def _create_bls_crypto(self, sk, pk, group_params):
-        return BlsCryptoCharm(sk=sk,
-                              pk=pk,
-                              params=group_params)
+        return BlsCryptoCharm(sk=sk, pk=pk, params=group_params)
+
+    def _create_bls_key_register(self) -> BlsKeyRegister:
+        return BlsKeyRegisterPoolLedger()
+
+    def _create_bls_bft(self, bls_crypto, bls_crypto_registry) -> BlsBft:
+        return BlsBftPlenum(bls_crypto, bls_crypto_registry, self.node_name, self.bls_store)
+
+
+class BlsFactoryIndyCrypto(BlsFactory):
+    def __init__(self, basedir=None, node_name=None, bls_store: BlsStore = None):
+        self.basedir = basedir
+        self.node_name = node_name
+        self.bls_store = bls_store
+
+    def _create_group_params_loader(self) -> BlsGroupParamsLoader:
+        return BlsGroupParamsLoaderIndyCrypto()
+
+    def _create_key_manager(self, group_params) -> BlsKeyManager:
+        assert self.basedir
+        assert self.node_name
+        return BlsKeyManagerFile(self.basedir, self.node_name)
+
+    def _get_bls_crypto_class(self):
+        return BlsCryptoIndyCrypto
+
+    def _create_bls_crypto(self, sk, pk, group_params):
+        return BlsCryptoIndyCrypto(sk=sk, pk=pk, params=group_params)
 
     def _create_bls_key_register(self) -> BlsKeyRegister:
         return BlsKeyRegisterPoolLedger()
@@ -47,4 +74,5 @@ def create_default_bls_factory(basedir=None, node_name=None, bls_store: BlsStore
     :param node_name: [optional] node's name; needed to save/load bls keys
     :return: BLS factory instance
     '''
-    return BlsFactoryCharm(basedir, node_name, bls_store)
+    # return BlsFactoryCharm(basedir, node_name, bls_store)
+    return BlsFactoryIndyCrypto(basedir, node_name, bls_store)
