8a334f450798b1654af4cb01c3ae894f4c53ac76
Sergey Shilov <sergey.shilov@dsr-company.com>
2017-09-21 17:39:06 +0300

    Remove trailing whitespace.

--- plenum/config.py
+++ plenum/config.py
@@ -33,7 +33,7 @@ LOG_DIR = '/var/log/plenum/'
 GENERAL_CONFIG_DIR = '/etc/plenum/'
 # walletDir = 'wallet'
 
-# it should be filled from baseConfig 
+# it should be filled from baseConfig
 NETWORK_NAME = ''
 
 GENERAL_CONFIG_FILE = 'plenum_config.py'
