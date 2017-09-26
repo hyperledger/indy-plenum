2ab940e7d88f860b1d7c19b4e5d76676ba613194
Victor Muzychenko <mzkvct@gmail.com>
2017-09-26 13:19:12 +0300

    create mostCommonElementWithFreq

--- plenum/common/util.py
+++ plenum/common/util.py
@@ -72,6 +72,7 @@ def randomSeed(size=32):
     return randomString(size)
 
 
+# TODO: this can be replaced by mostCommonElementWithFreq
 def mostCommonElement(elements: Iterable[T]) -> T:
     """
     Find the most frequent element of a collection.
@@ -79,7 +80,18 @@ def mostCommonElement(elements: Iterable[T]) -> T:
     :param elements: An iterable of elements
     :return: element of type T which is most frequent in the collection
     """
-    return Counter(elements).most_common(1)[0][0]
+    return mostCommonElementWithFreq(elements)[0]
+
+
+def mostCommonElementWithFreq(elements: Iterable[T]) -> T:
+    """
+    Find the most frequent element of a collection.
+
+    :param elements: An iterable of elements
+    :return: element of type T which is most frequent in the collection and
+        number of occurrences
+    """
+    return Counter(elements).most_common(1)[0]
 
 
 def updateNamedTuple(tupleToUpdate: NamedTuple, **kwargs):
