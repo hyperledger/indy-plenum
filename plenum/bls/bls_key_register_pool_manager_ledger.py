647babba270ea3a7884d8eb285de3b3f7aec37a4
Victor Muzychenko <mzkvct@gmail.com>
2017-09-26 19:52:11 +0300

    rake ledger instead of pool_manager in BlsKeyRegisterPoolManagerLedger

--- plenum/bls/bls_key_register_pool_manager_ledger.py
+++ plenum/bls/bls_key_register_pool_manager_ledger.py
@@ -1,6 +1,5 @@
 from crypto.bls.bls_key_register import BlsKeyRegister
 from plenum.common.constants import BLS_KEY, TXN_TYPE, NODE, ALIAS, DATA
-from plenum.server.pool_manager import TxnPoolManager
 
 
 class BlsKeyRegisterPoolManagerLedger(BlsKeyRegister):
@@ -8,11 +7,13 @@ class BlsKeyRegisterPoolManagerLedger(BlsKeyRegister):
     Ledger-based implementation of  BlsKeyRegister
     """
 
-    def __init__(self, pool_manager: TxnPoolManager):
-        self._pool_manager = pool_manager
+    def __init__(self, ledger):
+        self._ledger = ledger
         self._current_bls_keys = None  # {node_name : BLS key}
         # Not supported methods
-        self.get_pool_root_hash_committed = None
+
+    def get_pool_root_hash_committed(self):
+        raise NotImplementedError()
 
     def get_key_by_name(self, node_name, pool_state_root_hash=None):
         if self._current_bls_keys is None:
@@ -21,7 +22,7 @@ class BlsKeyRegisterPoolManagerLedger(BlsKeyRegister):
 
     def _load_keys_for_root(self):
         keys = {}
-        for _, txn in self._pool_manager.ledger.getAllTxn():
+        for _, txn in self._ledger.getAllTxn():
             if txn[TXN_TYPE] == NODE:
                 data = txn[DATA]
                 alias = data[ALIAS]
