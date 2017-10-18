0dd229a38fc6e9a932a74165acbc17fd50ea5743
Andrey Kononykhin <andkononykhin@gmail.com>
2017-10-18 18:09:35 +0300

    fixed flake8 errors
    Signed-off-by: Andrey Kononykhin <andkononykhin@gmail.com>

--- plenum/test/primary_selection/test_primary_selection_after_demoted_node_promotion.py
+++ plenum/test/primary_selection/test_primary_selection_after_demoted_node_promotion.py
@@ -23,12 +23,14 @@ logger = getlogger()
 def shortViewChangeTimeout():
     config.VIEW_CHANGE_TIMEOUT = 5
 
+
 def check_all_nodes_the_same_pool_list(nodes):
     allNodeNames = sorted([n.name for n in nodes])
     for node in nodes:
         _allNodeNames = sorted(node.allNodeNames)
         assert _allNodeNames == allNodeNames
         assert sorted(node.nodeReg.keys()) == _allNodeNames
+
 
 def test_primary_selection_after_demoted_node_promotion(
         shortViewChangeTimeout, looper, txnPoolNodeSet, nodeThetaAdded,
@@ -41,7 +43,7 @@ def test_primary_selection_after_demoted_node_promotion(
 
     nodeThetaSteward, nodeThetaStewardWallet, nodeTheta = nodeThetaAdded
 
-    #viewNo0 = checkViewNoForNodes(txnPoolNodeSet)
+    # viewNo0 = checkViewNoForNodes(txnPoolNodeSet)
     check_all_nodes_the_same_pool_list(txnPoolNodeSet)
 
     logger.info("1. Demote node Theta")
@@ -59,7 +61,7 @@ def test_primary_selection_after_demoted_node_promotion(
     sendReqsToNodesAndVerifySuffReplies(looper, nodeThetaStewardWallet,
                                         nodeThetaSteward, numReqs=3)
     # TODO view change might happen unexpectedly by unknown reason
-    #checkViewNoForNodes(remainingNodes, expectedViewNo=viewNo0)
+    # checkViewNoForNodes(remainingNodes, expectedViewNo=viewNo0)
 
     logger.info("2. Promote node Theta back")
 
@@ -74,7 +76,7 @@ def test_primary_selection_after_demoted_node_promotion(
     # ensure pool is working properly
     sendReqsToNodesAndVerifySuffReplies(looper, nodeThetaStewardWallet,
                                         nodeThetaSteward, numReqs=3)
-    #checkViewNoForNodes(txnPoolNodeSet, expectedViewNo=viewNo0)
+    # checkViewNoForNodes(txnPoolNodeSet, expectedViewNo=viewNo0)
 
     # TODO don't know who are primaries because of possible view changes
     logger.info("3. Restart one node")
@@ -87,17 +89,17 @@ def test_primary_selection_after_demoted_node_promotion(
     # ensure pool is working properly
     sendReqsToNodesAndVerifySuffReplies(looper, nodeThetaStewardWallet,
                                         nodeThetaSteward, numReqs=3)
-    #checkViewNoForNodes(remainingNodes, expectedViewNo=viewNo0)
+    # checkViewNoForNodes(remainingNodes, expectedViewNo=viewNo0)
 
     # start node
     restartedNode = start_stopped_node(stopped_node, looper, tconf,
-                                        tdirWithPoolTxns, allPluginsPath)
+                                       tdirWithPoolTxns, allPluginsPath)
     txnPoolNodeSet = remainingNodes + [restartedNode]
     ensure_all_nodes_have_same_data(looper, nodes=txnPoolNodeSet)
     # ensure pool is working properly
     sendReqsToNodesAndVerifySuffReplies(looper, nodeThetaStewardWallet,
                                         nodeThetaSteward, numReqs=3)
-    #checkViewNoForNodes(txnPoolNodeSet, expectedViewNo=viewNo0)
+    # checkViewNoForNodes(txnPoolNodeSet, expectedViewNo=viewNo0)
 
     logger.info("4. Do view changes to check that nodeTheta will be chosen "
                 "as a primary for any instance by all nodes after some rounds")
