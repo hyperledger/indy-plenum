c50b7644735f063d0c4e33a381bb2955f35b790f
Victor Muzychenko <mzkvct@gmail.com>
2017-10-19 13:30:15 +0300

    update test for large catchups
    Signed-off-by: Victor Muzychenko <mzkvct@gmail.com>

--- plenum/test/node_catchup/test_large_catchup.py
+++ plenum/test/node_catchup/test_large_catchup.py
@@ -1,21 +1,42 @@
 import pytest
 
+from plenum.common.messages.node_messages import CatchupRep
 from plenum.test.pool_transactions.helper import \
-    disconnect_node_and_ensure_disconnected
+    disconnect_node_and_ensure_disconnected, \
+    reconnect_node_and_ensure_connected
 from plenum.test.helper import sendReqsToNodesAndVerifySuffReplies
-from plenum.test.node_catchup.helper import waitNodeDataEquality, \
-    checkNodeDataForEquality, ensureClientConnectedToNodesAndPoolLedgerSame
+from plenum.test.node_catchup.helper import waitNodeDataEquality
 
-from plenum.test.pool_transactions.conftest import stewardAndWallet1, \
-    steward1, stewardWallet, clientAndWallet1, client1, wallet1, \
-    client1Connected
-from plenum.test.test_client import genTestClient
+from plenum.test.pool_transactions.conftest import \
+    client1, wallet1, client1Connected
 from plenum.test.test_node import checkNodesConnected
+from stp_core.validators.message_length_validator import MessageLenValidator
 
 
-def test_large_catchup(looper, txnPoolNodeSet, poolTxnClient):
+def fake(node):
+    old = node.nodestack.prepare_for_sending
+
+    def prepare_for_sending(msg, signer, message_splitter=lambda x: None):
+        if isinstance(msg, CatchupRep):
+            node.nodestack.prepare_for_sending = old
+            part_bytes = node.nodestack.sign_and_serialize(msg, signer)
+            new_limit = len(part_bytes) / 2
+            node.nodestack.msg_len_val = MessageLenValidator(new_limit)
+        return old(msg, signer, message_splitter)
+
+    node.nodestack.prepare_for_sending = prepare_for_sending
+
+
+def test_large_catchup(looper,
+                       txnPoolNodeSet,
+                       wallet1,
+                       client1,
+                       client1Connected,
+                       tconf,
+                       allPluginsPath,
+                       tdirWithPoolTxns):
     """
-    Checks that node can catchup large ledger
+    Checks that node can catchup large ledgers
     """
     # Prepare nodes
     lagging_node = txnPoolNodeSet[0]
@@ -24,24 +45,30 @@ def test_large_catchup(looper, txnPoolNodeSet, poolTxnClient):
     looper.run(checkNodesConnected(txnPoolNodeSet))
 
     # Prepare client
-    client, wallet = poolTxnClient
-    looper.add(client)
-    ensureClientConnectedToNodesAndPoolLedgerSame(looper, client, *all_nodes)
+    client, wallet = client1, wallet1
     looper.run(client.ensureConnectedToNodes())
 
     # Check that requests executed well
-    sendReqsToNodesAndVerifySuffReplies(looper, wallet, client, 10)
-    
-    # checkNodeDataForEquality(lagging_node, *txnPoolNodeSet[:-1])
-    #
-    # disconnect_node_and_ensure_disconnected(looper,
-    #                                         txnPoolNodeSet,
-    #                                         lagging_node,
-    #                                         stopNode=False)
-    # looper.removeProdable(lagging_node)
-    #
-    # looper.add(lagging_node)
-    # waitNodeDataEquality(looper, lagging_node, *txnPoolNodeSet[:-1])
-    #
-    # sendReqsToNodesAndVerifySuffReplies(looper, wallet, client, 10)
-    # checkNodeDataForEquality(lagging_node, *txnPoolNodeSet[:-1])
+    sendReqsToNodesAndVerifySuffReplies(looper, wallet, client, numReqs=10)
+
+    # Stop one node
+    waitNodeDataEquality(looper, lagging_node, *rest_nodes)
+    disconnect_node_and_ensure_disconnected(looper,
+                                            rest_nodes,
+                                            lagging_node,
+                                            stopNode=True)
+    looper.removeProdable(lagging_node)
+
+    # Send more requests to active nodes
+    sendReqsToNodesAndVerifySuffReplies(looper, wallet, client, numReqs=100)
+    waitNodeDataEquality(looper, *rest_nodes)
+
+    # Make message size limit smaller to ensure that catchup response is
+    # larger exceeds the limit
+    for node in rest_nodes:
+        fake(node)
+
+    # Restart stopped node and wait for successful catch up
+    looper.add(lagging_node)
+    reconnect_node_and_ensure_connected(looper, all_nodes, lagging_node)
+    waitNodeDataEquality(looper, *all_nodes)
