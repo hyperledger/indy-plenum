55629622bb9f9049ccbca539ea25ba3910777493
Andrey Kononykhin <andkononykhin@gmail.com>
2017-10-20 15:23:39 +0300

    added logging to test
    Signed-off-by: Andrey Kononykhin <andkononykhin@gmail.com>

--- plenum/test/client/test_client_sends_get_request_to_one_node.py
+++ plenum/test/client/test_client_sends_get_request_to_one_node.py
@@ -1,5 +1,6 @@
 import random
 
+from stp_core.common.log import getlogger
 from stp_core.loop.eventually import eventually
 
 from plenum.test.client.conftest import passThroughReqAcked1
@@ -10,11 +11,11 @@ from plenum.test.malicious_behaviors_client import \
     genDoesntSendRequestToSomeNodes
 from plenum.test.node_catchup.helper import waitNodeDataEquality
 
+logger = getlogger()
 
 nodeCount = 4
 clientFault = genDoesntSendRequestToSomeNodes("AlphaC")
 reqAcked1 = passThroughReqAcked1
-
 
 def test_client_sends_get_request_to_one_node(looper,
                                               client1,
@@ -27,6 +28,7 @@ def test_client_sends_get_request_to_one_node(looper,
     client = client1
     wallet = wallet1
 
+    logger.info("Stopping nodes")
     nodes_to_stop = list(nodeSet)[1:]
     stopNodes(nodes_to_stop, looper)
 
@@ -37,17 +39,20 @@ def test_client_sends_get_request_to_one_node(looper,
     def check_client_disconnected():
         assert not client.hasSufficientConnections
 
+    logger.info("Ensure that client has disconnected from the stopped nodes")
     # TODO non-default timeout
     looper.run(eventually(check_client_disconnected, retryWait=1))
 
     initial_submit_count = client.spylog.count(client.submitReqs)
     initial_send_count = client.spylog.count(client.send)
 
+    logger.info("Send set request")
     buy = {'type': 'buy', 'amount': random.randint(10, 100)}
     sign_and_send(buy)
     assert initial_submit_count + 1 == client.spylog.count(client.submitReqs)
     assert initial_send_count == client.spylog.count(client.send)
 
+    logger.info("Send get request")
     get_buy = {'type': 'get_buy'}
     client._read_only_requests.add('get_buy')
     sign_and_send(get_buy)
