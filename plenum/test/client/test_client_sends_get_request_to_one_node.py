989ec5f8c21dd0968f65c04c076d05e0f5e6918f
Victor Muzychenko <mzkvct@gmail.com>
2017-10-26 13:56:06 +0300

    swap tests to avoid negative influence
    Signed-off-by: Victor Muzychenko <mzkvct@gmail.com>

--- plenum/test/client/test_client_sends_get_request_to_one_node.py
+++ plenum/test/client/test_client_sends_get_request_to_one_node.py
@@ -15,6 +15,38 @@ clientFault = genDoesntSendRequestToSomeNodes("AlphaC")
 reqAcked1 = passThroughReqAcked1
 
 
+def test_client_sends_get_request_to_one_node(looper,
+                                              client1,
+                                              wallet1,
+                                              nodeSet):
+    """
+    Check that client sends read only request to one node only
+    """
+    client = client1
+    wallet = wallet1
+
+    def sign_and_send(op):
+        signed = wallet.signOp(op)
+        send_signed_requests(client, [signed])
+
+    logger.info("Send set request")
+    buy = {'type': 'buy', 'amount': random.randint(10, 100)}
+    sign_and_send(buy)
+
+    send_args = getAllArgs(client, client.send)
+    rids = send_args[0]['rids']
+    assert len(rids) > 1
+
+    logger.info("Send get request")
+    get_buy = {'type': 'get_buy'}
+    client._read_only_requests.add('get_buy')
+    sign_and_send(get_buy)
+
+    send_args = getAllArgs(client, client.send)
+    rids = send_args[0]['rids']
+    assert len(rids) == 1
+
+
 def test_client_can_send_get_request_to_one_node(looper,
                                                  client1,
                                                  wallet1,
@@ -56,35 +88,3 @@ def test_client_can_send_get_request_to_one_node(looper,
     sign_and_send(get_buy)
     assert initial_submit_count + 2 == client.spylog.count(client.submitReqs)
     assert initial_send_count + 1 == client.spylog.count(client.send)
-
-
-def test_client_sends_get_request_to_one_node(looper,
-                                              client1,
-                                              wallet1,
-                                              nodeSet):
-    """
-    Check that client sends read only request to one node only
-    """
-    client = client1
-    wallet = wallet1
-
-    def sign_and_send(op):
-        signed = wallet.signOp(op)
-        send_signed_requests(client, [signed])
-
-    logger.info("Send set request")
-    buy = {'type': 'buy', 'amount': random.randint(10, 100)}
-    sign_and_send(buy)
-
-    send_args = getAllArgs(client, client.send)
-    rids = send_args[0]['rids']
-    assert len(rids) > 1
-
-    logger.info("Send get request")
-    get_buy = {'type': 'get_buy'}
-    client._read_only_requests.add('get_buy')
-    sign_and_send(get_buy)
-
-    send_args = getAllArgs(client, client.send)
-    rids = send_args[0]['rids']
-    assert len(rids) == 1
