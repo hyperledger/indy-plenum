a2715e920e747cc7d3084c8b089d31008c45359a
Victor Muzychenko <mzkvct@gmail.com>
2017-10-30 19:14:57 +0300

    use nodes_with_bls to disable bls in test
    Signed-off-by: Victor Muzychenko <mzkvct@gmail.com>

--- plenum/test/client/test_client_resends_not_confirmed_request.py
+++ plenum/test/client/test_client_resends_not_confirmed_request.py
@@ -1,12 +1,8 @@
 import random
 
-from plenum.common.constants import REPLY, OP_FIELD_NAME
-from plenum.common.types import f
-from plenum.test.spy_helpers import getAllArgs
 from stp_core.common.log import getlogger
-from stp_core.loop.eventually import eventually
 from plenum.test.client.conftest import passThroughReqAcked1
-from plenum.test.helper import stopNodes, send_signed_requests, \
+from plenum.test.helper import send_signed_requests, \
     waitForSufficientRepliesForRequests
 from plenum.test.malicious_behaviors_client import \
     genDoesntSendRequestToSomeNodes
@@ -16,27 +12,7 @@ logger = getlogger()
 nodeCount = 4
 clientFault = genDoesntSendRequestToSomeNodes("AlphaC")
 reqAcked1 = passThroughReqAcked1
-
-
-def fake_send(client):
-    old_sendToNodes = client.sendToNodes
-
-    def sendToNodes(msg, names):
-        client.sendToNodes = old_sendToNodes
-        assert len(names) == 1
-        name = names[0]
-        old_got_expected = client._got_expected
-
-        def _got_expected(msg, sender):
-            if sender == name and msg[OP_FIELD_NAME] == REPLY:
-                client._got_expected = old_got_expected
-                msg[f.RESULT.nm].pop("state_proof", None)
-            return old_got_expected(msg, sender)
-
-        client._got_expected = _got_expected
-        return old_sendToNodes(msg, names)
-
-    client.sendToNodes = sendToNodes
+nodes_with_bls = 0
 
 
 def test_client_resends_not_confirmed_request(looper,
@@ -61,7 +37,6 @@ def test_client_resends_not_confirmed_request(looper,
     requests = sign_and_send(buy)
     waitForSufficientRepliesForRequests(looper, client, requests=requests)
 
-    fake_send(client)
     buy = {'type': 'get_buy'}
     client._read_only_requests.add('get_buy')
     requests = sign_and_send(buy)
