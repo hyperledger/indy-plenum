b4343993c978d870413c0bbfab7df8226bfdd093
Lovesh Harchandani <lovesh@users.noreply.github.com>
2017-11-07 14:32:56 +0530

    support hostname in test nodes bootstrap (#441)
    Signed-off-by: Lovesh <lovesh.harchandani@evernym.com>
    
    removing commented code
    
    removing commented code

--- plenum/common/util.py
+++ plenum/common/util.py
@@ -10,6 +10,7 @@ import logging
 import math
 import os
 import random
+import re
 import time
 from binascii import unhexlify, hexlify
 from collections import Counter, defaultdict
@@ -505,6 +506,17 @@ def is_network_ip_address_valid(ip_address):
         return False
     else:
         return True
+
+
+def is_hostname_valid(hostname):
+    # Taken from https://stackoverflow.com/a/2532344
+    if len(hostname) > 255:
+        return False
+    if hostname[-1] == ".":
+        hostname = hostname[:-1]    # strip exactly one dot from the right,
+        # if present
+    allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+    return all(allowed.match(x) for x in hostname.split("."))
 
 
 def check_endpoint_valid(endpoint):
--- plenum/test/script/test_bootstrap_test_node.py
+++ plenum/test/script/test_bootstrap_test_node.py
@@ -1,3 +1,7 @@
+from argparse import ArgumentTypeError
+
+import pytest
+
 from plenum.common.test_network_setup import TestNetworkSetup
 from plenum.common.txn_util import getTxnOrderedFields
 from plenum.common.util import randomString
@@ -5,18 +9,58 @@ from plenum.common.util import randomString
 portsStart = 9600
 
 
-def testBootstrapTestNode(tconf):
-    # TODO: Need to add some asserts
+@pytest.fixture(scope='module')
+def params(tconf):
     steward_defs, node_defs = TestNetworkSetup.gen_defs(
         ips=None, nodeCount=4, starting_port=portsStart)
 
     client_defs = TestNetworkSetup.gen_client_defs(clientCount=1)
     trustee_def = TestNetworkSetup.gen_trustee_def(1)
     nodeParamsFile = randomString()
+    return steward_defs, node_defs, client_defs, trustee_def, nodeParamsFile
 
+
+def testBootstrapTestNode(params, tconf):
+    # TODO: Need to add some asserts
+
+    steward_defs, node_defs, client_defs, trustee_def, nodeParamsFile = params
     TestNetworkSetup.bootstrapTestNodesCore(
         config=tconf, envName="test", appendToLedgers=False,
         domainTxnFieldOrder=getTxnOrderedFields(),
         trustee_def=trustee_def, steward_defs=steward_defs,
         node_defs=node_defs, client_defs=client_defs, localNodes=1,
         nodeParamsFileName=nodeParamsFile)
+
+
+def test_check_valid_ip_host(params, tconf):
+    _, _, client_defs, trustee_def, nodeParamsFile = params
+
+    valid = [
+        '34.200.79.65,52.38.24.189',
+        'ec2-54-173-9-185.compute-1.amazonaws.com,ec2-52-38-24-189.compute-1.amazonaws.com',
+        'ec2-54-173-9-185.compute-1.amazonaws.com,52.38.24.189,34.200.79.65',
+        '52.38.24.189,ec2-54-173-9-185.compute-1.amazonaws.com,34.200.79.65',
+        'ledger.net,ledger.net'
+    ]
+
+    invalid = [
+        '34.200.79()3.65,52.38.24.189',
+        '52.38.24.189,ec2-54-173$-9-185.compute-1.amazonaws.com,34.200.79.65',
+        '52.38.24.189,ec2-54-173-9-185.com$pute-1.amazonaws.com,34.200.79.65',
+        '52.38.24.189,ec2-54-173-9-185.com&pute-1.amazonaws.com,34.200.79.65',
+        '52.38.24.189,ec2-54-173-9-185.com*pute-1.amazonaws.com,34.200.79.65',
+    ]
+    for v in valid:
+        assert v.split(',') == TestNetworkSetup._bootstrap_args_type_ips_hosts(v)
+        steward_defs, node_defs = TestNetworkSetup.gen_defs(
+            ips=None, nodeCount=2, starting_port=portsStart)
+        TestNetworkSetup.bootstrapTestNodesCore(
+            config=tconf, envName="test", appendToLedgers=False,
+            domainTxnFieldOrder=getTxnOrderedFields(),
+            trustee_def=trustee_def, steward_defs=steward_defs,
+            node_defs=node_defs, client_defs=client_defs, localNodes=1,
+            nodeParamsFileName=nodeParamsFile)
+
+    for v in invalid:
+        with pytest.raises(ArgumentTypeError):
+            TestNetworkSetup._bootstrap_args_type_ips_hosts(v)
