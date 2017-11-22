b48bed22a0186b2b610ba47c2ff8dc40df3e36dc
Andrey Kononykhin <andkononykhin@gmail.com>
2017-11-22 21:10:12 +0300

    added missed script
    Signed-off-by: Andrey Kononykhin <andkononykhin@gmail.com>

---
+++ ci/build_base_image.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+set -x
+
+# TODO should be deprecated once base images are pushed to dockerhub
+
+# should be called from the root of the repo
+
+BASE_IMAGE=hyperledger/indy-core-baseci
+INDY_NODE_DIR=indy_node
+
+# TODO fix that before merge
+INDY_NODE_REPO_URL=https://github.com/andkononykhin/indy-node.git
+INDY_NODE_REPO_BRANCH=feature/ci-docker
+
+
+build_from_dockerfiles() {
+    mkdir $INDY_NODE_DIR
+
+    pushd $INDY_NODE_DIR
+    git init
+    git remote add --no-tags origin $INDY_NODE_REPO_URL
+    git config core.sparseCheckout true
+    echo "docker-files/baseimage" >> .git/info/sparse-checkout
+    git fetch --no-tags origin $INDY_NODE_REPO_BRANCH
+    git checkout $INDY_NODE_REPO_BRANCH
+    make -C docker-files/baseimage clean all
+    popd
+
+    rm -rf $INDY_NODE_DIR
+}
+
+docker pull "$BASE_IMAGE" || build_from_dockerfiles
