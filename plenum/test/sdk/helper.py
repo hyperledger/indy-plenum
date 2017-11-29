2421888d3e4218ae80e9d2d60a64a44967fc5660
Dmitry Surnin <dmitry.surnin@dsr-company.com>
2017-11-29 19:23:20 +0300

    Add funct to send and wait; move checkpoint tests to sdk
    Signed-off-by: Dmitry Surnin <dmitry.surnin@dsr-company.com>

--- plenum/test/sdk/helper.py
+++ plenum/test/sdk/helper.py
@@ -1,9 +1,9 @@
-from plenum.test.sdk.conftest import sdk_send_signed_requests, sdk_get_replies
+from plenum.test.sdk.conftest import sdk_send_signed_requests, sdk_get_replies, sdk_send_random_requests
 from plenum.test.batching_3pc.helper import checkNodesHaveSameRoots
 from plenum.test import waits
 
 
-def send_and_check(signed_reqs, looper, txnPoolNodeSet, pool_h, timeout = None):
+def send_and_check(signed_reqs, looper, txnPoolNodeSet, pool_h, timeout=None):
     if not timeout:
         timeout_per_request = waits.expectedTransactionExecutionTime(len(txnPoolNodeSet))
         # here we try to take into account what timeout for execution
@@ -17,3 +17,22 @@ def send_and_check(signed_reqs, looper, txnPoolNodeSet, pool_h, timeout = None):
     results = sdk_send_signed_requests(pool_h, signed_reqs)
     sdk_get_replies(looper, results, timeout=timeout)
     checkNodesHaveSameRoots(txnPoolNodeSet)
+
+
+def send_random_and_check(looper, txnPoolNodeSet, sdk_pool, sdk_wallet, count,
+                          customTimeoutPerReq: float = None, add_delay_to_timeout: float = 0,
+                          override_timeout_limit=False, total_timeout=None):
+    if not total_timeout:
+        node_count = len(txnPoolNodeSet)
+        timeout_per_request = customTimeoutPerReq or waits.expectedTransactionExecutionTime(node_count)
+        timeout_per_request += add_delay_to_timeout
+        # here we try to take into account what timeout for execution
+        # N request - total_timeout should be in
+        # timeout_per_request < total_timeout < timeout_per_request * N
+        # we cannot just take (timeout_per_request * N) because it is so huge.
+        # (for timeout_per_request=5 and N=10, total_timeout=50sec)
+        # lets start with some simple formula:
+        total_timeout = (1 + count / 10) * timeout_per_request
+
+    sdk_reqs = sdk_send_random_requests(looper, sdk_pool, sdk_wallet, count)
+    sdk_get_replies(looper, sdk_reqs, timeout=total_timeout)
