c85c225b46df1c9a511bfbb3d3107cb74d6be68f
Nikita Spivachuk <nikita.spivachuk@dsr-company.com>
2018-01-18 16:30:18 +0000

    INDY-1081: Migrated test to old client
    - Migrated test_pool_reaches_quorum_after_more_than_f_plus_1_nodes_turned_off_and_later_on from Indy SDK to the old client because a necessary fix in Python wrapper of libindy is not in its stable version yet.
    
    Signed-off-by: Nikita Spivachuk <nikita.spivachuk@dsr-company.com>

--- plenum/test/node_request/test_quorum_reconnected.py
+++ plenum/test/node_request/test_quorum_reconnected.py
@@ -1,11 +1,11 @@
 import pytest
 
-from plenum.common.request import Request
 from plenum.test import waits
-from plenum.test.helper import waitForViewChange, sdk_send_random_and_check, \
-    sdk_send_random_request, sdk_eval_timeout, sdk_get_reply, sdk_check_reply, \
-    check_request_is_not_returned_to_nodes, checkViewNoForNodes
-from plenum.test.pool_transactions.conftest import looper
+from plenum.test.helper import waitForViewChange, \
+    check_request_is_not_returned_to_nodes, checkViewNoForNodes, \
+    sendRandomRequest, waitForSufficientRepliesForRequests
+from plenum.test.pool_transactions.conftest import looper, clientAndWallet1, \
+    client1, wallet1, client1Connected
 from plenum.test.pool_transactions.helper import \
     disconnect_node_and_ensure_disconnected
 from plenum.test.test_node import ensureElectionsDone, getRequiredInstances
@@ -19,69 +19,62 @@ def stop_node(node_to_stop, looper, pool_nodes):
     looper.removeProdable(node_to_stop)
 
 
-def ensure_request_not_ordered_and_not_replied(sdk_req_resp, looper, nodes):
-    sdk_req_reply = sdk_get_reply(looper, sdk_req_resp,
-                              timeout=sdk_eval_timeout(1, len(nodes)))
-    with pytest.raises(AssertionError,
-                       match=r'Got no confirmed result for request.*'):
-        sdk_check_reply(sdk_req_reply)
-    req, _ = sdk_req_resp
-    check_request_is_not_returned_to_nodes(looper, nodes, Request(req))
-
-
-def test_pool_reaches_quorum_after_more_than_f_plus_1_nodes_were_off_and_later_on(
+def test_pool_reaches_quorum_after_more_than_f_plus_1_nodes_turned_off_and_later_on(
         looper, allPluginsPath, tdir, tconf,
-        txnPoolNodeSet, sdk_pool_handle, sdk_wallet_client):
+        txnPoolNodeSet, wallet1, client1, client1Connected):
 
     nodes = txnPoolNodeSet
     initial_view_no = nodes[0].viewNo
 
-    sdk_send_random_and_check(looper, nodes,
-                              sdk_pool_handle, sdk_wallet_client, 1)
+    request = sendRandomRequest(wallet1, client1)
+    waitForSufficientRepliesForRequests(looper, client1, requests=[request])
 
     stop_node(nodes[0], looper, nodes)
     waitForViewChange(looper, nodes[1:], expectedViewNo=initial_view_no + 1)
     ensureElectionsDone(looper, nodes[1:],
                         numInstances=getRequiredInstances(nodeCount))
 
-    sdk_send_random_and_check(looper, nodes,
-                              sdk_pool_handle, sdk_wallet_client, 1)
+    request = sendRandomRequest(wallet1, client1)
+    waitForSufficientRepliesForRequests(looper, client1, requests=[request])
 
     stop_node(nodes[1], looper, nodes)
     looper.runFor(tconf.ToleratePrimaryDisconnection + 2)
     checkViewNoForNodes(nodes[2:], initial_view_no + 1)
 
-    sdk_req_resp = sdk_send_random_request(looper, sdk_pool_handle,
-                                           sdk_wallet_client)
-    ensure_request_not_ordered_and_not_replied(sdk_req_resp, looper, nodes)
+    request = sendRandomRequest(wallet1, client1)
+    with pytest.raises(AssertionError):
+        waitForSufficientRepliesForRequests(looper, client1, requests=[request])
+    check_request_is_not_returned_to_nodes(looper, nodes, request)
 
     stop_node(nodes[2], looper, nodes)
     looper.runFor(tconf.ToleratePrimaryDisconnection + 2)
     checkViewNoForNodes(nodes[3:], initial_view_no + 1)
 
-    sdk_req_resp = sdk_send_random_request(looper, sdk_pool_handle,
-                                           sdk_wallet_client)
-    ensure_request_not_ordered_and_not_replied(sdk_req_resp, looper, nodes)
+    request = sendRandomRequest(wallet1, client1)
+    with pytest.raises(AssertionError):
+        waitForSufficientRepliesForRequests(looper, client1, requests=[request])
+    check_request_is_not_returned_to_nodes(looper, nodes, request)
 
     nodes[2] = start_stopped_node(nodes[2], looper, tconf, tdir, allPluginsPath)
     looper.runFor(waits.expectedPoolElectionTimeout(len(nodes)))
 
-    sdk_req_resp = sdk_send_random_request(looper, sdk_pool_handle,
-                                           sdk_wallet_client)
-    ensure_request_not_ordered_and_not_replied(sdk_req_resp, looper, nodes)
+    request = sendRandomRequest(wallet1, client1)
+    with pytest.raises(AssertionError):
+        waitForSufficientRepliesForRequests(looper, client1, requests=[request])
+    check_request_is_not_returned_to_nodes(looper, nodes, request)
 
     nodes[1] = start_stopped_node(nodes[1], looper, tconf, tdir, allPluginsPath)
     ensureElectionsDone(looper, nodes[1:],
                         numInstances=getRequiredInstances(nodeCount))
     waitForViewChange(looper, nodes[1:], expectedViewNo=initial_view_no + 1)
 
-    sdk_send_random_and_check(looper, nodes,
-                              sdk_pool_handle, sdk_wallet_client, 1)
+    request = sendRandomRequest(wallet1, client1)
+    waitForSufficientRepliesForRequests(looper, client1, requests=[request])
 
     nodes[0] = start_stopped_node(nodes[0], looper, tconf, tdir, allPluginsPath)
     ensureElectionsDone(looper, nodes,
                         numInstances=getRequiredInstances(nodeCount))
     waitForViewChange(looper, nodes, expectedViewNo=initial_view_no + 1)
 
-    sdk_send_random_and_check(looper, nodes,
-                              sdk_pool_handle, sdk_wallet_client, 1)
+    request = sendRandomRequest(wallet1, client1)
+    waitForSufficientRepliesForRequests(looper, client1, requests=[request])
