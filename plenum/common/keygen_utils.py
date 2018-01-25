4ff0e486d9b9fa026477145fa6fd99ee0b4cc38f
anikitinDSR <andrew.nikitin@dsr-corporation.com>
2018-01-25 20:57:54 +0300

    [INDY-1072] Added comment which public and verkey would be initialized (#507)
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>

--- plenum/common/keygen_utils.py
+++ plenum/common/keygen_utils.py
@@ -40,7 +40,9 @@ def initNodeKeysForBothStacks(name, keys_dir, sigseed, *, use_bls=True, override
 
     node_stack_name = name
     client_stack_name = node_stack_name + CLIENT_STACK_SUFFIX
+    print("Init local keys for client-stack")
     initLocalKeys(client_stack_name, keys_dir, sigseed, use_bls=False, override=override)
+    print("Init local keys for node-stack")
     keys = initLocalKeys(node_stack_name, keys_dir, sigseed, use_bls=use_bls, override=override)
     return keys
 
