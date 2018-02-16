b65558c9a0cce5e8786e5d9ee8f92586bf4a2d9d
ashcherbakov <alexander.sherbakov@dsr-company.com>
2018-02-16 20:26:46 +0300

    improved test
    Signed-off-by: ashcherbakov <alexander.sherbakov@dsr-company.com>

--- plenum/test/checkpoints/test_watermarks_on_delayed_backup.py
+++ plenum/test/checkpoints/test_watermarks_on_delayed_backup.py
@@ -9,7 +9,7 @@ logger = getLogger()
 inst_id = 1
 
 CHK_FREQ = 5
-LOG_SIZE = 2 * CHK_FREQ
+LOG_SIZE = 4 * CHK_FREQ
 
 
 def test_watermarks_restored_after_stable(
@@ -19,34 +19,39 @@ def test_watermarks_restored_after_stable(
         sdk_pool_handle, sdk_wallet_client):
     """
     A backup replica doesn't participate in consensus, and hence doesn't update watermarks.
-    Then if gets a quorum of stable checkpoints, updates the watermarks,
-    and starts to participate oin consensus.
+    Then if it gets a quorum of stashed checkpoints (in fact Replica.STASHED_CHECKPOINTS_BEFORE_CATCHUP + 1 checkpoints
+    where we have a quorum of n-f-1 for each),
+    then it updates the watermarks,
+    and starts to participate in consensus.
     """
+
     # 1. patch backup replica on a non-primary node so that it doesn't participate
     # in consensus, so that watermarks are not updated on it.
     broken_replica, non_broken_replica = break_backup_replica(txnPoolNodeSet)
 
-    # 2. send the number of requests which is less than a stable checkpoint,
+    # 2. send the number of requests which is less than Replica.STASHED_CHECKPOINTS_BEFORE_CATCHUP + 1
+    # quorumed checkpoints,
     # but sufficient for one watermark change (on a non-broken replica).
     sdk_send_batches_of_random_and_check(looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_client,
                                          num_reqs=1 * 9, num_batches=9)
     assert broken_replica.last_ordered_3pc == (0, 0)
     assert broken_replica.h == 0
-    assert broken_replica.H == 10
+    assert broken_replica.H == 20
     assert non_broken_replica.last_ordered_3pc == (0, 9)
     assert non_broken_replica.h == 5
-    assert non_broken_replica.H == 15
+    assert non_broken_replica.H == 25
 
-    # 3. send requests to reach a stable checkpoint.
+    # 3. send requests to reach Replica.STASHED_CHECKPOINTS_BEFORE_CATCHUP + 1
+    # quorumed checkpoints.
     # The broken replica should correct watermarks.
     sdk_send_batches_of_random_and_check(looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_client,
                                          num_reqs=1, num_batches=1)
     assert broken_replica.last_ordered_3pc == (0, 0)
     assert broken_replica.h == 10
-    assert broken_replica.H == 20
+    assert broken_replica.H == 30
     assert non_broken_replica.last_ordered_3pc == (0, 10)
     assert non_broken_replica.h == 10
-    assert non_broken_replica.H == 20
+    assert non_broken_replica.H == 30
 
     # 4. Repair broken replica amd make sure that it p[articipates in consensus
     # (after watermarks were corrected).
@@ -55,10 +60,10 @@ def test_watermarks_restored_after_stable(
                                          num_reqs=7, num_batches=7)
     assert broken_replica.last_ordered_3pc == (0, 17)
     assert broken_replica.h == 15
-    assert broken_replica.H == 25
+    assert broken_replica.H == 35
     assert non_broken_replica.last_ordered_3pc == (0, 17)
     assert non_broken_replica.h == 15
-    assert non_broken_replica.H == 25
+    assert non_broken_replica.H == 35
 
 
 def break_backup_replica(txnPoolNodeSet):
