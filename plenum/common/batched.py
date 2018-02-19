390ad3deb086743fa75f4c37f58f72f17052bceb
dsurnin <dmitry.surnin@dsr-corporation.com>
2018-02-19 14:40:32 +0300

    Do not make batch if only one msg left
    Signed-off-by: dsurnin <dmitry.surnin@dsr-corporation.com>

--- plenum/common/batched.py
+++ plenum/common/batched.py
@@ -141,8 +141,11 @@ class Batched(MessageProcessor):
             del self.outBoxes[rid]
 
     def _make_batch(self, msgs):
-        batch = Batch(msgs, None)
-        serialized_batch = self.sign_and_serialize(batch)
+        if len(msgs) > 1:
+            batch = Batch(msgs, None)
+            serialized_batch = self.sign_and_serialize(batch)
+        else:
+            serialized_batch = msgs[0]
         return serialized_batch
 
     def _test_batch_len(self, batch_len):
