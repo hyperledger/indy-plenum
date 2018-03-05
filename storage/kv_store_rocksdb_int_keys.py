186f773baf686483cf93ba767c5322673cb1b0b5
Sergey Shilov <sergey.shilov@dsr-company.com>
2018-03-05 19:50:07 +0300

    Fix comparator of KeyValueStorageRocksdbIntKeys class.
    Signed-off-by: Sergey Shilov <sergey.shilov@dsr-company.com>

--- storage/kv_store_rocksdb_int_keys.py
+++ storage/kv_store_rocksdb_int_keys.py
@@ -6,12 +6,8 @@ except ImportError:
     print('Cannot import rocksdb, please install')
 
 
-class KeyValueStorageRocksdbIntKeys(KeyValueStorageRocksdb):
-    def __init__(self, db_dir, db_name, open=True):
-        super().__init__(db_dir, db_name, open)
-
-    @staticmethod
-    def compare(a, b):
+class IntegerComparator(rocksdb.IComparator):
+    def compare(self, a, b):
         a = int(a)
         b = int(b)
         if (a < b):
@@ -20,8 +16,16 @@ class KeyValueStorageRocksdbIntKeys(KeyValueStorageRocksdb):
             return 1
         return 0
 
+    def name(self):
+        return b'IntegerComparator'
+
+
+class KeyValueStorageRocksdbIntKeys(KeyValueStorageRocksdb):
+    def __init__(self, db_dir, db_name, open=True):
+        super().__init__(db_dir, db_name, open)
+
     def open(self):
         opts = rocksdb.Options()
         opts.create_if_missing = True
-        opts.comparator = self.compare
+        opts.comparator = IntegerComparator()
         self._db = rocksdb.DB(self._db_path, opts)
