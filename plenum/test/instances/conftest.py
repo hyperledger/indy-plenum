31f0126812c5f51f56f9f9ae97ff463defe16d7a
ArtObr <artemobruchnikov@gmail.com>
2018-03-12 16:22:56 +0300

    little import fix
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>

--- plenum/test/instances/conftest.py
+++ plenum/test/instances/conftest.py
@@ -1,2 +1,4 @@
 from plenum.test.pool_transactions.conftest import looper
-from plenum.test.node_request.conftest import *
+from plenum.test.node_request.conftest import committed1, \
+    prepared1, preprepared1, propagated1, reqAcked1, \
+    sent1, noRetryReq, faultyNodes
