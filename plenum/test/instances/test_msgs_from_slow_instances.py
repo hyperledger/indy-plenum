92aed51d42a77641b72c7b45f60b4da2d90830eb
ArtObr <artemobruchnikov@gmail.com>
2018-03-13 11:28:32 +0300

    Fix for test pass
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>

--- plenum/test/instances/test_msgs_from_slow_instances.py
+++ plenum/test/instances/test_msgs_from_slow_instances.py
@@ -7,7 +7,9 @@ from plenum.test.delayers import delayerMsgTuple
 from plenum.test.test_node import TestNode
 from plenum.test import waits
 from plenum.test.pool_transactions.conftest import looper
-from plenum.test.node_request.conftest import prepared1
+from plenum.test.node_request.conftest import committed1, \
+    prepared1, preprepared1, propagated1, reqAcked1, \
+    sent1, noRetryReq, faultyNodes
 
 nodeCount = 4
 
