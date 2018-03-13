2e55dcb2100c90b6f883d92b944b4fbdd3f6981f
ArtObr <artemobruchnikov@gmail.com>
2018-03-13 12:01:22 +0300

    [INDY-1001] Move plenum/test/instances tests to sdk (#563)
    * Initial test change
    
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>
    
    * Import fix
    
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>
    
    * little fix
    
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>
    
    * little import fix
    
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>
    
    * Division imports from conftest to different folders
    
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>
    
    * Fix for test pass
    
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>

--- plenum/test/instances/test_msgs_from_slow_instances.py
+++ plenum/test/instances/test_msgs_from_slow_instances.py
@@ -6,7 +6,10 @@ from plenum.server.replica import Replica
 from plenum.test.delayers import delayerMsgTuple
 from plenum.test.test_node import TestNode
 from plenum.test import waits
-
+from plenum.test.pool_transactions.conftest import looper
+from plenum.test.node_request.conftest import committed1, \
+    prepared1, preprepared1, propagated1, reqAcked1, \
+    sent1, noRetryReq, faultyNodes
 
 nodeCount = 4
 
@@ -14,19 +17,19 @@ faultyNodes = 1
 
 
 @pytest.fixture()
-def configNodeSet(nodeSet):
-    A, B, C, D = nodeSet.nodes.values()
+def configNodeSet(txnPoolNodeSet):
+    A, B, C, D = txnPoolNodeSet
     # Nodes C and D delay Commit request from node A for protocol instance 0
     for n in [C, D]:
         n.nodeIbStasher.delay(delayerMsgTuple(30,
                                               Commit,
                                               senderFilter=A.name,
                                               instFilter=0))
-    return nodeSet
+    return txnPoolNodeSet
 
 
 def testMsgFromInstanceDelay(configNodeSet, looper, prepared1):
-    A, B, C, D = configNodeSet.nodes.values()
+    A, B, C, D = configNodeSet
 
     def getCommits(node: TestNode, instId: int):
         replica = node.replicas[instId]  # type: Replica
@@ -41,6 +44,6 @@ def testMsgFromInstanceDelay(configNodeSet, looper, prepared1):
             assert len(commReqs) > 0
             assert Replica.generateName(A.name, 1) in commReqs[0][0]
 
-    numOfNodes = len(configNodeSet.nodes)
+    numOfNodes = len(configNodeSet)
     timeout = waits.expectedClientRequestPropagationTime(numOfNodes)
     looper.run(eventually(checkPresence, retryWait=.5, timeout=timeout))
