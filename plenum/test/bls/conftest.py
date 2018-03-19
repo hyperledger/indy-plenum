af5fc8ecd4e3eced0d15fd973999712169550708
ArtObr <artemobruchnikov@gmail.com>
2018-03-19 17:29:05 +0300

    [INDY-992] Move bls tests to sdk (#564)
    * Initial test integration
    
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>
    
    * Two more files done
    
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>
    
    * new test
    
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>
    
    * 4 more folders done
    
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>
    
    * two more files
    
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>
    
    * some changes
    
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>
    
    * More tests integrated. Problems: validator functions of client in test_state_proof; problem with wrong bls keys
    
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>
    
    * Script fix. bls validation fix
    
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>
    
    * little fix
    
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>
    
    * Fix for bls validation tests. Not done: update_incorrect test and state_proof test
    
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>
    
    * Only state_proof test left
    
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>
    
    * Tests integration done
    
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>
    
    * Import deletion. flake8 error fix.
    
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>
    
    * Review fixes
    
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>
    
    * Little fix
    
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>

--- plenum/test/bls/conftest.py
+++ plenum/test/bls/conftest.py
@@ -4,7 +4,7 @@ from crypto.bls.bls_multi_signature import MultiSignatureValue, MultiSignature
 from plenum.common.constants import DOMAIN_LEDGER_ID
 from plenum.common.util import get_utc_epoch
 from plenum.test.bls.helper import generate_state_root
-
+from plenum.test.pool_transactions.conftest import looper
 
 participants = ["Node1", "Node2", "Node3"]
 signature = "somefakesignaturesomefakesignaturesomefakesignature"
