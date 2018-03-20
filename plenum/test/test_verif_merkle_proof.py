5708f72d2d465c33b99ad5304f770b5d9858efaf
ArtObr <artemobruchnikov@gmail.com>
2018-03-20 13:15:17 +0300

    More tests integrated
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>

--- plenum/test/test_verif_merkle_proof.py
+++ plenum/test/test_verif_merkle_proof.py
@@ -12,7 +12,7 @@ def testMerkleProofForFirstLeaf(client1: TestClient, replied1):
 
 
 def testMerkleProofForNonFirstLeaf(
-        looper, nodeSet, wallet1, client1, replied1):
+        looper, txnPoolNodeSet, wallet1, client1, replied1):
     req2 = sendRandomRequest(wallet1, client1)
     waitForSufficientRepliesForRequests(looper, client1, requests=[req2])
     replies = client1.getRepliesFromAllNodes(*req2.key).values()
