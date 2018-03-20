39ae3d8613e5ee3f5f3ed1499ecc08a244453b1b
Sergey Shilov <sergey.shilov@dsr-company.com>
2018-03-20 17:59:18 +0300

    Implement get_equal_or_prev() functionality for KeyValueStorageRocksdbIntKeys.
    Signed-off-by: Sergey Shilov <sergey.shilov@dsr-company.com>

--- storage/kv_store_rocksdb_int_keys.py
+++ storage/kv_store_rocksdb_int_keys.py
@@ -29,3 +29,21 @@ class KeyValueStorageRocksdbIntKeys(KeyValueStorageRocksdb):
         opts.create_if_missing = True
         opts.comparator = IntegerComparator()
         self._db = rocksdb.DB(self._db_path, opts)
+
+    def get_equal_or_prev(self, key):
+        # return value can be:
+        #    None, if required key less then minimal key from DB
+        #    Equal by key if key exist in DB
+        #    Previous if key does not exist in Db, but there is key less than required
+
+        if isinstance(key, int):
+            key = str(key)
+        if isinstance(key, str):
+            key = key.encode()
+        iter = self._db.itervalues()
+        iter.seek_for_prev(key)
+        try:
+            value = next(iter)
+        except StopIteration:
+            value = None
+        return value
