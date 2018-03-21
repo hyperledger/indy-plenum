8b233bd305cbd2964eefdbb00ddd3527cb2f6c7f
ArtObr <artemobruchnikov@gmail.com>
2018-03-21 07:47:50 +0300

    Skips for merkle proof tests
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>

--- plenum/test/test_verif_merkle_proof.py
+++ plenum/test/test_verif_merkle_proof.py
@@ -6,11 +6,13 @@ from plenum.test.helper import waitForSufficientRepliesForRequests, \
 from plenum.test.test_client import TestClient
 
 
+@pytest.skip('Wait for decision')
 def testMerkleProofForFirstLeaf(client1: TestClient, replied1):
     replies = client1.getRepliesFromAllNodes(*replied1.key).values()
     assert Client.verifyMerkleProof(*replies)
 
 
+@pytest.skip('Wait for decision')
 def testMerkleProofForNonFirstLeaf(
         looper, txnPoolNodeSet, wallet1, client1, replied1):
     req2 = sendRandomRequest(wallet1, client1)
