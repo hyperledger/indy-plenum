e595923d2a574264d68e29327b74fa10cbdbd33c
Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
2018-03-28 11:01:53 +0300

    [INDY-1210] Added check, that pool works after view_change
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>

--- plenum/test/view_change/test_resend_instance_change_messages.py
+++ plenum/test/view_change/test_resend_instance_change_messages.py
@@ -1,12 +1,10 @@
 import pytest
 from plenum.test.pool_transactions.helper import disconnect_node_and_ensure_disconnected
 from plenum.test.helper import checkViewNoForNodes, sdk_send_random_and_check
-from plenum.test.test_node import checkNodesConnected
 from stp_core.loop.eventually import eventually
 from functools import partial
-from plenum.test.pool_transactions.helper import reconnect_node_and_ensure_connected
-from stp_core.common.log import getlogger
 from plenum.test.delayers import icDelay
+from plenum.test.node_catchup.helper import ensure_all_nodes_have_same_data
 
 nodeCount = 5
 
@@ -23,7 +21,11 @@ def check_count_connected_node(nodes, expected_count):
     assert set([n.connectedNodeCount for n in nodes]) == {expected_count}
 
 
-def test_resend_instance_change_messages(looper, txnPoolNodeSet, tconf):
+def test_resend_instance_change_messages(looper,
+                                         txnPoolNodeSet,
+                                         tconf,
+                                         sdk_wallet_steward,
+                                         sdk_pool_handle):
     primary_node = txnPoolNodeSet[0]
     old_view_no = checkViewNoForNodes(txnPoolNodeSet, 0)
     assert primary_node.master_replica.isPrimary
@@ -44,3 +46,5 @@ def test_resend_instance_change_messages(looper, txnPoolNodeSet, tconf):
     looper.runFor(tconf.INSTANCE_CHANGE_TIMEOUT)
     looper.run(eventually(partial(checkViewNoForNodes, txnPoolNodeSet, expectedViewNo=old_view_no + 1),
                           timeout=tconf.VIEW_CHANGE_TIMEOUT))
+    sdk_send_random_and_check(looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_steward, 5)
+    ensure_all_nodes_have_same_data(looper, txnPoolNodeSet)
