25787cd51e577f7ce7585acb44936bf485b48766
toktar <renata.toktar@dsr-corporation.com>
2018-04-10 13:52:01 +0300

    INDY-1173: Added implementation of abstract methods
    changes:
    -Added implementation of abstract methods LedgerRequestHandler to ConfigReqHandler
    
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- plenum/server/config_req_handler.py
+++ plenum/server/config_req_handler.py
@@ -1,6 +1,20 @@
+from plenum.common.request import Request
 from plenum.server.ledger_req_handler import LedgerRequestHandler
 
 
 class ConfigReqHandler(LedgerRequestHandler):
     def __init__(self, ledger, state):
         super().__init__(ledger, state)
+
+    def doStaticValidation(self, request: Request):
+        pass
+
+    def get_query_response(self, request):
+        pass
+
+    def validate(self, req: Request):
+        pass
+
+    def apply(self, req: Request, cons_time: int):
+        pass
+
