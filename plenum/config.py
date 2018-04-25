499b0dc798c1088ec0ebcef498b57fdc2f7edb24
toktar <renata.toktar@dsr-corporation.com>
2018-04-25 11:23:40 +0300

    INDY-1184: change TestRunningTimeLimitSec
    Changes:
     - timeout increase to analyse problem with tests
    
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- plenum/config.py
+++ plenum/config.py
@@ -147,7 +147,7 @@ enableStdOutLogging = True
 # OPTIONS RELATED TO TESTS
 
 # TODO test 60sec
-TestRunningTimeLimitSec = 100
+TestRunningTimeLimitSec = 400
 
 # Expected time for one stack to get connected to another
 ExpectedConnectTime = 3.3 if sys.platform == 'win32' else 2
