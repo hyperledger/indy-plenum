4a1ef9ac177bb85988c892ddd539212a69ccb03b
Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
2018-04-26 15:04:51 +0300

    [FIX 3rd parties] Remove dublicate again
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>

--- build-scripts/ubuntu-1604/build-3rd-parties.sh
+++ build-scripts/ubuntu-1604/build-3rd-parties.sh
@@ -71,8 +71,6 @@ build_from_pypi rlp 0.5.1
 build_from_pypi sha3 0.2.1
 build_from_pypi libnacl 1.6.1
 build_from_pypi six 1.11.0
-build_from_pypi pyzmq 16.0.2
-build_from_pypi intervaltree 2.1.0
 build_from_pypi portalocker 0.5.7
 build_from_pypi sortedcontainers 1.5.7
 build_from_pypi setuptools 38.5.2
