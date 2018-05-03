72a4ab204e06fffe7203e4559e302788fd19b166
ArtObr <artemobruchnikov@gmail.com>
2018-05-03 12:35:44 +0300

    INDY-1274: Change code according to review comments
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>

--- plenum/server/node.py
+++ plenum/server/node.py
@@ -260,7 +260,6 @@ class Node(HasActionQueue, Motor, Propagator, MessageProcessor, HasFileStorage,
                                notifierEventsEnabled=self.config.SpikeEventsEnabled)
 
         self.replicas = self.create_replicas()
-        self.replicas.set_default_handlers()
 
         # Any messages that are intended for protocol instances not created.
         # Helps in cases where a new protocol instance have been added by a
