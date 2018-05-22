d685cef573cc3872b540bf42e14a18627739bbb1
toktar <renata.toktar@dsr-corporation.com>
2018-05-22 12:37:07 +0300

    INDY-1303: change logic in test_view_change_in_different_time
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- plenum/test/view_change/test_view_change_with_different_prepared_certificate.py
+++ plenum/test/view_change/test_view_change_with_different_prepared_certificate.py
@@ -25,24 +25,10 @@ def test_view_change_with_different_prepared_certificate(txnPoolNodeSet, looper,
                                                          tconf,
                                                          viewNo,
                                                          monkeypatch):
-    """
-    One of the conditions to finish catch-up during view change is to have MAX_CATCHUPS_DONE_DURING_VIEW_CHANGE
-    rounds of catch-up without any new transactions caught up.
-    But this should not finish very quickly.
-    So, we should try to catch-up until MIN_TIMEOUT_CATCHUPS_DONE_DURING_VIEW_CHANGE.
-
-    In the test:
-    - Before starting view change, mock `has_ordered_till_last_prepared_certificate` so that it always returns False.
-    - This means that the only condition on how we can finish catch-up is by MIN_TIMEOUT_CATCHUPS_DONE_DURING_VIEW_CHANGE
-    timeout and having more than MAX_CATCHUPS_DONE_DURING_VIEW_CHANGE rounds of catch-up without new txns caught up.
-     - Check that view change is not finished until MIN_TIMEOUT_CATCHUPS_DONE_DURING_VIEW_CHANGE
-     - Check that view change is eventually finished after MIN_TIMEOUT_CATCHUPS_DONE_DURING_VIEW_CHANGE
-    """
 
     # 1. Send some txns
     sdk_send_random_and_check(looper, txnPoolNodeSet, sdk_pool_handle,
                               sdk_wallet_client, 4)
-    tryOrderCount = 0
 
     def start_view_change(commit: Commit):
         # if get_count(replica, replica._request_three_phase_msg) > 1:
@@ -59,32 +45,7 @@ def test_view_change_with_different_prepared_certificate(txnPoolNodeSet, looper,
 
     requests = sdk_send_random_requests(looper, sdk_pool_handle,
                                         sdk_wallet_client, 2)
-
-    # ensureElectionsDone(looper=looper, nodes=txnPoolNodeSet)
-    # for node in txnPoolNodeSet:
-    #     if node.name != fast_node:
-    #         replica = node.replicas[0]
-    #         monkeypatch.delattr(replica, "tryOrder")
     sdk_get_replies(looper, requests)
-    #
-    # # 4. check that it's not finished till
-    # # MIN_TIMEOUT_CATCHUPS_DONE_DURING_VIEW_CHANGE
-    # no_view_chanage_timeout = tconf.MIN_TIMEOUT_CATCHUPS_DONE_DURING_VIEW_CHANGE - 1
-    # with pytest.raises(EventuallyTimeoutException):
-    #     ensureElectionsDone(looper=looper, nodes=txnPoolNodeSet,
-    #                         customTimeout=no_view_chanage_timeout)
-    #
-    # # 5. make sure that view change is finished eventually
-    # # (it should be finished quite soon after we waited for MIN_TIMEOUT_CATCHUPS_DONE_DURING_VIEW_CHANGE)
-    # ensureElectionsDone(looper=looper, nodes=txnPoolNodeSet, customTimeout=2)
-    # waitForViewChange(looper=looper, txnPoolNodeSet=txnPoolNodeSet,
-    #                   expectedViewNo=expected_view_no)
-    # ensure_all_nodes_have_same_data(looper, nodes=txnPoolNodeSet)
-    #
-    # # 6. ensure that the pool is still functional.
-    # sdk_ensure_pool_functional(looper, txnPoolNodeSet,
-    #                            sdk_wallet_client,
-    #                            sdk_pool_handle)
 
 
 def tmp(prepare: Prepare, sender: str):
@@ -96,25 +57,41 @@ def test_view_change_in_different_time(txnPoolNodeSet, looper,
                                        sdk_wallet_client,
                                        tconf,
                                        monkeypatch):
+    """Test case:
+    given 4 nodes
+    disable normal view change to make tests deterministic
+    indefinitely delay receiving commit messages on all nodes
+    send some requests
+    wait until all nodes have same last prepare certificate
+    trigger view change on all nodes (using view_changer.on_master_degradation)
+    stop delaying commits on two nodes
+    wait until view change is complete
+    stop delaying commits on two other nodes
+    try ordering transactions
+    Expected result with correct view change: transactions should be ordered normally
+    Expected result with current view change: transactions won't be ordered because pool is in inconsistent state
+    """
 
-    view_no = 1
-    delay = 3
+    first_two_nodes = txnPoolNodeSet[:2]
+    other_two_nodes = txnPoolNodeSet[2:]
+    first_two_nodes_stashers = [n.nodeIbStasher for n in first_two_nodes]
+    other_two_nodes_stashers = [n.nodeIbStasher for n in other_two_nodes]
 
-    first_two_nodes = [n.nodeIbStasher for n in txnPoolNodeSet[:2]]
-    other_two_nodes = [n.nodeIbStasher for n in txnPoolNodeSet[2:]]
-
-
-    with delay_rules(first_two_nodes, cDelay()):
-        with delay_rules(other_two_nodes, cDelay()):
+    with delay_rules(first_two_nodes_stashers, cDelay()):
+        with delay_rules(other_two_nodes_stashers, cDelay()):
             requests = sdk_send_random_requests(looper, sdk_pool_handle,
-                                                sdk_wallet_client, 2)
+                                                sdk_wallet_client, 1)
 
             def prepare_certificate(nodes: [Node]):
-                prepare_cert_count = 0
                 for node in nodes:
                     replica = node.replicas[0]
+                    (last_pp_view_no, last_pp_seq_no) = replica.last_ordered_3pc
                     tmp = replica.last_prepared_certificate_in_view()
-                    assert tmp == (replica.viewNo,)
+                    assert tmp == (replica.viewNo, last_pp_seq_no + 1)
+
+            def view_change_done(nodes: [Node]):
+                            for node in nodes:
+                                assert node.viewNo == 1
 
             looper.run(eventually(prepare_certificate, txnPoolNodeSet,
                        retryWait=1, timeout=100))
@@ -123,23 +100,22 @@ def test_view_change_in_different_time(txnPoolNodeSet, looper,
                 node.view_changer.on_master_degradation()
 
             # Wait for view change done on other two nodes
-            ensureElectionsDone(looper=looper, nodes=other_two_nodes)
+            looper.run(eventually(view_change_done, other_two_nodes,
+                                  retryWait=1, timeout=100))
+
+ #           ensureElectionsDone(looper=looper, nodes=txnPoolNodeSet)
 
         # Wait for view change done one first two nodes
-        ensureElectionsDone(looper=looper, nodes=first_two_nodes)
+        looper.run(eventually(view_change_done, first_two_nodes,
+                              retryWait=1, timeout=100))
 
-    # Send requests, we should fail here :)
+#        ensureElectionsDone(looper=looper, nodes=txnPoolNodeSet)
+
     sdk_get_replies(looper, requests)
 
-
-    # for node in txnPoolNodeSet:
-    #     node.nodeIbStasher.delay(cDelay(delay))
-    #
-    # stashers = (n.nodeIbStasher for n in txnPoolNodeSet[2:3])
-    # delay_rules(stashers, icDelay())
-    #
-    # stashers = (n.nodeIbStasher for n in txnPoolNodeSet[0:1])
-    # delay_rules(stashers, cDelay())
+    # Send requests, we should fail here :)
+    sdk_send_random_and_check(looper, txnPoolNodeSet, sdk_pool_handle,
+                              sdk_wallet_client, 4)
 
 
 def test_delay_commits(txnPoolNodeSet, looper,
@@ -155,14 +131,9 @@ def test_delay_commits(txnPoolNodeSet, looper,
     for node in txnPoolNodeSet:
         node.nodeIbStasher.delay(cDelay(delay))
 
-    # sdk_send_random_and_check(looper, txnPoolNodeSet, sdk_pool_handle,
-    #                           sdk_wallet_client, 1, total_timeout=100,
-    #                           customTimeoutPerReq=100, add_delay_to_timeout=100)
-    # ensureElectionsDone(looper=looper, nodes=txnPoolNodeSet)
     requests = sdk_send_random_requests(looper, sdk_pool_handle,
                                         sdk_wallet_client, 2)
     for node in txnPoolNodeSet[0:2]:
         node.view_changer.sendInstanceChange(view_no)
 
-    tmp = sdk_get_replies(looper, requests)
-    print(tmp)
+    sdk_get_replies(looper, requests)
