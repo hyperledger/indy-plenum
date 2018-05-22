ffc2abd78e2e40b7c094dc802fe9dc8fbe0c332a
toktar <renata.toktar@dsr-corporation.com>
2018-05-22 13:47:38 +0300

    INDY-1303: add test_view_change_with_delay_commits_and_prepares
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- plenum/test/view_change/test_view_change_with_different_prepared_certificate.py
+++ plenum/test/view_change/test_view_change_with_different_prepared_certificate.py
@@ -3,10 +3,11 @@ from _ast import Dict, List
 from collections import Sequence
 
 import pytest
+import sys
 
 from plenum.common.messages.node_messages import Prepare, PrePrepare, Commit
 from plenum.server.node import Node
-from plenum.test.delayers import icDelay, cDelay
+from plenum.test.delayers import icDelay, cDelay, pDelay
 from plenum.test.helper import waitForViewChange, sdk_send_random_and_check, \
     sdk_send_random_requests, sdk_get_replies
 from plenum.test.node_request.helper import sdk_ensure_pool_functional
@@ -16,6 +17,7 @@ from plenum.test.stasher import delay_rules
 from plenum.test.test_node import ensureElectionsDone
 from stp_core.loop.eventually import eventually
 from stp_core.loop.exceptions import EventuallyTimeoutException
+
 fast_node = "Gamma"
 
 
@@ -25,7 +27,6 @@ def test_view_change_with_different_prepared_certificate(txnPoolNodeSet, looper,
                                                          tconf,
                                                          viewNo,
                                                          monkeypatch):
-
     # 1. Send some txns
     sdk_send_random_and_check(looper, txnPoolNodeSet, sdk_pool_handle,
                               sdk_wallet_client, 4)
@@ -82,19 +83,12 @@ def test_view_change_in_different_time(txnPoolNodeSet, looper,
             requests = sdk_send_random_requests(looper, sdk_pool_handle,
                                                 sdk_wallet_client, 1)
 
-            def prepare_certificate(nodes: [Node]):
-                for node in nodes:
-                    replica = node.replicas[0]
-                    (last_pp_view_no, last_pp_seq_no) = replica.last_ordered_3pc
-                    tmp = replica.last_prepared_certificate_in_view()
-                    assert tmp == (replica.viewNo, last_pp_seq_no + 1)
-
             def view_change_done(nodes: [Node]):
-                            for node in nodes:
-                                assert node.viewNo == 1
+                for node in nodes:
+                    assert node.viewNo == 1
 
             looper.run(eventually(prepare_certificate, txnPoolNodeSet,
-                       retryWait=1, timeout=100))
+                                  retryWait=1, timeout=100))
 
             for node in txnPoolNodeSet:
                 node.view_changer.on_master_degradation()
@@ -103,13 +97,13 @@ def test_view_change_in_different_time(txnPoolNodeSet, looper,
             looper.run(eventually(view_change_done, other_two_nodes,
                                   retryWait=1, timeout=100))
 
- #           ensureElectionsDone(looper=looper, nodes=txnPoolNodeSet)
+        #           ensureElectionsDone(looper=looper, nodes=txnPoolNodeSet)
 
         # Wait for view change done one first two nodes
         looper.run(eventually(view_change_done, first_two_nodes,
                               retryWait=1, timeout=100))
 
-#        ensureElectionsDone(looper=looper, nodes=txnPoolNodeSet)
+    #        ensureElectionsDone(looper=looper, nodes=txnPoolNodeSet)
 
     sdk_get_replies(looper, requests)
 
@@ -119,10 +113,10 @@ def test_view_change_in_different_time(txnPoolNodeSet, looper,
 
 
 def test_delay_commits(txnPoolNodeSet, looper,
-                                       sdk_pool_handle,
-                                       sdk_wallet_client,
-                                       tconf,
-                                       monkeypatch):
+                       sdk_pool_handle,
+                       sdk_wallet_client,
+                       tconf,
+                       monkeypatch):
     sdk_send_random_and_check(looper, txnPoolNodeSet, sdk_pool_handle,
                               sdk_wallet_client, 4)
 
@@ -137,3 +131,79 @@ def test_delay_commits(txnPoolNodeSet, looper,
         node.view_changer.sendInstanceChange(view_no)
 
     sdk_get_replies(looper, requests)
+
+
+def test_view_change_with_delay_commits_and_prepares(txnPoolNodeSet, looper,
+                                                     sdk_pool_handle,
+                                                     sdk_wallet_client,
+                                                     tconf):
+    '''
+    Test case:
+    given 4 nodes
+    increase view change timeout to exceed allowed test running time
+    disable normal view change to make tests deterministic
+    indefinitely delay receiving commit messages on all nodes
+    send some requests
+    wait until all nodes have same last prepare certificate
+    indefinitely delay receiving prepare messages on two nodes
+    send one more request
+    wait until other two nodes increase last prepare certificate
+    trigger view change on all nodes (using view_changer.on_master_degradation)
+    stop delaying commits
+    wait for view change done on all nodes
+    Expected result with correct view change: view change will be successfully finished
+    Expected result with current view change: view change will take maximum time allowed for view change, triggering test timeout
+    :return:
+    '''
+
+    # increase view change timeout to exceed allowed test running time
+    view_change_timeout = 5  # default=60
+    for node in txnPoolNodeSet:
+        node._view_change_timeout = view_change_timeout
+    # disable normal view change to make tests deterministic
+    tconf.unsafe.add("disable_view_change")
+
+    first_two_nodes = txnPoolNodeSet[:2]
+    other_two_nodes = txnPoolNodeSet[2:]
+    first_two_nodes_stashers = [n.nodeIbStasher for n in first_two_nodes]
+    nodes_stashers = [n.nodeIbStasher for n in txnPoolNodeSet]
+
+    # indefinitely delay receiving commit messages on all nodes
+    with delay_rules(nodes_stashers, cDelay(sys.maxsize)):
+        # send some requests
+        requests = sdk_send_random_requests(looper, sdk_pool_handle,
+                                            sdk_wallet_client, 1)
+
+        # wait until all nodes have same last prepare certificate
+        looper.run(eventually(prepare_certificate, txnPoolNodeSet,
+                              retryWait=1, timeout=100))
+
+        # indefinitely delay receiving prepare messages on two nodes
+        with delay_rules(first_two_nodes_stashers, pDelay(sys.maxsize)):
+            # send one more request
+            requests2 = sdk_send_random_requests(looper, sdk_pool_handle,
+                                                 sdk_wallet_client, 3)
+
+            # wait until other two nodes increase last prepare certificate
+            looper.run(eventually(prepare_certificate, other_two_nodes,
+                                  retryWait=1, timeout=100))
+
+            # trigger view change on all nodes (using view_changer.on_master_degradation)
+            for node in txnPoolNodeSet:
+                node.view_changer.on_master_degradation()
+            # stop delaying commits
+
+    # wait for view change done on all nodes
+    ensureElectionsDone(looper=looper,
+                        nodes=txnPoolNodeSet,
+                        customTimeout=view_change_timeout + 2)
+    sdk_get_replies(looper, requests)
+    sdk_get_replies(looper, requests2)
+
+
+def prepare_certificate(nodes: [Node]):
+    for node in nodes:
+        replica = node.replicas[0]
+        (last_pp_view_no, last_pp_seq_no) = replica.last_ordered_3pc
+        tmp = replica.last_prepared_certificate_in_view()
+        assert tmp == (replica.viewNo, last_pp_seq_no + 1)
