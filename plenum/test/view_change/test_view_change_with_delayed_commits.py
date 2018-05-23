fbbe8b637cc727ced88e07722f1e6ceb3c28f95c
Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>
2018-05-23 12:40:10 +0300

    INDY-1304: Refactored failing test case
    Signed-off-by: Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>

--- plenum/test/view_change/test_view_change_with_delayed_commits.py
+++ plenum/test/view_change/test_view_change_with_delayed_commits.py
@@ -1,73 +1,104 @@
 import pytest
 
+from plenum.common.util import max_3PC_key
 from plenum.test.delayers import cDelay
-from plenum.test.helper import sdk_send_random_requests, sdk_get_replies, sdk_send_random_and_check, logger
+from plenum.test.helper import sdk_send_random_and_check, sdk_send_random_request, sdk_get_reply, logger
 from plenum.test.stasher import delay_rules
 from stp_core.loop.eventually import eventually
 
 
 @pytest.fixture(scope="module")
 def tconf(tconf):
+    """
+    Patch config so that monitor won't start view change unexpectedly
+    """
     old_unsafe = tconf.unsafe
     tconf.unsafe.add("disable_view_change")
     yield tconf
     tconf.unsafe = old_unsafe
 
 
-def test_view_change_with_delayed_commits(txnPoolNodeSet, looper,
-                                          sdk_pool_handle,
-                                          sdk_wallet_client,
-                                          tconf):
+def last_prepared_certificate(nodes):
+    """
+    Find last prepared certificate in pool.
+    When we don't have any request ordered in new view last_prepared_certificate_in_view()
+    returns None, but in order to ease maths (like being able to use max_3PC_key, or calculating
+    next expected 3PC key) this value is replaced with (view_no, 0).
+    """
 
-    def last_prepared_certificate(nodes, num):
-        for n in nodes:
-            assert n.master_replica.last_prepared_certificate_in_view() == num
+    def patched_last_prepared_certificate(n):
+        result = n.master_replica.last_prepared_certificate_in_view()
+        if result is None:
+            result = (n.master_replica.viewNo, 0)
+        return result
 
-    def view_change_done(nodes, view_no):
-        for n in nodes:
-            assert n.master_replica.viewNo == view_no
-            assert n.master_replica.last_prepared_before_view_change is None
+    return max_3PC_key(patched_last_prepared_certificate(n) for n in nodes)
 
-    majority_nodes = txnPoolNodeSet[:3]
-    minority_nodes = txnPoolNodeSet[3:]
-    majority_stashers = [n.nodeIbStasher for n in majority_nodes]
-    minority_stashers = [n.nodeIbStasher for n in minority_nodes]
 
-    with delay_rules(majority_stashers, cDelay()):
-        with delay_rules(minority_stashers, cDelay()):
-            requests = sdk_send_random_requests(looper, sdk_pool_handle,
-                                                sdk_wallet_client, 1)
+def check_last_prepared_certificate(nodes, num):
+    # Check that last_prepared_certificate reaches some 3PC key on N-f nodes
+    assert sum(1 for n in nodes if n.master_replica.last_prepared_certificate_in_view() == num) >= 3
 
-            looper.run(eventually(last_prepared_certificate, txnPoolNodeSet, (0, 1)))
 
-            for n in txnPoolNodeSet:
+def check_view_change_done(nodes, view_no):
+    # Check that view change is done and view_no is not less than target
+    for n in nodes:
+        assert n.master_replica.viewNo >= view_no
+        assert n.master_replica.last_prepared_before_view_change is None
+
+
+def do_view_change_with_pending_request_and_one_fast_node(fast_node,
+                                                          nodes, looper, sdk_pool_handle, sdk_wallet_client):
+    """
+    Perform view change while processing request, with one node receiving commits much sooner than others.
+    With current implementation of view change this will result in corrupted state of fast node
+    """
+
+    fast_stasher = fast_node.nodeIbStasher
+
+    slow_nodes = [n for n in nodes if n != fast_node]
+    slow_stashers = [n.nodeIbStasher for n in slow_nodes]
+
+    # Get last prepared certificate in pool
+    lpc = last_prepared_certificate(nodes)
+    # Get pool current view no
+    view_no = lpc[0]
+
+    # Delay all COMMITs
+    with delay_rules(slow_stashers, cDelay()):
+        with delay_rules(fast_stasher, cDelay()):
+            # Send request
+            request = sdk_send_random_request(looper, sdk_pool_handle, sdk_wallet_client)
+
+            # Wait until this request is prepared on N-f nodes
+            looper.run(eventually(check_last_prepared_certificate, nodes, (lpc[0], lpc[1] + 1)))
+
+            # Trigger view change
+            for n in nodes:
                 n.view_changer.on_master_degradation()
 
-        # Now commits are processed on minority of nodes wait until view change is done
-        looper.run(eventually(view_change_done, txnPoolNodeSet, 1, timeout=60))
+        # Now commits are processed on fast node
+        # Wait until view change is complete
+        looper.run(eventually(check_view_change_done, nodes, view_no + 1, timeout=60))
 
-    sdk_get_replies(looper, requests)
+    # Finish request gracefully
+    sdk_get_reply(looper, request)
 
 
-    majority_nodes = txnPoolNodeSet[1:]
-    minority_nodes = txnPoolNodeSet[:1]
-    majority_stashers = [n.nodeIbStasher for n in majority_nodes]
-    minority_stashers = [n.nodeIbStasher for n in minority_nodes]
+def test_two_view_changes_with_delayed_commits(txnPoolNodeSet, looper,
+                                               sdk_pool_handle,
+                                               sdk_wallet_client,
+                                               tconf):
+    # Perform view change with Delta acting as fast node
+    # With current view change implementation its state will become different from other nodes
+    do_view_change_with_pending_request_and_one_fast_node(txnPoolNodeSet[3], txnPoolNodeSet,
+                                                          looper, sdk_pool_handle, sdk_wallet_client)
 
-    with delay_rules(majority_stashers, cDelay()):
-        with delay_rules(minority_stashers, cDelay()):
-            requests = sdk_send_random_requests(looper, sdk_pool_handle,
-                                                sdk_wallet_client, 1)
+    # Perform view change with Alpha acting as fast node
+    # With current view change implementation its state will become different from other nodes,
+    # resulting in pool losing consensus and failing to finish view change at all
+    do_view_change_with_pending_request_and_one_fast_node(txnPoolNodeSet[0], txnPoolNodeSet,
+                                                          looper, sdk_pool_handle, sdk_wallet_client)
 
-            looper.run(eventually(last_prepared_certificate, txnPoolNodeSet[:3], (1, 2)))
-
-            for n in txnPoolNodeSet:
-                n.view_changer.on_master_degradation()
-
-        # Now commits are processed on minority of nodes wait until view change is done
-        looper.run(eventually(view_change_done, txnPoolNodeSet, 2, timeout=60))
-
-    sdk_get_replies(looper, requests)
-
-    sdk_send_random_and_check(looper, txnPoolNodeSet, sdk_pool_handle,
-                              sdk_wallet_client, 4)
+    # Check that pool can write transactions
+    sdk_send_random_and_check(looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_client, 1)
