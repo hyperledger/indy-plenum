36ecfcaec042c2813b70507d6f9a3ab9d02abd40
Lovesh Harchandani <lovesh.bond@gmail.com>
2018-05-25 15:46:33 -0600

    move methods to new handler
    Signed-off-by: Lovesh Harchandani <lovesh.bond@gmail.com>

--- plenum/server/ledger_req_handler.py
+++ plenum/server/ledger_req_handler.py
@@ -46,16 +46,8 @@ class LedgerRequestHandler(RequestHandler, metaclass=ABCMeta):
         :return: list of committed transactions
         """
 
-        (seqNoStart, seqNoEnd), committedTxns = \
-            self.ledger.commitTxns(txnCount)
-        stateRoot = base58.b58decode(stateRoot.encode())
-        # Probably the following assertion fail should trigger catchup
-        assert self.ledger.root_hash == txnRoot, '{} {}'.format(
-            self.ledger.root_hash, txnRoot)
-        self.state.commit(rootHash=stateRoot)
-        if self.ts_store:
-            self.ts_store.set(ppTime, stateRoot)
-        return txnsWithSeqNo(seqNoStart, seqNoEnd, committedTxns)
+        return self._commit(self.ledger, self.state, txnCount, stateRoot,
+                            txnRoot, ppTime, ts_store=self.ts_store)
 
     def onBatchCreated(self, state_root):
         pass
@@ -77,6 +69,25 @@ class LedgerRequestHandler(RequestHandler, metaclass=ABCMeta):
     def transform_txn_for_ledger(txn):
         return txn
 
+    @staticmethod
+    def _commit(ledger, state, txnCount, stateRoot, txnRoot, ppTime, ts_store=None,
+                ignore_txn_root_check=False):
+        (seqNoStart, seqNoEnd), committedTxns = ledger.commitTxns(txnCount)
+        stateRoot = base58.b58decode(stateRoot.encode()) if isinstance(
+            stateRoot, str) else stateRoot
+        if not ignore_txn_root_check:
+            # Probably the following assertion fail should trigger catchup
+            assert ledger.root_hash == txnRoot, '{} {}'.format(ledger.root_hash,
+                                                               txnRoot)
+        state.commit(rootHash=stateRoot)
+        if ts_store:
+            ts_store.set(ppTime, stateRoot)
+        return txnsWithSeqNo(seqNoStart, seqNoEnd, committedTxns)
+
     @property
     def operation_types(self) -> set:
+        return self.write_types.union(self.query_types)
+
+    @property
+    def valid_txn_types(self) -> set:
         return self.write_types.union(self.query_types)
