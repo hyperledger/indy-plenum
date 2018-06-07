8b0b94afd3c427b50d12f2f8cc167c47d416808d
toktar <renata.toktar@dsr-corporation.com>
2018-06-07 18:01:01 +0300

    INDY-1370: fix fixtures in testOneNodeAltersAClientRequest()
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- plenum/test/signing/test_signing.py
+++ plenum/test/signing/test_signing.py
@@ -1,8 +1,9 @@
 import pytest
 import sys
 
-from plenum.test.delayers import ppDelay, ppgDelay
-from plenum.test.helper import sdk_json_to_request_object
+from plenum.test.delayers import ppDelay, ppgDelay, nom_delay, req_delay
+from plenum.test.helper import sdk_json_to_request_object, \
+    sdk_send_random_requests
 from stp_core.loop.eventually import eventually
 from plenum.common.exceptions import InsufficientCorrectSignatures
 from stp_core.common.log import getlogger
@@ -19,15 +20,6 @@ whitelist = ['doing nothing for now',
 
 
 @pytest.fixture(scope="module")
-def setup(txnPoolNodeSet):
-    alpha = txnPoolNodeSet[0]
-    alpha.nodeIbStasher.delay(ppgDelay(sys.maxsize))
-    pool_without_alpha = list(txnPoolNodeSet)
-    pool_without_alpha.remove(alpha)
-    return adict(goodNodes=pool_without_alpha)
-
-
-@pytest.fixture(scope="module")
 def evilAlpha(txnPoolNodeSet):
     makeNodeFaulty(txnPoolNodeSet[0], changesRequest)
 
@@ -37,30 +29,47 @@ faultyNodes = 1
 
 def testOneNodeAltersAClientRequest(looper,
                                     txnPoolNodeSet,
-                                    setup,
                                     evilAlpha,
-                                    sent1):
-    sent1 = sdk_json_to_request_object(sent1[0][0])
-    checkPropagated(looper, txnPoolNodeSet, sent1, faultyNodes)
+                                    sdk_pool_handle,
+                                    sdk_wallet_client):
+    """Malicious Alpha node sends incorrect propagate. This test check that
+    nodes raise InsufficientCorrectSignatures in validate this propagate"""
 
-    goodNodes = setup.goodNodes
+    alpha = txnPoolNodeSet[0]
+    goodNodes = list(txnPoolNodeSet)
+    goodNodes.remove(alpha)
+    # delay incoming client messages for good nodes by 250 milliseconds
+    # this gives Alpha a chance to send a propagate message
+    for n in goodNodes:  # type: TestNode
+        n.nodeIbStasher.delay(ppDelay(sys.maxsize))
+        n.nodeIbStasher.delay(req_delay(sys.maxsize))
+    pastNodes = []
+
+    request_couple_json = sdk_send_random_requests(looper, sdk_pool_handle,
+                                                   sdk_wallet_client, 1)
+    sent1 = sdk_json_to_request_object(request_couple_json[0][0])
+    checkPropagated(looper, txnPoolNodeSet, sent1, faultyNodes)
 
     def check():
         for node in goodNodes:
+            if node not in pastNodes:
+                # ensure the nodes are suspicious of Alpha
+                params = node.spylog.getLastParams(TestNode.reportSuspiciousNode)
+                frm = params["nodeName"]
+                reason = params["reason"]
+                assert frm == 'Alpha'
+                assert reason == InsufficientCorrectSignatures.reason.format(0, 1)
 
-            # ensure the nodes are suspicious of Alpha
-            params = node.spylog.getLastParams(TestNode.reportSuspiciousNode)
-            frm = params["nodeName"]
-            reason = params["reason"]
-            assert frm == 'Alpha'
-            assert reason == InsufficientCorrectSignatures.reason.format(0, 1)
+                # ensure Alpha's propagates were ignored by the other nodes
+                key = sent1.digest
+                props = node.requests[key].propagates
+                assert 'Alpha' not in props
+                for good in goodNodes:
+                    assert good.name in props
+                pastNodes.append(node)
 
-            # ensure Alpha's propagates were ignored by the other nodes
-            key = sent1.digest
-            props = node.requests[key].propagates
-            assert 'Alpha' not in props
-            for good in goodNodes:
-                assert good.name in props
+        for node in goodNodes:
+            node.nodeIbStasher.resetDelays()
 
 
 
