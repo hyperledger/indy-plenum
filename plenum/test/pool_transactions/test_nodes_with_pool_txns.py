d2037e678bfdfe22264f31ecabd3ce6404e08288
Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
2018-06-14 18:40:35 +0300

    [New SDK] Move to separate module
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>

--- plenum/test/pool_transactions/test_nodes_with_pool_txns.py
+++ plenum/test/pool_transactions/test_nodes_with_pool_txns.py
@@ -280,28 +280,3 @@ def testStewardCannotAddNodeWithInvalidHa(looper, tdir, tconf,
         assert 'invalid network ip address' in e._excinfo[1].args[0] or \
                'expected types' in e._excinfo[1].args[0] or \
                'network port out of the range' in e._excinfo[1].args[0]
-
-
-def test_add_node_with_not_unique_alias(looper,
-                                        tdir,
-                                        tconf,
-                                        sdk_pool_handle,
-                                        sdk_wallet_steward,
-                                        allPluginsPath):
-    new_node_name = "Alpha"
-    new_steward_wallet, steward_did = sdk_add_new_nym(looper,
-                                                      sdk_pool_handle,
-                                                      sdk_wallet_steward,
-                                                      alias="TEST_STEWARD1",
-                                                      role='STEWARD')
-    with pytest.raises(RequestRejectedException) as e:
-        sdk_add_new_node(looper,
-                         sdk_pool_handle,
-                         (new_steward_wallet, steward_did),
-                         new_node_name,
-                         tdir,
-                         tconf,
-                         allPluginsPath)
-    assert 'existing data has conflicts with request data' in \
-           e._excinfo[1].args[0]
-    sdk_pool_refresh(looper, sdk_pool_handle)
