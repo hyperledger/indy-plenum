bf26279794e781a70db686bee612ed9ed840596b
Andrey Kononykhin <andkononykhin@gmail.com>
2018-06-27 11:56:48 +0300

    fixes and improves metadata routine
    Signed-off-by: Andrey Kononykhin <andkononykhin@gmail.com>

--- __metadata__.py
+++
@@ -1,9 +0,0 @@
-"""
-plenum package metadata
-"""
-__version_info__ = (1, 4)
-__version__ = '.'.join(map(str, __version_info__))
-__author__ = "Hyperledger"
-__license__ = "Apache 2.0"
-
-__all__ = ['__version_info__', '__version__', '__author__', '__license__']
