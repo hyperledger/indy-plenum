02a30559ea512f1fc3b58dab2c612ad3b345bc0c
ashcherbakov <alexander.sherbakov@dsr-company.com>
2018-07-05 14:24:33 +0300

    improve tests
    Signed-off-by: ashcherbakov <alexander.sherbakov@dsr-company.com>

--- plenum/test/view_change/test_client_req_during_view_change.py
+++ plenum/test/view_change/test_client_req_during_view_change.py
@@ -8,6 +8,9 @@ def test_no_client_reqs_during_view_change(txnPoolNodeSet, looper,
                                            sdk_wallet_client, sdk_pool_handle):
     for node in txnPoolNodeSet:
         node.view_changer.view_change_in_progress = True
-
     with pytest.raises(PoolLedgerTimeoutException):
         sdk_send_random_and_check(looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_client, 10)
+
+    for node in txnPoolNodeSet:
+        node.view_changer.view_change_in_progress = False
+    sdk_send_random_and_check(looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_client, 10)
