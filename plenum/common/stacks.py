7f0ad018faef8f8832103960813055f993a1c1f9
Sergey Shilov <sergey.shilov@dsr-company.com>
2018-07-09 11:14:46 +0300

    INDY-1431: implement client stack restart on reached connections limit.
    Client stack is restarted if connections threshold is reached and
    specified minimal time (configurable, should be greater than TIME_WAIT)
    is spent since previous client stack restart to avoid sockets overhead
    in TIME_WAIT or FIN_WAIT state.
    
    Signed-off-by: Sergey Shilov <sergey.shilov@dsr-company.com>

--- plenum/common/stacks.py
+++ plenum/common/stacks.py
@@ -1,3 +1,7 @@
+import time
+import zmq
+
+from random import randint
 from typing import Callable, Any, List, Dict
 
 from plenum.common.batched import Batched, logger
@@ -10,7 +14,6 @@ from stp_core.common.constants import CONNECTION_PREFIX
 from stp_core.types import HA
 from stp_zmq.kit_zstack import KITZStack
 from stp_zmq.simple_zstack import SimpleZStack
-
 
 # conf_ = getConfigOnce()
 conf_ = get_global_config_else_read_config()
@@ -34,14 +37,70 @@ class ClientZStack(simple_zstack_class, MessageProcessor):
             seed=seed,
             onlyListener=True,
             config=config,
-            msgRejectHandler=msgRejectHandler)
+            msgRejectHandler=msgRejectHandler,
+            create_listener_monitor=True)
         MessageProcessor.__init__(self, allowDictOnly=False)
-        self.connectedClients = set()
+
+        self.max_connected_clients_num = config.MAX_CONNECTED_CLIENTS_NUM
+        self.postrestart_wait_time = config.STACK_POSTRESTART_WAIT_TIME
+        self.min_stack_restart_timeout = config.MIN_STACK_RESTART_TIMEOUT
+        self.max_stack_restart_time_deviation = config.MAX_STACK_RESTART_TIME_DEVIATION
+        self.init_stack_restart_params()
+
+    def init_stack_restart_params(self):
+        self.connected_clients_num = 0
+        self.stack_restart_is_needed = False
+        self.last_start_time = time.time()
+        self.next_restart_min_time = self.last_start_time + \
+            self.min_stack_restart_timeout + \
+            randint(0, self.max_stack_restart_time_deviation)
+
+    def check_listener_events(self):
+        events = self.get_monitor_events(self.listener_monitor)
+        for event in events:
+            logger.trace('{} listener event: {}'.format(self, event))
+            if event['event'] == zmq.EVENT_ACCEPTED:
+                self.connected_clients_num += 1
+            if event['event'] == zmq.EVENT_DISCONNECTED:
+                assert self.connected_clients_num > 0
+                self.connected_clients_num -= 1
+            logger.trace('{}: number of connected clients: {}'.format(self, self.connected_clients_num))
+
+    def restart(self):
+        logger.warning("Stopping client stack on node {}".format(self))
+        self.stop()
+        time.sleep(0.2)
+        logger.warning("Starting client stack on node {}".format(self))
+        self.start()
+        # Sleep to allow disconnected clients to reconnect before sending replies from the server side.
+        time.sleep(self.postrestart_wait_time)
+
+    def _can_restart(self):
+        return self.next_restart_min_time < time.time()
+
+    def check_for_stack_restart(self):
+        stack_restart_is_needed_prev = self.stack_restart_is_needed
+        self.stack_restart_is_needed = self.connected_clients_num > self.max_connected_clients_num
+        if self.stack_restart_is_needed:
+            if not stack_restart_is_needed_prev:
+                logger.warning('{}: connections limit exceeded! Actual: {}, limit: {}.'
+                               .format(self, self.connected_clients_num, self.max_connected_clients_num))
+            if self._can_restart():
+                logger.warning('Going to restart client stack {} due to exceeded connections limit! Actual: {}, limit: {}.'
+                               .format(self, self.connected_clients_num, self.max_connected_clients_num))
+                self.restart()
+                self.init_stack_restart_params()
+            elif not stack_restart_is_needed_prev:
+                logger.warning(
+                    '{}: connections limit exceeded but too few time spent since client stack start, restart it later.'
+                        .format(self))
+        elif stack_restart_is_needed_prev:
+            logger.warning('{}: client stack restart is not needed anymore. Connections: Actual: {}, limit: {}.'
+                           .format(self, self.connected_clients_num, self.max_connected_clients_num))
 
     def serviceClientStack(self):
-        newClients = self.connecteds - self.connectedClients
-        self.connectedClients = self.connecteds
-        return newClients
+        self.check_listener_events()
+        self.check_for_stack_restart()
 
     def newClientsConnected(self, newClients):
         raise NotImplementedError("{} must implement this method".format(self))
