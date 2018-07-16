6aeeb4d9eba7da40881be59af0c0a52cdb49af20
Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
2018-07-16 13:31:37 +0300

    [INDY-1462] tests refactoring
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>

--- plenum/test/replica/test_update_watermarks_api.py
+++ plenum/test/replica/test_update_watermarks_api.py
@@ -1,7 +1,9 @@
 import sys
 
+from plenum.test.replica.helper import emulate_catchup, emulate_select_primaries
 
-def test_is_Master_update_watermarks(replica):
+
+def test_propagate_primary_is_Master_update_watermarks(replica):
     # expected behaviour is that h must be set as last ordered ppSeqNo
     replica.isMaster = True
     replica.h = 0
@@ -11,7 +13,7 @@ def test_is_Master_update_watermarks(replica):
     assert replica.h == 500
 
 
-def test_is_Master_watermarks_not_changed_if_last_ordered_not_changed(replica):
+def test_propagate_primary_is_Master_watermarks_not_changed_if_last_ordered_not_changed(replica):
     replica.isMaster = True
     replica.h = 0
     assert replica.h == 0
@@ -19,7 +21,7 @@ def test_is_Master_watermarks_not_changed_if_last_ordered_not_changed(replica):
     assert replica.h == 0
 
 
-def test_not_Master_update_watermarks_to_maxsize(replica):
+def test_propagate_primary_not_Master_update_watermarks_to_maxsize(replica):
     # expected behaviour is that h must be set as last ordered ppSeqNo
     replica.isMaster = False
     replica.h = 0
@@ -30,7 +32,7 @@ def test_not_Master_update_watermarks_to_maxsize(replica):
     assert replica.H == sys.maxsize
 
 
-def test_not_Master_watermarks_not_changed_if_last_ordered_not_changed(replica):
+def test_propagate_primary_not_Master_watermarks_not_changed_if_last_ordered_not_changed(replica):
     replica.isMaster = False
     replica.h = 0
     assert replica.h == 0
@@ -38,7 +40,7 @@ def test_not_Master_watermarks_not_changed_if_last_ordered_not_changed(replica):
     assert replica.h == 0
 
 
-def test_non_Master_watermarks_not_maxsize_if_is_primary(replica, tconf):
+def test_propagate_primary_non_Master_watermarks_not_maxsize_if_is_primary(replica, tconf):
     replica.isMaster = False
     replica._primaryName = replica.name
     replica.h = 100
@@ -74,26 +76,34 @@ def test_reset_watermarks_before_new_view_non_master(replica, tconf):
 
 def test_catchup_without_vc_and_no_primary_on_master(replica, tconf):
     replica.isMaster = True
-    replica.isMaster = False
     replica._primaryName = None
-    # next 3 calls emulate a catchup without vc when there is no primary selected
+    # next calls emulate a catchup without vc when there is no primary selected
     # like it will be called for 'update watermark' procedure
-    replica.caught_up_till_3pc((replica.viewNo, 100))
-    assert replica.h == 100
-    assert replica.H == 100 + tconf.LOG_SIZE
+    emulate_catchup(replica)
+    # select_primaries after allLedgersCaughtUp
+    emulate_select_primaries(replica)
+    if replica.viewNo == 0:
+        assert replica.h == 100
+        assert replica.H == 100 + tconf.LOG_SIZE
+    else:
+        assert replica.h == 0
+        assert replica.H == tconf.LOG_SIZE
 
 
 def test_catchup_without_vc_and_no_primary_on_backup(replica, tconf):
     replica.isMaster = False
-    replica.isMaster = False
     replica._primaryName = None
-    # next 3 calls emulate a catchup without vc when there is no primary selected
+    # next calls emulate a catchup without vc when there is no primary selected
     # like it will be called for 'update watermark' procedure
-    replica.catchup_clear_for_backup()
-    replica._reset_watermarks_before_new_view()
-    replica._setup_for_non_master_after_view_change(replica.viewNo)
-    assert replica.h == 0
-    assert replica.H == tconf.LOG_SIZE
+    emulate_catchup(replica)
+    # select_primaries after allLedgersCaughtUp
+    emulate_select_primaries(replica)
+    if replica.viewNo > 0:
+        assert replica.h == 0
+        assert replica.H == tconf.LOG_SIZE
+    else:
+        assert replica.h == 0
+        assert replica.H == sys.maxsize
 
 
 def test_catchup_without_during_vc_with_primary_on_master(replica, tconf):
@@ -101,7 +111,7 @@ def test_catchup_without_during_vc_with_primary_on_master(replica, tconf):
     # (by checkpoints or ledger_statuses)
     replica._primaryName = 'SomeNode'
     replica.isMaster = True
-    replica.caught_up_till_3pc((replica.viewNo, 100))
+    emulate_catchup(replica)
     assert replica.last_ordered_3pc == (replica.viewNo, 100)
     assert replica.h == 100
     assert replica.H == 100 + tconf.LOG_SIZE
@@ -113,7 +123,7 @@ def test_catchup_without_during_vc_with_primary_on_backup(replica):
     # (by checkpoints or ledger_statuses)
     replica._primaryName = 'SomeNode'
     replica.isMaster = False
-    replica.catchup_clear_for_backup()
+    emulate_catchup(replica)
     assert replica.last_ordered_3pc == (replica.viewNo, 0)
     assert replica.h == 0
     assert replica.H == sys.maxsize
@@ -122,9 +132,9 @@ def test_catchup_without_during_vc_with_primary_on_backup(replica):
 def test_view_change_no_propagate_primary_on_master(replica, tconf):
     replica.isMaster = True
     replica._primaryName = 'SomeNode'
-    # next 3 calls emulate simple view_change procedure (replica's watermark related steps)
-    replica.caught_up_till_3pc((replica.viewNo, 100))
-    replica.primaryName = 'SomeOtherNode'
+    # next calls emulate simple view_change procedure (replica's watermark related steps)
+    emulate_catchup(replica)
+    emulate_select_primaries(replica)
     if replica.viewNo > 0:
         assert replica.h == 0
         assert replica.H == tconf.LOG_SIZE
@@ -136,20 +146,24 @@ def test_view_change_no_propagate_primary_on_master(replica, tconf):
 def test_view_change_no_propagate_primary_on_backup(replica, tconf):
     replica.isMaster = False
     replica._primaryName = 'SomeNode'
-    # next 3 calls emulate simple view_change procedure (replica's watermark related steps)
-    replica.catchup_clear_for_backup()
-    replica._reset_watermarks_before_new_view()
-    replica._setup_for_non_master_after_view_change(replica.viewNo)
-    assert replica.h == 0
-    assert replica.H == tconf.LOG_SIZE
+    # next calls emulate simple view_change procedure (replica's watermark related steps)
+    emulate_catchup(replica)
+    emulate_select_primaries(replica)
+    if replica.viewNo > 0:
+        assert replica.h == 0
+        assert replica.H == tconf.LOG_SIZE
+    else:
+        assert replica.h == 0
+        assert replica.H == sys.maxsize
 
 
 def test_view_change_propagate_primary_on_master(replica, tconf):
     replica.isMaster = True
     replica._primaryName = 'SomeNode'
-    # next 3 calls emulate view_change for propagate primary situation
+    # next calls emulate view_change for propagate primary situation
     # (when the new node join to the pool)
-    replica.caught_up_till_3pc((replica.viewNo, 100))
+    emulate_catchup(replica)
+    emulate_select_primaries(replica)
     replica.on_propagate_primary_done()
     assert replica.h == 100
     assert replica.H == 100 + tconf.LOG_SIZE
@@ -158,11 +172,10 @@ def test_view_change_propagate_primary_on_master(replica, tconf):
 def test_view_change_propagate_primary_on_backup(replica):
     replica.isMaster = False
     replica._primaryName = 'SomeNode'
-    # next 3 calls emulate view_change for propagate primary situation
+    # next calls emulate view_change for propagate primary situation
     # (when the new node join to the pool)
-    replica.catchup_clear_for_backup()
-    replica._reset_watermarks_before_new_view()
-    replica._setup_for_non_master_after_view_change(replica.viewNo)
+    emulate_catchup(replica)
+    emulate_select_primaries(replica)
     replica.on_propagate_primary_done()
     assert replica.h == 0
     assert replica.H == sys.maxsize
