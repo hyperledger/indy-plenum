c901a9a31301deb216079aa00e791d904512df4a
Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
2018-07-18 10:18:59 +0300

    [INDY-1470] Add fix for received instance_change from unknown node
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>

--- plenum/test/view_change/conftest.py
+++ plenum/test/view_change/conftest.py
@@ -1,6 +1,10 @@
 import pytest
 
+from plenum.common.util import get_utc_epoch
+from plenum.server.quorums import Quorums
+from plenum.server.view_change.view_changer import ViewChanger
 from plenum.test.conftest import getValueFromModule
+from plenum.test.testing_utils import FakeSomething
 
 
 @pytest.fixture()
@@ -22,3 +26,26 @@ def perf_chk_patched(tconf, request):
 
     request.addfinalizer(reset)
     return tconf
+
+
+@pytest.fixture(scope='function', params=[0, 10])
+def fake_view_changer(request, tconf):
+    node_stack = FakeSomething(
+        name="fake stack",
+        connecteds={"Alpha", "Beta", "Gamma", "Delta"}
+    )
+    monitor = FakeSomething(
+        isMasterDegraded=lambda: False,
+    )
+    node = FakeSomething(
+        name="SomeNode",
+        viewNo=request.param,
+        quorums=Quorums(getValueFromModule(request, 'nodeCount', default=4)),
+        nodestack=node_stack,
+        utc_epoch=lambda *args: get_utc_epoch(),
+        config=tconf,
+        monitor=monitor,
+        discard=lambda a, b, c: print(b),
+    )
+    view_changer = ViewChanger(node)
+    return view_changer
