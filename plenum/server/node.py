9117cf29589033c637f487666ba216a709c36c0a
Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
2018-07-25 15:27:22 +0300

    [INDY-1478] process case when CURRENT_STATE with viewNo=0
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>

--- plenum/server/node.py
+++ plenum/server/node.py
@@ -1521,7 +1521,7 @@ class Node(HasActionQueue, Motor, Propagator, MessageProcessor, HasFileStorage,
         if self.viewNo - view_no > 1:
             self.discard(msg, "un-acceptable viewNo {}"
                          .format(view_no), logMethod=logger.warning)
-        elif (view_no > self.viewNo):
+        elif (view_no > self.viewNo) or from_current_state:
             if view_no not in self.msgsForFutureViews:
                 self.msgsForFutureViews[view_no] = deque()
             logger.debug('{} stashing a message for a future view: {}'.format(self, msg))
