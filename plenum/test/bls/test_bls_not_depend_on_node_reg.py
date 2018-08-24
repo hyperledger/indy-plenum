64ecfb628f871408fe742062e676f0be2ea8ae02
ArtObr <artemobruchnikov@gmail.com>
2018-08-24 10:12:49 +0300

    INDY-1583: Fix for test
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>

--- plenum/test/bls/test_bls_not_depend_on_node_reg.py
+++ plenum/test/bls/test_bls_not_depend_on_node_reg.py
@@ -1,3 +1,9 @@
+from crypto.bls.bls_multi_signature import MultiSignature
+
+from common.serializers.base58_serializer import Base58Serializer
+
+from common.serializers.json_serializer import JsonSerializer
+
 from plenum.test.pool_transactions.helper import demote_node
 from plenum.test.test_node import TestNode, checkNodesConnected, ensureElectionsDone, ensure_node_disconnected
 from plenum.test.helper import sdk_send_batches_of_random_and_check
@@ -11,7 +17,7 @@ nodeCount = 7
 def test_bls_not_depend_on_node_reg(looper, txnPoolNodeSet,
                                     sdk_pool_handle, sdk_wallet_client):
     sdk_send_batches_of_random_and_check(looper, txnPoolNodeSet,
-                                         sdk_pool_handle, sdk_wallet_client, 1, 2)
+                                         sdk_pool_handle, sdk_wallet_client, 3, 3)
 
     node = txnPoolNodeSet[2]
     last_pre_prepare = \
@@ -33,7 +39,7 @@ def test_bls_not_depend_on_node_reg_demotion(looper, txnPoolNodeSet,
                                              sdk_pool_handle, sdk_wallet_client, tdir, tconf, allPluginsPath,
                                              sdk_wallet_stewards):
     sdk_send_batches_of_random_and_check(looper, txnPoolNodeSet,
-                                         sdk_pool_handle, sdk_wallet_client, 1, 2)
+                                         sdk_pool_handle, sdk_wallet_client, 3, 3)
 
     primary_node = txnPoolNodeSet[0]
     node_to_stop = txnPoolNodeSet[3]
@@ -42,8 +48,9 @@ def test_bls_not_depend_on_node_reg_demotion(looper, txnPoolNodeSet,
 
     last_pre_prepare = \
         node_to_stop.master_replica.prePrepares[node_to_stop.master_replica.last_ordered_3pc]
-    bls = getattr(last_pre_prepare, f.BLS_MULTI_SIG.nm)
-    state_root_hash = bls[0]
+    multi_sig = MultiSignature.from_list(*last_pre_prepare.blsMultiSig)
+    serializer = Base58Serializer()
+    state_root_hash = serializer.deserialize(multi_sig.value.pool_state_root_hash)
 
     node_to_stop.cleanupOnStopping = True
     node_to_stop.stop()
