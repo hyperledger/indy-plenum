699b49e0e21a3bd2706e7a2545851dce59bf4eec
Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>
2018-08-24 15:19:04 +0300

    INDY-1639: Improve moving average interface
    Signed-off-by: Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>

--- plenum/common/moving_average.py
+++ plenum/common/moving_average.py
@@ -7,6 +7,10 @@ class MovingAverage(ABC):
     def update(self, value: float):
         pass
 
+    @abstractmethod
+    def reset(self, value: float):
+        pass
+
     @property
     @abstractmethod
     def value(self) -> float:
@@ -14,12 +18,22 @@ class MovingAverage(ABC):
 
 
 class ExponentialMovingAverage(MovingAverage):
-    def __init__(self, start: float, alpha: float):
+    def __init__(self, alpha: float, start: float = 0.0):
         self._value = start
         self._alpha = alpha
 
+    def __eq__(self, other):
+        if not isinstance(other, ExponentialMovingAverage):
+            return False
+        if self._alpha != other._alpha:
+            return False
+        return self._value == other._value
+
     def update(self, value: float):
         self._value = value * self._alpha + self._value * (1 - self._alpha)
+
+    def reset(self, value: float):
+        self._value = value
 
     @property
     def value(self) -> float:
--- plenum/test/monitoring/test_moving_average.py
+++ plenum/test/monitoring/test_moving_average.py
@@ -1,6 +1,6 @@
 import pytest
 
-from plenum.common.moving_average import ExponentialMovingAverage
+from plenum.common.moving_average import MovingAverage, ExponentialMovingAverage
 
 
 @pytest.fixture(params=[ExponentialMovingAverage])
@@ -8,9 +8,9 @@ def ma_type(request):
     return request.param
 
 
-def create_moving_average(cls, start: float):
+def create_moving_average(cls, start: float) -> MovingAverage:
     if cls == ExponentialMovingAverage:
-        return cls(start, alpha=0.5)
+        return cls(alpha=0.5, start=start)
     return None
 
 
@@ -26,6 +26,29 @@ def test_moving_average_doesnt_change_value_when_fed_same_values(ma_type):
     for _ in range(10):
         ma.update(start)
     assert ma.value == start
+
+
+def test_moving_average_has_value_semantics_for_eq(ma_type):
+    start = 4.2
+    ma1 = create_moving_average(ma_type, start)
+    ma2 = create_moving_average(ma_type, start)
+    assert ma1 == ma2
+
+    ma1.update(42)
+    assert ma1 != ma2
+
+    ma2.update(42)
+    assert ma1 == ma2
+
+
+def test_moving_average_resets_to_same_state_as_new(ma_type):
+    ma = create_moving_average(ma_type, 1.3)
+    for _ in range(10):
+        ma.update(42)
+
+    start = 4.2
+    ma.reset(start)
+    assert ma == create_moving_average(ma_type, start)
 
 
 def test_moving_average_increases_value_when_updated_with_larger_value(ma_type):
@@ -67,8 +90,8 @@ def test_moving_average_changes_faster_with_larger_difference_to_target(ma_type)
 def test_exp_moving_average_converges_faster_with_larger_alpha():
     start = 4.2
     target = 42
-    ma1 = ExponentialMovingAverage(start, alpha=0.2)
-    ma2 = ExponentialMovingAverage(start, alpha=0.8)
+    ma1 = ExponentialMovingAverage(0.2, start)
+    ma2 = ExponentialMovingAverage(0.8, start)
 
     for _ in range(10):
         ma1.update(target)
@@ -83,7 +106,7 @@ def test_exp_moving_average_moves_halfway_to_target_in_desired_number_of_steps()
     start = 4.2
     target = 42
     halfway = 0.5 * (start + target)
-    ma = ExponentialMovingAverage(start, alpha)
+    ma = ExponentialMovingAverage(alpha, start)
 
     for i in range(steps - 1):
         ma.update(target)
