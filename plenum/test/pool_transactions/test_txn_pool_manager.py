0e52134fa6368ac40731f05604a35e6113f63f16
ArtObr <artemobruchnikov@gmail.com>
2018-09-21 15:57:09 +0300

    INDY-1506: Fix for test
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>

--- plenum/test/pool_transactions/test_txn_pool_manager.py
+++ plenum/test/pool_transactions/test_txn_pool_manager.py
@@ -24,41 +24,6 @@ def pool_node_txns(poolTxnData):
     return node_txns
 
 
-def test_get_nym_by_name(txnPoolNodeSet, pool_node_txns):
-    check_get_nym_by_name(txnPoolNodeSet, pool_node_txns)
-
-
-def test_get_nym_by_name_not_in_registry(txnPoolNodeSet, pool_node_txns):
-    nodes_to_remove = [txnPoolNodeSet[4].name, txnPoolNodeSet[5].name]
-    for node in txnPoolNodeSet:
-        for node_to_remove in nodes_to_remove:
-            del node.nodeReg[node_to_remove]
-            del node.cliNodeReg[node_to_remove + CLIENT_STACK_SUFFIX]
-    check_get_nym_by_name(txnPoolNodeSet, pool_node_txns)
-
-
-def test_get_nym_by_name_demoted(txnPoolNodeSet, pool_node_txns,
-                                 looper, sdk_wallet_steward, sdk_pool_handle):
-    # sdk_wallet_steward fixture is a steward for [0] node,
-    # so we can do things below:
-    demote_node(looper, sdk_wallet_steward, sdk_pool_handle,
-                txnPoolNodeSet[0])
-    check_get_nym_by_name(txnPoolNodeSet, pool_node_txns)
-
-
-def check_get_nym_by_name(txnPoolNodeSet, pool_node_txns):
-    for i in range(nodeCount):
-        node = txnPoolNodeSet[i]
-        pool_manager = node.poolManager
-        node_name = node.name
-
-        node_nym = pool_manager.get_nym_by_name(node_name)
-        expected_data = get_payload_data(pool_node_txns[i])[TARGET_NYM]
-
-        assert node_nym
-        assert node_nym == expected_data
-
-
 def test_twice_demoted_node_dont_write_txns(txnPoolNodeSet,
                                             looper, sdk_wallet_stewards, sdk_pool_handle):
     request_count = 5
@@ -78,9 +43,42 @@ def test_twice_demoted_node_dont_write_txns(txnPoolNodeSet,
 
     old = get_node_prods_count(txnPoolNodeSet[0])
 
-    # let primary node make 2 prod runs so we make sure that
-    #  node did not appear in network reconnection
+    # Let primary node make 2 prod runs so we make sure that
+    # node did not appear in network reconnection
     looper.run(eventually(is_prods_run, txnPoolNodeSet[0], old, 2))
 
     assert txnPoolNodeSet[0].domainLedger.size - request_count == \
            demoted_node.domainLedger.size
+
+
+def test_get_nym_by_name(txnPoolNodeSet, pool_node_txns):
+    check_get_nym_by_name(txnPoolNodeSet, pool_node_txns)
+
+
+def test_get_nym_by_name_not_in_registry(txnPoolNodeSet, pool_node_txns):
+    nodes_to_remove = [txnPoolNodeSet[4].name, txnPoolNodeSet[5].name]
+    for node in txnPoolNodeSet:
+        for node_to_remove in nodes_to_remove:
+            del node.nodeReg[node_to_remove]
+            del node.cliNodeReg[node_to_remove + CLIENT_STACK_SUFFIX]
+    check_get_nym_by_name(txnPoolNodeSet, pool_node_txns)
+
+
+def test_get_nym_by_name_demoted(txnPoolNodeSet, pool_node_txns,
+                                 looper, sdk_wallet_stewards, sdk_pool_handle):
+    demote_node(looper, sdk_wallet_stewards[0], sdk_pool_handle,
+                txnPoolNodeSet[0])
+    check_get_nym_by_name(txnPoolNodeSet, pool_node_txns)
+
+
+def check_get_nym_by_name(txnPoolNodeSet, pool_node_txns):
+    for i in range(nodeCount):
+        node = txnPoolNodeSet[i]
+        pool_manager = node.poolManager
+        node_name = node.name
+
+        node_nym = pool_manager.get_nym_by_name(node_name)
+        expected_data = get_payload_data(pool_node_txns[i])[TARGET_NYM]
+
+        assert node_nym
+        assert node_nym == expected_data
