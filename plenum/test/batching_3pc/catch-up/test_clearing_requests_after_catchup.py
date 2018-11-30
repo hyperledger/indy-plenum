af3ff327bdd2bda3044e2d7c21c46737d7ef32ec
ArtObr <artemobruchnikov@gmail.com>
2018-11-30 14:13:49 +0300

    INDY-1881: Comment fix
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>

--- plenum/test/batching_3pc/catch-up/test_clearing_requests_after_catchup.py
+++ plenum/test/batching_3pc/catch-up/test_clearing_requests_after_catchup.py
@@ -66,7 +66,7 @@ def test_free_non_finalized_catchuped_requests(
     node.ledgerManager.last_caught_up_3PC = (0, 1)
     node.allLedgersCaughtUp()
 
-    # Request freed and deleted from requestQueues
+    # Request deleted from requestQueues
     assert key not in node.requests
 
 
@@ -82,7 +82,7 @@ def test_dont_free_finalized_sended_requests_if_more_caught_up_3pc(
     node.replicas[0].prePrepares[(0, 2)] = FakeSomething(ledgerId=0, reqIdr=[key])
     node.replicas[0].requestQueues[ledger_id].add(key)
 
-    # Request freed and deleted from requestQueues
+    # Request not freed and not deleted from requestQueues
     assert not node.requests.get(key).executed
     assert key in node.replicas[0].requestQueues[ledger_id]
 
@@ -98,7 +98,7 @@ def test_free_finalized_non_sended_if_more_caught_up_3pc(
     node.requests.mark_as_forwarded(req, 3)
     node.replicas[0].requestQueues[ledger_id].add(key)
 
-    # Request freed and deleted from requestQueues
+    # Request not freed and not deleted from requestQueues
     assert not node.requests.get(key).executed
     assert key in node.replicas[0].requestQueues[ledger_id]
 
@@ -112,5 +112,5 @@ def test_free_non_finalized_which_isnt_ordered(
     key = req.key
     node.requests.add(req)
 
-    # Request freed and deleted from requestQueues
+    # Request not deleted from requestQueues
     assert key in node.requests
