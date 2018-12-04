2db5aa33152009d1f77f5c194aa24d663be0c183
Sergey Shilov <sergey.shilov@dsr-company.com>
2018-12-04 20:08:12 +0300

    Add test_req_drop_on_prepare_phase for non-primary.
    Signed-off-by: Sergey Shilov <sergey.shilov@dsr-company.com>

--- plenum/test/req_drop/test_req_drop_on_prepare_phase_non_primary.py
+++ plenum/test/req_drop/test_req_drop_on_prepare_phase_non_primary.py
@@ -12,6 +12,7 @@ from plenum.test.node_request.helper import sdk_ensure_pool_functional
 
 howlong = 20
 initial_ledger_size = 0
+logLevel = 0
 
 
 @pytest.fixture(scope="module")
@@ -37,39 +38,42 @@ def tconf(tconf):
 def setup(txnPoolNodeSet, looper, sdk_pool_handle, sdk_wallet_client):
     global initial_ledger_size
     A, B, C, D = txnPoolNodeSet  # type: TestNode
-    delay(Prepare, frm=[B, C, D], to=A, howlong=howlong)
-    delay(Commit, frm=[B, C, D], to=A, howlong=howlong + 3)
+    lagged_node = C
+    frm = [A, B, D]
+    delay(Prepare, frm=frm, to=lagged_node, howlong=howlong)
+    delay(Commit, frm=frm, to=lagged_node, howlong=howlong + 3)
     initial_ledger_size = txnPoolNodeSet[0].domainLedger.size
     request_couple_json = sdk_send_random_requests(
         looper, sdk_pool_handle, sdk_wallet_client, 1)
     return request_couple_json
 
 
-def test_req_drop_on_prepare_phase_on_master_primary_and_then_ordered(
+def test_req_drop_on_prepare_phase_on_non_primary_and_then_ordered(
         tconf, setup, looper, txnPoolNodeSet,
         sdk_wallet_client, sdk_pool_handle):
     global initial_ledger_size
     A, B, C, D = txnPoolNodeSet  # type: TestNode
+    lagged_node = C
 
     def check_propagates():
-        # A should have received a request from the client
-        assert len(recvdRequest(A)) == 1
-        # A should have received a PROPAGATEs
-        assert len(recvdPropagate(A)) == 3
-        # A should have sent a PROPAGATE
-        assert len(sentPropagate(A)) == 1
-        # A should have one request in the requests queue
-        assert len(A.requests) == 1
+        # Node should have received a request from the client
+        assert len(recvdRequest(lagged_node)) == 1
+        # Node should have received a PROPAGATEs
+        assert len(recvdPropagate(lagged_node)) == 3
+        # Node should have sent a PROPAGATE
+        assert len(sentPropagate(lagged_node)) == 1
+        # Node should have one request in the requests queue
+        assert len(lagged_node.requests) == 1
 
     timeout = howlong - 2
     looper.run(eventually(check_propagates, retryWait=.5, timeout=timeout))
 
     def check_drop():
-        # A should have not received Prepares and Commits for master instance
-        assert len(recvdPrepareForInstId(A, 0)) == 0
-        assert len(recvdCommitForInstId(A, 0)) == 0
+        # Node should have not received Prepares and Commits for master instance
+        assert len(recvdPrepareForInstId(lagged_node, 0)) == 0
+        assert len(recvdCommitForInstId(lagged_node, 0)) == 0
         # Request object should be dropped by timeout
-        assert len(A.requests) == 0
+        assert len(lagged_node.requests) == 0
 
     timeout = tconf.ORDERING_PHASE_REQ_TIMEOUT + tconf.OUTDATED_REQS_CHECK_INTERVAL + 1
     looper.run(eventually(check_drop, retryWait=.5, timeout=timeout))
@@ -79,16 +83,16 @@ def test_req_drop_on_prepare_phase_on_master_primary_and_then_ordered(
 
     def check_prepares_and_commits_received():
         # A should have received all delayed Prepares and Commits for master instance
-        assert len(recvdPrepareForInstId(A, 0)) == 3
-        assert len(recvdCommitForInstId(A, 0)) == 3
+        assert len(recvdPrepareForInstId(lagged_node, 0)) == 2
+        assert len(recvdCommitForInstId(lagged_node, 0)) == 3
 
     timeout = howlong * 2
     looper.run(eventually(check_prepares_and_commits_received, retryWait=.5, timeout=timeout))
 
     def check_ledger_size():
         # The request should be eventually ordered
-        for node in txnPoolNodeSet:
-            assert node.domainLedger.size - initial_ledger_size == 1
+        for n in txnPoolNodeSet:
+            assert n.domainLedger.size - initial_ledger_size == 1
 
     looper.run(eventually(check_ledger_size, retryWait=.5, timeout=timeout))
 
