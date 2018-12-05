d409e7b9685c716c67a6210048c90ff829452a1a
Sergey Shilov <sergey.shilov@dsr-company.com>
2018-12-05 14:06:57 +0300

    Add test_req_drop_on_preprepare_phase on PrePrepare phase.
    Signed-off-by: Sergey Shilov <sergey.shilov@dsr-company.com>

--- plenum/test/req_drop/test_req_drop_on_preprepare_phase_non_primary.py
+++ plenum/test/req_drop/test_req_drop_on_preprepare_phase_non_primary.py
@@ -1,14 +1,15 @@
 import pytest
 
 from plenum.common.constants import PROPAGATE
-from plenum.test.helper import sdk_json_to_request_object, sdk_send_random_requests
+from plenum.test.helper import sdk_send_random_requests
 from stp_core.loop.eventually import eventually
-from plenum.common.messages.node_messages import Propagate, PrePrepare, Prepare, Commit
+from plenum.common.messages.node_messages import PrePrepare, Prepare, Commit
 from plenum.test.delayers import delay, msg_rep_delay
 from plenum.test.propagate.helper import recvdRequest, recvdPropagate, \
     sentPropagate, recvdPrePrepareForInstId, recvdPrepareForInstId, recvdCommitForInstId
 from plenum.test.test_node import TestNode
 from plenum.test.node_request.helper import sdk_ensure_pool_functional
+from plenum.test.spy_helpers import getAllArgs
 
 
 howlong = 20
@@ -24,8 +25,8 @@ def tconf(tconf):
 
     tconf.OUTDATED_REQS_CHECK_ENABLED = True
     tconf.OUTDATED_REQS_CHECK_INTERVAL = 1
-    tconf.PROPAGATES_PHASE_REQ_TIMEOUT = 3
-    tconf.ORDERING_PHASE_REQ_TIMEOUT = 3600
+    tconf.PROPAGATES_PHASE_REQ_TIMEOUT = 3600
+    tconf.ORDERING_PHASE_REQ_TIMEOUT = 10
     yield tconf
 
     tconf.OUTDATED_REQS_CHECK_ENABLED = OUTDATED_REQS_CHECK_ENABLED_OLD
@@ -40,10 +41,9 @@ def setup(txnPoolNodeSet, looper, sdk_pool_handle, sdk_wallet_client):
     A, B, C, D = txnPoolNodeSet  # type: TestNode
     lagged_node = C
     frm = [A, B, D]
-    delay(Propagate, frm=frm, to=lagged_node, howlong=howlong)
-    delay(PrePrepare, frm=frm, to=lagged_node, howlong=howlong + 2)
-    delay(Prepare, frm=frm, to=lagged_node, howlong=howlong + 4)
-    delay(Commit, frm=frm, to=lagged_node, howlong=howlong + 6)
+    delay(PrePrepare, frm=frm, to=lagged_node, howlong=howlong)
+    delay(Prepare, frm=frm, to=lagged_node, howlong=howlong + 2)
+    delay(Commit, frm=frm, to=lagged_node, howlong=howlong + 4)
     # Delay MessageRep by long simulating loss as if Propagate is missing, it
     # is requested
     A.nodeIbStasher.delay(msg_rep_delay(10 * howlong, [PROPAGATE, ]))
@@ -53,19 +53,18 @@ def setup(txnPoolNodeSet, looper, sdk_pool_handle, sdk_wallet_client):
     return request_couple_json
 
 
-def test_req_drop_on_propagate_phase_on_non_primary_and_then_ordered(
+def test_req_drop_on_preprepare_phase_on_non_primary_and_then_ordered(
         tconf, setup, looper, txnPoolNodeSet,
         sdk_wallet_client, sdk_pool_handle):
     global initial_ledger_size
     A, B, C, D = txnPoolNodeSet  # type: TestNode
-    sent1 = sdk_json_to_request_object(setup[0][0])
     lagged_node = C
 
-    def check_propagates_and_3pc_delayed():
+    def check_preprepares_delayed():
         # Node should have received a request from the client
         assert len(recvdRequest(lagged_node)) == 1
         # Node should not have received a PROPAGATE
-        assert len(recvdPropagate(lagged_node)) == 0
+        assert len(recvdPropagate(lagged_node)) == 3
         # Node should have sent a PROPAGATE
         assert len(sentPropagate(lagged_node)) == 1
         # Node should have not received PrePrepares for master instance
@@ -78,31 +77,41 @@ def test_req_drop_on_propagate_phase_on_non_primary_and_then_ordered(
         assert len(lagged_node.requests) == 1
 
     timeout = howlong - 2
-    looper.run(eventually(check_propagates_and_3pc_delayed, retryWait=.5, timeout=timeout))
+    looper.run(eventually(check_preprepares_delayed, retryWait=.5, timeout=timeout))
 
     def check_drop():
+        # Node should have not received PrePrepare, Prepares and Commits for master instance
+        assert len(recvdPrePrepareForInstId(lagged_node, 0)) == 0
+        assert len(recvdPrepareForInstId(lagged_node, 0)) == 0
+        assert len(recvdCommitForInstId(lagged_node, 0)) == 0
+        # Request object should be dropped by timeout
         assert len(lagged_node.requests) == 0
 
-    timeout = tconf.PROPAGATES_PHASE_REQ_TIMEOUT + tconf.OUTDATED_REQS_CHECK_INTERVAL + 1
+    timeout = tconf.ORDERING_PHASE_REQ_TIMEOUT + tconf.OUTDATED_REQS_CHECK_INTERVAL + 1
     looper.run(eventually(check_drop, retryWait=.5, timeout=timeout))
 
     for n in txnPoolNodeSet:
         n.nodeIbStasher.resetDelays()
 
-    def check_propagates_received():
-        # Node should have received 3 PROPAGATEs
-        assert len(recvdPropagate(lagged_node)) == 3
-        # Node should have total of 4 PROPAGATEs (3 from other nodes and 1 from
-        # itself)
-        key = sent1.digest
-        assert key in lagged_node.requests
-        assert len(lagged_node.requests[key].propagates) == 4
-        # Node should still have sent two PROPAGATEs since request
-        # was dropped and re-received over propagate
-        assert len(sentPropagate(lagged_node)) == 2
+    def check_propagates_requested():
+        # Node should have received delayed PrePrepare
+        assert len(recvdPrePrepareForInstId(lagged_node, 0)) == 1
+        # Check that PROPAGATEs are requested by both replicas as PrePrepare has been
+        # received for request that was dropped.
+        assert len(getAllArgs(lagged_node, TestNode.request_propagates)) == 2
+
+    timeout = howlong
+    looper.run(eventually(check_propagates_requested, retryWait=.5, timeout=timeout))
+
+    def check_propagates_and_3pc_received():
+        # Node should not have received requested PROPAGATEs
+        assert len(recvdPropagate(lagged_node)) == 6
+        # Node should have received delayed Prepares and Commits for master instance
+        assert len(recvdPrepareForInstId(lagged_node, 0)) == 2
+        assert len(recvdCommitForInstId(lagged_node, 0)) == 3
 
     timeout = howlong + 2
-    looper.run(eventually(check_propagates_received, retryWait=.5, timeout=timeout))
+    looper.run(eventually(check_propagates_and_3pc_received, retryWait=.5, timeout=timeout))
 
     def check_ledger_size():
         # The request should be eventually ordered
