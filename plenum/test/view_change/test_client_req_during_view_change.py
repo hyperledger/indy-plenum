80b18e65f451839c6d41492f841e054828b5a276
toktar <renata.toktar@dsr-corporation.com>
2018-12-06 18:51:23 +0300

    INDY-1896: process read and action requests
    Changes:
    - move check in unpackClientMsg and add check for request type
    - add tests
    
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- plenum/test/view_change/test_client_req_during_view_change.py
+++ plenum/test/view_change/test_client_req_during_view_change.py
@@ -1,12 +1,24 @@
+import functools
+
 import pytest
 
+from plenum.common.constants import NODE, TXN_TYPE, GET_TXN
 from plenum.common.exceptions import RequestNackedException
 from plenum.test.helper import sdk_send_random_and_check, \
     sdk_send_random_requests, sdk_get_and_check_replies, sdk_gen_request, \
     checkDiscardMsg
+from plenum.test.pool_transactions.helper import sdk_build_get_txn_request, sdk_sign_and_send_prepared_request
+from plenum.test.testing_utils import FakeSomething
 
 
-def test_client_msg_discard_in_view_change_integration(txnPoolNodeSet,
+@pytest.fixture(scope='function')
+def test_node(test_node):
+    test_node.view_changer = FakeSomething(view_change_in_progress=True,
+                                           view_no=1)
+    return test_node
+
+
+def test_client_write_request_discard_in_view_change_integration(txnPoolNodeSet,
                                                        looper,
                                                        sdk_pool_handle,
                                                        sdk_wallet_client):
@@ -26,24 +38,55 @@ def test_client_msg_discard_in_view_change_integration(txnPoolNodeSet,
                "change is in progress" in e.args[0]
 
 
-def test_client_msg_discard_in_view_change_with_dict(txnPoolNodeSet):
-    node = txnPoolNodeSet[0]
-    node.view_changer.view_change_in_progress = True
-    node.send_nack_to_client = check_nack_msg
+def test_client_get_request_not_discard_in_view_change_integration(txnPoolNodeSet,
+                                                       looper,
+                                                       sdk_pool_handle,
+                                                       sdk_wallet_client):
+    '''
+    Check that client requests sent in view change will discard.
+    '''
+    for node in txnPoolNodeSet:
+        node.view_changer.view_change_in_progress = True
+    _, steward_did = sdk_wallet_client
+    request = sdk_build_get_txn_request(looper, steward_did, 1)
 
-    msg = sdk_gen_request("op").as_dict
-    node.unpackClientMsg(msg, "frm")
-    checkDiscardMsg([node, ], msg, "view change in progress")
+    sdk_request = sdk_sign_and_send_prepared_request(looper,
+                                               sdk_wallet_client,
+                                               sdk_pool_handle,
+                                               request)
+    sdk_get_and_check_replies(looper, [sdk_request])
 
 
-def test_client_msg_discard_in_view_change_with_request(txnPoolNodeSet):
-    node = txnPoolNodeSet[0]
-    node.view_changer.view_change_in_progress = True
-    node.send_nack_to_client = check_nack_msg
+def test_client_write_request_discard_in_view_change_with_dict(test_node):
+    test_node.send_nack_to_client = check_nack_msg
 
-    msg = sdk_gen_request("op")
-    node.unpackClientMsg(msg, "frm")
-    checkDiscardMsg([node, ], msg.as_dict, "view change in progress")
+    msg = sdk_gen_request({TXN_TYPE: NODE}).as_dict
+    test_node.unpackClientMsg(msg, "frm")
+    checkDiscardMsg([test_node, ], msg, "view change in progress")
+
+
+def test_client_get_request_not_discard_in_view_change_with_dict(test_node):
+    sender = "frm"
+    msg = sdk_gen_request({TXN_TYPE: GET_TXN}).as_dict
+
+    def post_to_client_in_box(received_msg, received_frm):
+        assert received_frm == sender
+        assert received_msg == msg
+    test_node.postToClientInBox = post_to_client_in_box
+
+    def discard(received_msg, reason, logLevel):
+        assert False, "Message {} was discard with '{}'".format(received_msg, reason)
+    test_node.discard = discard
+
+    test_node.unpackClientMsg(msg, sender)
+
+
+def test_client_msg_discard_in_view_change_with_request(test_node):
+    test_node.send_nack_to_client = check_nack_msg
+
+    msg = sdk_gen_request({TXN_TYPE: NODE})
+    test_node.unpackClientMsg(msg, "frm")
+    checkDiscardMsg([test_node, ], msg.as_dict, "view change in progress")
 
 
 def check_nack_msg(req_key, reason, to_client):
