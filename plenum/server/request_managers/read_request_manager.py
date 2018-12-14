4a5662b8dfd54cadee241a8b4aacfbdb7c418c6f
ArtObr <artemobruchnikov@gmail.com>
2018-12-14 10:51:18 +0300

    INDY-1856: Defining interfaces
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>

---
+++ plenum/server/request_managers/read_request_manager.py
@@ -0,0 +1,9 @@
+from typing import Dict
+
+from plenum.server.request_handlers.handler_interfaces.read_request_handler import ReadRequestHandler
+from plenum.server.request_managers.request_manager import RequestManager
+
+
+class ReadRequestManager(RequestManager):
+    def __init__(self):
+        self.request_handlers = {}  # type: Dict[int,ReadRequestHandler]
