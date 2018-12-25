531eeac05ec5b67c551bd8c77513417b4f7f3d45
ArtObr <artemobruchnikov@gmail.com>
2018-12-25 17:20:23 +0300

    INDY-1856: Flake8 fix
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>

--- plenum/server/request_handlers/utils.py
+++ plenum/server/request_handlers/utils.py
@@ -8,6 +8,7 @@ def is_steward(state, nym, isCommitted: bool = True):
     role = get_role(state, nym, isCommitted)
     return role == STEWARD
 
+
 def get_role(state, nym, isCommitted: bool = True):
     nymData = get_nym_details(state, nym, isCommitted)
     if not nymData:
@@ -15,12 +16,14 @@ def get_role(state, nym, isCommitted: bool = True):
     else:
         return nymData.get(ROLE)
 
+
 def get_nym_details(state, nym, isCommitted: bool = True):
     key = nym_to_state_key(nym)
     data = state.get(key, isCommitted)
     if not data:
         return {}
     return domain_state_serializer.deserialize(data)
+
 
 def nym_to_state_key(nym: str) -> bytes:
     return sha256(nym.encode()).digest()
