57ad2b27957a7c59566bec583ca68aac5dbe5c70
ArtObr <artemobruchnikov@gmail.com>
2019-02-06 18:01:13 +0300

    INDY-1917: Review fixes
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>

--- plenum/test/replica/test_monitor_reset_after_replica_addition.py
+++ plenum/test/replica/test_monitor_reset_after_replica_addition.py
@@ -26,8 +26,8 @@ def tconf(tconf):
     tconf.throughput_measurement_params = old_throughput_measurement_params
 
 
-def test_replica_addition(looper, sdk_pool_handle, txnPoolNodeSet,
-                          sdk_wallet_steward, tdir, tconf, allPluginsPath):
+def test_monitor_reset_after_replica_addition(looper, sdk_pool_handle, txnPoolNodeSet,
+                                              sdk_wallet_steward, tdir, tconf, allPluginsPath):
     sdk_send_random_and_check(looper, txnPoolNodeSet,
                               sdk_pool_handle, sdk_wallet_steward, 30)
     sdk_node_theta_added(looper, txnPoolNodeSet, tdir, tconf,
