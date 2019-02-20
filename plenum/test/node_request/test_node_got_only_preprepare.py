da66fbffcd338e3fe744b73b5039cfc98f694557
ashcherbakov <alexander.sherbakov@dsr-corporation.com>
2019-02-21 01:13:59 +0300

    fix tests
    Signed-off-by: ashcherbakov <alexander.sherbakov@dsr-corporation.com>

--- plenum/test/node_request/test_node_got_only_preprepare.py
+++ plenum/test/node_request/test_node_got_only_preprepare.py
@@ -1,9 +1,11 @@
 import pytest
 
+from plenum.test.checkpoints.helper import check_stashed_chekpoints
+from plenum.test.node_catchup.helper import waitNodeDataEquality
 from plenum.test.node_request.helper import nodes_last_ordered_equal
 from stp_core.loop.eventually import eventually
 
-from plenum.test.helper import sdk_send_batches_of_random_and_check, sdk_send_batches_of_random, sdk_get_reply
+from plenum.test.helper import sdk_send_batches_of_random_and_check
 from plenum.test.malicious_behaviors_node import dont_send_prepare_and_commit_to, reset_sending
 
 from plenum.test.checkpoints.conftest import chkFreqPatched
@@ -12,7 +14,7 @@ from plenum.test.checkpoints.conftest import chkFreqPatched
 @pytest.fixture(scope="module")
 def tconf(tconf):
     OldMax3PCBatchSize = tconf.Max3PCBatchSize
-    tconf.Max3PCBatchSize = 2
+    tconf.Max3PCBatchSize = 1
     yield tconf
     tconf.Max3PCBatchSize = OldMax3PCBatchSize
 
@@ -64,14 +66,15 @@ def test_2_nodes_get_only_preprepare(looper,
                                      sdk_wallet_client,
                                      tconf,
                                      chkFreqPatched):
+    # CHK_FREQ = 2 in this test
+    # num of stashed checkpoints to start catchup is 2 (or 4 batches)
+
     master_node = txnPoolNodeSet[0]
     behind_nodes = txnPoolNodeSet[-2:]
-    delta = tconf.CHK_FREQ * 3
-    num_of_batches = 1
 
     # Nodes order batches
     sdk_send_batches_of_random_and_check(
-        looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_client, num_of_batches, num_of_batches)
+        looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_client, 1, 1)
     nodes_last_ordered_equal(*txnPoolNodeSet)
 
     # Emulate connection problems, 1st behind_node receiving only pre-prepares
@@ -79,20 +82,23 @@ def test_2_nodes_get_only_preprepare(looper,
 
     # Send some txns and 1st behind_node cant order them while pool is working
     sdk_send_batches_of_random_and_check(
-        looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_client, num_of_batches, num_of_batches)
-    assert behind_nodes[0].master_last_ordered_3PC[1] + num_of_batches == \
+        looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_client, 1, 1)
+    assert behind_nodes[0].master_last_ordered_3PC[1] + 1 == \
            master_node.master_last_ordered_3PC[1]
+
+    # 1st behind got 1 stashed checkpoint from each node
+    check_stashed_chekpoints(behind_nodes[0], 3)
 
     # Remove connection problems
     reset_sending(txnPoolNodeSet[:-2])
 
     # Send txns
     sdk_send_batches_of_random_and_check(
-        looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_client, num_of_batches, num_of_batches)
+        looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_client, 1, 1)
 
     # 1st behind_node is getting new prepares, but still can't order,
     # cause can't get quorum for prepare for previous batch
-    assert behind_nodes[0].master_last_ordered_3PC[1] + num_of_batches * 2 == \
+    assert behind_nodes[0].master_last_ordered_3PC[1] + 1 * 2 == \
            master_node.master_last_ordered_3PC[1]
 
     # Emulate connection problems, 2nd behind_node receiving only pre-prepares
@@ -100,29 +106,33 @@ def test_2_nodes_get_only_preprepare(looper,
 
     # Send some txns and 2nd behind_node cant order them while pool is working
     sdk_send_batches_of_random_and_check(
-        looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_client, num_of_batches, num_of_batches)
-    assert behind_nodes[1].master_last_ordered_3PC[1] + num_of_batches == \
+        looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_client, 1, 1)
+    assert behind_nodes[1].master_last_ordered_3PC[1] + 1 == \
            master_node.master_last_ordered_3PC[1]
+
+    # 2d behind got 1 stashed checkpoint from all nodes except 1st behind
+    check_stashed_chekpoints(behind_nodes[1], 2)
+
+    # 1st behind got another stashed checkpoint, so should catch-up now
+    waitNodeDataEquality(looper, master_node, behind_nodes[0], customTimeout=60)
 
     # Remove connection problems
     reset_sending(txnPoolNodeSet[:-2])
 
     # Send txns
     sdk_send_batches_of_random_and_check(
-        looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_client, num_of_batches, num_of_batches)
+        looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_client, 1, 1)
 
     # 2nd behind_node is getting new prepares, but still can't order,
     # cause can't get quorum for prepare for previous batch
-    assert behind_nodes[1].master_last_ordered_3PC[1] + num_of_batches * 2 == \
+    assert behind_nodes[1].master_last_ordered_3PC[1] + 1 * 2 == \
            master_node.master_last_ordered_3PC[1]
 
     # After achieving stable checkpoint, behind_node start ordering
-    reqs = sdk_send_batches_of_random(looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_client,
-                                      delta, delta)
+    sdk_send_batches_of_random_and_check(looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_client,
+                                         1, 1)
+    # 2d behind got another stashed checkpoint, so should catch-up now
+    waitNodeDataEquality(looper, master_node, behind_nodes[1], customTimeout=60)
 
     # Pool is working
-    looper.run(eventually(nodes_last_ordered_equal, *behind_nodes, master_node, timeout=60))
-
-    # Finish request gracefully
-    for req in reqs:
-        sdk_get_reply(looper, req)
+    waitNodeDataEquality(looper, master_node, *behind_nodes, customTimeout=5)
