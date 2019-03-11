6fd03c68ef200b5542fea36e1cd6713642e07f95
ashcherbakov <alexander.sherbakov@dsr-corporation.com>
2019-03-11 23:36:25 +0300

    get rid of current state delays in tests
    Signed-off-by: ashcherbakov <alexander.sherbakov@dsr-corporation.com>

--- plenum/test/node_catchup_with_3pc/test_stashing_3pc_while_catchup.py
+++ plenum/test/node_catchup_with_3pc/test_stashing_3pc_while_catchup.py
@@ -5,7 +5,7 @@ from plenum.common.messages.node_messages import CatchupRep
 from plenum.common.startable import Mode
 from plenum.server.node import Node
 from plenum.test import waits
-from plenum.test.delayers import cr_delay, cs_delay
+from plenum.test.delayers import cr_delay
 from plenum.test.pool_transactions.helper import \
     disconnect_node_and_ensure_disconnected
 from plenum.test.helper import sdk_send_random_and_check, assertExp
@@ -62,40 +62,39 @@ def test_3pc_while_catchup(tdir, tconf,
 
     initial_all_ledgers_caught_up = lagging_node.spylog.count(Node.allLedgersCaughtUp)
     assert all(replica.stasher.num_stashed_catchup == 0 for inst_id, replica in lagging_node.replicas.items())
-    # delay CurrentState to avoid Primary Propagation (since it will lead to more catch-ups not needed in this test).
-    with delay_rules(lagging_node.nodeIbStasher, cs_delay()):
-        with delay_rules(lagging_node.nodeIbStasher, cr_delay(ledger_filter=DOMAIN_LEDGER_ID)):
-            looper.add(lagging_node)
-            txnPoolNodeSet[-1] = lagging_node
-            looper.run(checkNodesConnected(txnPoolNodeSet))
 
-            # wait till we got catchup replies for messages missed while the node was offline,
-            # so that now we can order more messages, and they will not be caught up, but stashed
-            looper.run(
-                eventually(lambda: assertExp(lagging_node.nodeIbStasher.num_of_stashed(CatchupRep) >= 3), retryWait=1,
-                           timeout=60))
+    with delay_rules(lagging_node.nodeIbStasher, cr_delay(ledger_filter=DOMAIN_LEDGER_ID)):
+        looper.add(lagging_node)
+        txnPoolNodeSet[-1] = lagging_node
+        looper.run(checkNodesConnected(txnPoolNodeSet))
 
-            # make sure that more requests are being ordered while catch-up is in progress
-            sdk_send_random_and_check(looper, txnPoolNodeSet, sdk_pool_handle,
-                                      sdk_wallet_client, 10)
-
-            assert lagging_node.mode == Mode.syncing
-            assert all(replica.stasher.num_stashed_catchup > 0 for inst_id, replica in lagging_node.replicas.items())
-
-        # check that the catch-up is finished
+        # wait till we got catchup replies for messages missed while the node was offline,
+        # so that now we can order more messages, and they will not be caught up, but stashed
         looper.run(
-            eventually(
-                lambda: assertExp(lagging_node.mode == Mode.participating), retryWait=1,
-                timeout=waits.expectedPoolCatchupTime(len(txnPoolNodeSet))
-            )
+            eventually(lambda: assertExp(lagging_node.nodeIbStasher.num_of_stashed(CatchupRep) >= 3), retryWait=1,
+                       timeout=60))
+
+        # make sure that more requests are being ordered while catch-up is in progress
+        sdk_send_random_and_check(looper, txnPoolNodeSet, sdk_pool_handle,
+                                  sdk_wallet_client, 10)
+
+        assert lagging_node.mode == Mode.syncing
+        assert all(replica.stasher.num_stashed_catchup > 0 for inst_id, replica in lagging_node.replicas.items())
+
+    # check that the catch-up is finished
+    looper.run(
+        eventually(
+            lambda: assertExp(lagging_node.mode == Mode.participating), retryWait=1,
+            timeout=waits.expectedPoolCatchupTime(len(txnPoolNodeSet))
         )
-        looper.run(
-            eventually(
-                lambda: assertExp(
-                    lagging_node.spylog.count(Node.allLedgersCaughtUp) == initial_all_ledgers_caught_up + 1)
-            )
+    )
+    looper.run(
+        eventually(
+            lambda: assertExp(
+                lagging_node.spylog.count(Node.allLedgersCaughtUp) == initial_all_ledgers_caught_up + 1)
         )
-        # check that the node was able to order requests stashed during catch-up
-        # do not check for audit ledger since we didn't catch-up audit ledger when txns were ordering durinf catch-up
-        waitNodeDataEquality(looper, *txnPoolNodeSet, exclude_from_check='check_audit', customTimeout=5)
-        assert all(replica.stasher.num_stashed_catchup == 0 for inst_id, replica in lagging_node.replicas.items())
+    )
+    # check that the node was able to order requests stashed during catch-up
+    # do not check for audit ledger since we didn't catch-up audit ledger when txns were ordering during catch-up
+    waitNodeDataEquality(looper, *txnPoolNodeSet, exclude_from_check='check_audit', customTimeout=5)
+    assert all(replica.stasher.num_stashed_catchup == 0 for inst_id, replica in lagging_node.replicas.items())
