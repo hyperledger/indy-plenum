34eb224c181ec35b674dcb5085af81b496725a51
ashcherbakov <alexander.sherbakov@dsr-corporation.com>
2019-03-22 16:16:21 +0300

    improve logging; correctly set last committed after catchup of audit
    Signed-off-by: ashcherbakov <alexander.sherbakov@dsr-corporation.com>

--- plenum/test/audit_ledger/conftest.py
+++ plenum/test/audit_ledger/conftest.py
@@ -3,40 +3,43 @@ import pytest
 from plenum.common.constants import CONFIG_LEDGER_ID, DOMAIN_LEDGER_ID, POOL_LEDGER_ID, AUDIT_LEDGER_ID
 from plenum.server.batch_handlers.audit_batch_handler import AuditBatchHandler
 
+@pytest.fixture(scope="module")
+def node(txnPoolNodeSet):
+    return txnPoolNodeSet[0]
 
 @pytest.fixture(scope="module")
-def db_manager(txnPoolNodeSet):
-    return txnPoolNodeSet[0].db_manager
+def db_manager(node):
+    return node.db_manager
 
 
 @pytest.fixture(scope="function")
-def initial_pool_size(txnPoolNodeSet):
-    return txnPoolNodeSet[0].getLedger(POOL_LEDGER_ID).size
+def initial_pool_size(node):
+    return node.getLedger(POOL_LEDGER_ID).size
 
 
 @pytest.fixture(scope="function")
-def initial_domain_size(txnPoolNodeSet):
-    return txnPoolNodeSet[0].getLedger(DOMAIN_LEDGER_ID).size
+def initial_domain_size(node):
+    return node.getLedger(DOMAIN_LEDGER_ID).size
 
 
 @pytest.fixture(scope="function")
-def initial_config_size(txnPoolNodeSet):
-    return txnPoolNodeSet[0].getLedger(CONFIG_LEDGER_ID).size
+def initial_config_size(node):
+    return node.getLedger(CONFIG_LEDGER_ID).size
 
 
 @pytest.fixture(scope="function")
-def view_no(txnPoolNodeSet):
-    return txnPoolNodeSet[0].master_replica.last_ordered_3pc[0]
+def view_no(node):
+    return node.master_replica.last_ordered_3pc[0]
 
 
 @pytest.fixture(scope="function")
-def pp_seq_no(txnPoolNodeSet):
-    return txnPoolNodeSet[0].master_replica.last_ordered_3pc[1]
+def pp_seq_no(node):
+    return node.master_replica.last_ordered_3pc[1]
 
 
 @pytest.fixture(scope="function")
-def initial_seq_no(txnPoolNodeSet):
-    return txnPoolNodeSet[0].getLedger(AUDIT_LEDGER_ID).size
+def initial_seq_no(node):
+    return node.getLedger(AUDIT_LEDGER_ID).size
 
 
 @pytest.fixture(scope="function")
