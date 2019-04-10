69e8d1c58529eef4186d161ac10d66b7cd4f0221
Andrey Kononykhin <andkononykhin@gmail.com>
2019-04-10 07:55:47 +0300

    fixes test
    Signed-off-by: Andrey Kononykhin <andkononykhin@gmail.com>

--- plenum/test/node_catchup_with_3pc/test_pp_obsolescence_check_pass_for_stashed.py
+++ plenum/test/node_catchup_with_3pc/test_pp_obsolescence_check_pass_for_stashed.py
@@ -1,15 +1,14 @@
 import pytest
 
 from plenum.test import waits
-from plenum.common.constants import LEDGER_STATUS, DOMAIN_LEDGER_ID
-from plenum.common.messages.node_messages import MessageReq, CatchupReq
+from plenum.common.constants import LEDGER_STATUS, AUDIT_LEDGER_ID
+from plenum.common.messages.node_messages import MessageRep, MessageReq, CatchupReq
 from plenum.server.catchup.node_leecher_service import NodeLeecherService
-from plenum.test.delayers import ppDelay, pDelay, cDelay
+from plenum.test.delayers import DEFAULT_DELAY
 from plenum.test.helper import sdk_send_random_and_check
 from plenum.test.node_request.test_timestamp.helper import get_timestamp_suspicion_count
 from plenum.test.node_catchup.helper import ensure_all_nodes_have_same_data
-from plenum.test.stasher import delay_rules, start_delaying, stop_delaying_and_process
-from stp_core.loop.eventually import eventually
+from plenum.test.stasher import delay_rules
 
 
 # TODO better to use non hard coded value of nodes number
@@ -27,17 +26,13 @@ def tconf(tconf):
     tconf.ACCEPTABLE_DEVIATION_PREPREPARE_SECS = old_value
 
 
-def delay_domain_ledger_catchup():
+def delay_audit_ledger_catchup():
     def delay(msg):
         msg = msg[0]
-        delay = PATCHED_ACCEPTABLE_DEVIATION_PREPREPARE_SECS + 1
-        if isinstance(msg, MessageReq) and \
+        if isinstance(msg, MessageRep) and \
                 msg.msg_type == LEDGER_STATUS and \
-                msg.params.get('ledgerId') == DOMAIN_LEDGER_ID:
-            return delay
-        if isinstance(msg, CatchupReq) and \
-                msg.ledgerId == DOMAIN_LEDGER_ID:
-            return delay
+                msg.params.get('ledgerId') == AUDIT_LEDGER_ID:
+            return DEFAULT_DELAY
 
     return delay
 
@@ -48,50 +43,35 @@ def test_stashed_pp_pass_obsolescence_check(tdir, tconf,
                                      sdk_pool_handle,
                                      sdk_wallet_client):
     lagging_node = txnPoolNodeSet[-1]
-    other_nodes = txnPoolNodeSet[:-1]
-    other_stashers = [node.nodeIbStasher for node in other_nodes]
-
-    # TODO the test partly copies logic of catchup forcing from test_slow_catchup_while_ordering.py,
-    # better to create a helper
 
     def lagging_node_state() -> NodeLeecherService.State:
         return lagging_node.ledgerManager._node_leecher._state
 
-    def check_lagging_node_is_not_syncing_audit():
-        assert lagging_node_state() != NodeLeecherService.State.SyncingAudit
+    # fill domain ledger with some requests
+    # (workaround for known issues with AUDIT ledger)
+    # TODO add task number
+    sdk_send_random_and_check(looper, txnPoolNodeSet,
+                              sdk_pool_handle, sdk_wallet_client, 1)
 
-    delay_handler = None
-    # Prevent lagging node from ordering
-    with delay_rules(lagging_node.nodeIbStasher, ppDelay(), pDelay(), cDelay()):
-        # Order request on all nodes except lagging one
-        sdk_send_random_and_check(looper, txnPoolNodeSet,
-                                  sdk_pool_handle, sdk_wallet_client, 1)
-
-        # Prevent lagging node from catching up domain ledger (and finishing catchup),
-        # delay should be long enough to exceed PP ACCEPTABLE_DEVIATION_PREPREPARE_SECS
-        delay_handler = start_delaying(other_stashers, delay_domain_ledger_catchup())
-
+    # Prevent lagging node from catching up domain ledger (and finishing catchup)
+    with delay_rules(lagging_node.nodeIbStasher, delay_audit_ledger_catchup()):
         # Start catchup on lagging node
         lagging_node.ledgerManager.start_catchup()
         assert lagging_node_state() == NodeLeecherService.State.SyncingAudit
 
-        # Ensure that audit ledger is caught up by lagging node
-        looper.run(eventually(check_lagging_node_is_not_syncing_audit))
-        assert lagging_node_state() != NodeLeecherService.State.Idle
-
-        # Order one more request on all nodes except lagging one
+        # Order request on all nodes except lagging one where they goes to stashed state
         sdk_send_random_and_check(looper, txnPoolNodeSet,
                                   sdk_pool_handle, sdk_wallet_client, 1)
 
-    # Now delayed 3PC messages reach lagging node, but goes to stahed state since
-    # catch up is still not finished. Here timestamps for PP are set.
+        # lagging node is still syncing Audit ledger
+        assert lagging_node_state() == NodeLeecherService.State.SyncingAudit
 
-    # Stop catchup delaying to allow lagging node catching up domain ledger
-    # which contains more transactions than it was when audit ledger was caught up,
-    # delayed 3PC messages are still in stashed state
-    stop_delaying_and_process(delay_handler)
+        # delay catchup end to exceed PP ACCEPTABLE_DEVIATION_PREPREPARE_SECS
+        looper.runFor(PATCHED_ACCEPTABLE_DEVIATION_PREPREPARE_SECS + 1)
 
-    # Now delayed 3PC messages are unstanshed and can be ordered,
+    # Now catchup should end and lagging node starts processing stashed PPs
+    # and resumes ordering
+
     # ensure that all nodes will have same data after that
     ensure_all_nodes_have_same_data(looper, txnPoolNodeSet)
 
