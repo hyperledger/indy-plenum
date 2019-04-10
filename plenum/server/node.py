454c1962a9765bab72cf4aef37d5b0aee2f11a32
toktar <renata.toktar@dsr-corporation.com>
2019-04-10 14:44:08 +0300

    Add an input validation in getLedgerStatus()
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- plenum/server/node.py
+++ plenum/server/node.py
@@ -2336,11 +2336,9 @@ class Node(HasActionQueue, Motor, Propagator, MessageProcessor, HasFileStorage,
         if ledgerId == POOL_LEDGER_ID and not self.poolLedger:
             # Since old style nodes don't know have pool ledger
             return None
-        try:
-            return self.build_ledger_status(ledgerId)
-        except Exception as e:
-            logger.warning(e)
+        if ledgerId not in self.ledger_ids:
             return None
+        return self.build_ledger_status(ledgerId)
 
     def sendLedgerStatus(self, nodeName: str, ledgerId: int):
         ledgerStatus = self.getLedgerStatus(ledgerId)
--- plenum/test/node_request/message_request/test_request_ls_for_incorrect_ledger.py
+++ plenum/test/node_request/message_request/test_request_ls_for_incorrect_ledger.py
@@ -1,16 +1,11 @@
 from plenum.common.constants import DOMAIN_LEDGER_ID
 from plenum.test.conftest import test_node
-from plenum.test.logging.conftest import logsearch
 
 
-def test_request_ls_for_incorrect_ledger(test_node, logsearch):
+def test_request_ls_for_incorrect_ledger(test_node):
     incorrect_ledger_id = 12345
     correct_ledger_id = DOMAIN_LEDGER_ID
-    log_msg_count, _ = logsearch(msgs=["Invalid ledger type: "
-                                       "{}".format(incorrect_ledger_id)])
-    old_re_ask_count = len(log_msg_count)
+
     assert not test_node.getLedgerStatus(incorrect_ledger_id)
-    # check logs
-    assert len(log_msg_count) - old_re_ask_count == 1
     # check that node build a ledger status for a correct ledger
     assert test_node.getLedgerStatus(correct_ledger_id)
