79f5978f10209efb9c192d67eb596634e9f4d27a
Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>
2019-04-12 19:23:41 +0300

    INDY-1863: Allow limiting maximum number of 3PC batches in flight
    Signed-off-by: Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>

--- plenum/server/replica_validator.py
+++ plenum/server/replica_validator.py
@@ -107,9 +107,13 @@ class ReplicaValidator:
             return False
         if self.replica.viewNo < self.replica.last_ordered_3pc[0]:
             return False
-        if self.replica.viewNo == self.replica.last_ordered_3pc[0] and \
-                self.replica.lastPrePrepareSeqNo < self.replica.last_ordered_3pc[1]:
-            return False
+        if self.replica.viewNo == self.replica.last_ordered_3pc[0]:
+            if self.replica.lastPrePrepareSeqNo < self.replica.last_ordered_3pc[1]:
+                return False
+            if self.replica.config.Max3PCBatchesInFlight is not None:
+                batches_in_flight = self.replica.lastPrePrepareSeqNo - self.replica.last_ordered_3pc[1]
+                if batches_in_flight > self.replica.config.Max3PCBatchesInFlight:
+                    return False
         return True
 
     def can_order(self):
