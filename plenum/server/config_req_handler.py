302eb93a963d52ddeb40013e38325e47b4b4550f
Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>
2019-05-07 18:23:35 +0300

    INDY-2066: Register TXN_AUTHOR_AGREEMENT and implement static validation
    Signed-off-by: Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>

--- plenum/server/config_req_handler.py
+++ plenum/server/config_req_handler.py
@@ -1,8 +1,13 @@
+from plenum.common.constants import TXN_AUTHOR_AGREEMENT, TXN_AUTHOR_AGREEMENT_AML, GET_TXN_AUTHOR_AGREEMENT, \
+    GET_TXN_AUTHOR_AGREEMENT_AML
 from plenum.common.request import Request
 from plenum.server.ledger_req_handler import LedgerRequestHandler
 
 
 class ConfigReqHandler(LedgerRequestHandler):
+    write_types = {TXN_AUTHOR_AGREEMENT, TXN_AUTHOR_AGREEMENT_AML}
+    query_types = {GET_TXN_AUTHOR_AGREEMENT, GET_TXN_AUTHOR_AGREEMENT_AML}
+
     def __init__(self, ledger, state):
         super().__init__(ledger, state)
 
