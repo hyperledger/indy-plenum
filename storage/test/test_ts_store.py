ffd6fd61b43e4f013d09d0c364ffd0a75faf20ec
Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>
2019-05-13 18:05:49 +0300

    INDY-2066: Improve test_ts_store
    Signed-off-by: Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>

--- storage/test/test_ts_store.py
+++ storage/test/test_ts_store.py
@@ -1,6 +1,6 @@
 import pytest
 from storage.helper import initKeyValueStorageIntKeys
-from plenum.common.constants import KeyValueStorageType
+from plenum.common.constants import KeyValueStorageType, DOMAIN_LEDGER_ID, CONFIG_LEDGER_ID
 from storage.state_ts_store import StateTsDbStorage
 
 
@@ -11,11 +11,23 @@ def empty_storage(request, tmpdir_factory):
     else:
         kv_storage_type = KeyValueStorageType.Rocksdb
 
-    storage = StateTsDbStorage("test",
-                               initKeyValueStorageIntKeys(
-                                   kv_storage_type,
-                                   tmpdir_factory.mktemp('').strpath,
-                                   "test_db"))
+    data_location = tmpdir_factory.mktemp('').strpath
+
+    domain_storage = initKeyValueStorageIntKeys(
+        kv_storage_type,
+        data_location,
+        "test_db")
+
+    config_storage = initKeyValueStorageIntKeys(
+        kv_storage_type,
+        data_location,
+        "config_test_db")
+
+    storage = StateTsDbStorage("test", {
+        DOMAIN_LEDGER_ID: domain_storage,
+        CONFIG_LEDGER_ID: config_storage
+    })
+
     return storage
 
 
@@ -23,38 +35,66 @@ def empty_storage(request, tmpdir_factory):
 def storage_with_ts_root_hashes(empty_storage):
     storage = empty_storage
 
-    ts_list = {
+    domain_ts_list = {
         2: "aaaa",
         4: "bbbb",
         5: "cccc",
-        100: "ffff",
+        100: "ffff"
     }
-    for k, v in ts_list.items():
+
+    config_ts_list = {
+        0: "pppp",
+        3: "qqqq",
+        5: "rrrr",
+        50: "ssss"
+    }
+
+    for k, v in domain_ts_list.items():
         storage.set(k, v)
-    return storage, ts_list
+    for k, v in config_ts_list.items():
+        storage.set(k, v, ledger_id=CONFIG_LEDGER_ID)
+
+    return storage, domain_ts_list, config_ts_list
 
 
 def test_none_if_key_less_then_minimal_key(storage_with_ts_root_hashes):
-    storage, _ = storage_with_ts_root_hashes
-    assert storage.get_equal_or_prev(1) is None
+    storage, domain_ts_list, config_ts_list = storage_with_ts_root_hashes
+    min_domain_ts = min(domain_ts_list.keys())
+    min_config_ts = min(config_ts_list.keys())
+    assert storage.get_equal_or_prev(min_domain_ts - 1) is None
+    assert storage.get_equal_or_prev(min_config_ts - 1, DOMAIN_LEDGER_ID) is None
+    assert storage.get_equal_or_prev(min_config_ts - 1, CONFIG_LEDGER_ID) is None
 
 
 def test_previous_key_for_given(storage_with_ts_root_hashes):
-    storage, ts_list = storage_with_ts_root_hashes
-    assert storage.get_equal_or_prev(3).decode("utf-8") == ts_list[2]
-    assert storage.get_equal_or_prev(101).decode("utf-8") == ts_list[100]
+    storage, ts_list, _ = storage_with_ts_root_hashes
+    storage, domain_ts_list, config_ts_list = storage_with_ts_root_hashes
+    domain_ts_list = {ts: root_hash for ts, root_hash in domain_ts_list.items() if ts + 1 not in domain_ts_list}
+    config_ts_list = {ts: root_hash for ts, root_hash in config_ts_list.items() if ts + 1 not in config_ts_list}
+    for ts, root_hash in domain_ts_list.items():
+        assert storage.get_equal_or_prev(ts + 1).decode() == root_hash
+        assert storage.get_equal_or_prev(ts + 1, DOMAIN_LEDGER_ID).decode() == root_hash
+    for ts, root_hash in config_ts_list.items():
+        assert storage.get_equal_or_prev(ts + 1, CONFIG_LEDGER_ID).decode() == root_hash
 
 
 def test_get_required_key(storage_with_ts_root_hashes):
-    storage, ts_list = storage_with_ts_root_hashes
-    assert storage.get_equal_or_prev(2).decode("utf-8") == ts_list[2]
+    storage, domain_ts_list, config_ts_list = storage_with_ts_root_hashes
+    for ts, root_hash in domain_ts_list.items():
+        assert storage.get_equal_or_prev(ts).decode() == root_hash
+        assert storage.get_equal_or_prev(ts, DOMAIN_LEDGER_ID).decode() == root_hash
+    for ts, root_hash in config_ts_list.items():
+        assert storage.get_equal_or_prev(ts, CONFIG_LEDGER_ID).decode() == root_hash
 
 
 def test_get_last_key(storage_with_ts_root_hashes):
-    storage, ts_list = storage_with_ts_root_hashes
-    assert storage.get_last_key().decode() == '100'
+    storage, domain_ts_list, config_ts_list = storage_with_ts_root_hashes
+    assert storage.get_last_key().decode() == str(max(domain_ts_list.keys()))
+    assert storage.get_last_key(DOMAIN_LEDGER_ID).decode() == str(max(domain_ts_list.keys()))
+    assert storage.get_last_key(CONFIG_LEDGER_ID).decode() == str(max(config_ts_list.keys()))
 
 
 def test_empty_storage_get_last_key(empty_storage):
     storage = empty_storage
     assert storage.get_last_key() is None
+    assert storage.get_last_key(CONFIG_LEDGER_ID) is None
