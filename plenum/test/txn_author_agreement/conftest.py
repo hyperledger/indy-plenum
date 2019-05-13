303e51f2e330bbb4ba74dca43792c6477f698a81
Andrey Kononykhin <andkononykhin@gmail.com>
2019-05-13 20:16:20 +0300

    adds more taa data to state
    Signed-off-by: Andrey Kononykhin <andkononykhin@gmail.com>

--- plenum/test/txn_author_agreement/conftest.py
+++ plenum/test/txn_author_agreement/conftest.py
@@ -1,11 +1,17 @@
 import pytest
 
+from state.pruning_state import PruningState
+from storage.kv_in_memory import KeyValueStorageInMemory
 from ledger.compact_merkle_tree import CompactMerkleTree
+
+from plenum.common.util import randomString
 from plenum.common.ledger import Ledger
 from plenum.server.config_req_handler import ConfigReqHandler
 from plenum.test.testing_utils import FakeSomething
-from state.pruning_state import PruningState
-from storage.kv_in_memory import KeyValueStorageInMemory
+
+from plenum.test.txn_author_agreement.helper import (
+    TaaData, expected_state_data
+)
 
 
 @pytest.fixture
@@ -27,3 +33,23 @@ def config_req_handler(config_state,
     return ConfigReqHandler(config_ledger,
                             config_state,
                             domain_state=FakeSomething())
+
+
+@pytest.fixture
+def taa_in_data():
+    return [
+        TaaData(randomString(8), randomString(32), n, n + 10)
+        for n in range(10)
+    ]
+
+
+@pytest.fixture
+def taa_state_data(taa_in_data):
+    return {data.version: expected_state_data(data) for data in taa_in_data}
+
+
+@pytest.fixture
+def taa_digests(taa_in_data):
+    # TODO use some other API, e.g. sdk's one
+    return {data.version: ConfigReqHandler._taa_digest(
+        data.version, data.text).decode() for data in taa_in_data}
--- plenum/test/txn_author_agreement/helper.py
+++ plenum/test/txn_author_agreement/helper.py
@@ -1,9 +1,23 @@
 from random import randint
+from typing import NamedTuple, Dict
 
-from plenum.common.constants import TXN_TYPE, CURRENT_PROTOCOL_VERSION, TXN_AUTHOR_AGREEMENT, CONFIG_LEDGER_ID
+from plenum.common.constants import (
+    TXN_TYPE, CURRENT_PROTOCOL_VERSION, TXN_AUTHOR_AGREEMENT, CONFIG_LEDGER_ID,
+    TXN_AUTHOR_AGREEMENT_TEXT, TXN_AUTHOR_AGREEMENT_VERSION,
+    TXN_PAYLOAD, TXN_METADATA, TXN_METADATA_SEQ_NO, TXN_METADATA_TIME
+)
+
 from plenum.common.types import OPERATION, f
 from plenum.common.util import randomString
 from plenum.server.config_req_handler import ConfigReqHandler
+
+
+TaaData = NamedTuple("TaaData", [
+    ("version", str),
+    ("text", str),
+    ("seqNo", int),
+    ("txnTime", int)
+])
 
 
 def gen_txn_author_agreement(did: str, version: str, text: str):
@@ -29,3 +43,16 @@ def get_config_req_handler(node):
     config_req_handler = node.get_req_handler(CONFIG_LEDGER_ID)
     assert isinstance(config_req_handler, ConfigReqHandler)
     return config_req_handler
+
+
+def expected_state_data(data: TaaData) -> Dict:
+    return {
+        TXN_PAYLOAD: {
+            TXN_AUTHOR_AGREEMENT_VERSION: data.version,
+            TXN_AUTHOR_AGREEMENT_TEXT: data.text
+        },
+        TXN_METADATA: {
+            TXN_METADATA_SEQ_NO: data.seqNo,
+            TXN_METADATA_TIME: data.txnTime
+        }
+    }
