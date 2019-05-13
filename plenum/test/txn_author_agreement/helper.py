afffbb49e8b24fda5c5ea2212402adc00e361192
ArtObr <artemobruchnikov@gmail.com>
2019-05-13 13:30:54 +0300

    INDY-2066: Fix tests to use libindy
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>

--- plenum/test/txn_author_agreement/helper.py
+++ plenum/test/txn_author_agreement/helper.py
@@ -1,28 +1,14 @@
-from random import randint
+from indy.ledger import build_txn_author_agreement_request
 
-from plenum.common.constants import TXN_TYPE, CURRENT_PROTOCOL_VERSION, TXN_AUTHOR_AGREEMENT, CONFIG_LEDGER_ID
-from plenum.common.types import OPERATION, f
+from plenum.common.constants import CONFIG_LEDGER_ID
 from plenum.common.util import randomString
 from plenum.server.config_req_handler import ConfigReqHandler
 
 
-def gen_txn_author_agreement(did: str, version: str, text: str):
-    return {
-        OPERATION: {
-            TXN_TYPE: TXN_AUTHOR_AGREEMENT,
-            'text': text,
-            'version': version
-        },
-        f.IDENTIFIER.nm: did,
-        f.REQ_ID.nm: randint(1, 2147483647),
-        f.PROTOCOL_VERSION.nm: CURRENT_PROTOCOL_VERSION
-    }
-
-
-def gen_random_txn_author_agreement(did: str):
+async def prepare_txn_author_agreement(did: str):
     text = randomString(1024)
     version = randomString(16)
-    return gen_txn_author_agreement(did, version, text)
+    return await build_txn_author_agreement_request(did, text, version)
 
 
 def get_config_req_handler(node):
--- plenum/test/txn_author_agreement/test_txn_author_agreement.py
+++ plenum/test/txn_author_agreement/test_txn_author_agreement.py
@@ -9,14 +9,16 @@ from plenum.server.config_req_handler import ConfigReqHandler
 from plenum.test.helper import sdk_get_and_check_replies
 from plenum.test.node_catchup.helper import ensure_all_nodes_have_same_data
 from plenum.test.pool_transactions.helper import sdk_sign_and_send_prepared_request
-from plenum.test.txn_author_agreement.helper import gen_random_txn_author_agreement, get_config_req_handler
+from plenum.test.txn_author_agreement.helper import get_config_req_handler, \
+    prepare_txn_author_agreement
 
 
 def test_send_valid_txn_athr_agrmt_succeeds(looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_trustee):
-    req = gen_random_txn_author_agreement(sdk_wallet_trustee[1])
-    rep = sdk_sign_and_send_prepared_request(looper, sdk_wallet_trustee, sdk_pool_handle, json.dumps(req))
+    req = looper.loop.run_until_complete(prepare_txn_author_agreement(sdk_wallet_trustee[1]))
+    rep = sdk_sign_and_send_prepared_request(looper, sdk_wallet_trustee, sdk_pool_handle, req)
     sdk_get_and_check_replies(looper, [rep])
 
+    req = json.loads(req)
     version = req[OPERATION][TXN_AUTHOR_AGREEMENT_VERSION]
     text = req[OPERATION][TXN_AUTHOR_AGREEMENT_TEXT]
     digest = ConfigReqHandler._taa_digest(version, text)
@@ -37,29 +39,37 @@ def test_send_valid_txn_athr_agrmt_succeeds(looper, txnPoolNodeSet, sdk_pool_han
 
 
 def test_send_invalid_txn_author_agreement_fails(looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_trustee):
-    req = gen_random_txn_author_agreement(sdk_wallet_trustee[1])
+    req = looper.loop.run_until_complete(prepare_txn_author_agreement(sdk_wallet_trustee[1]))
+    req = json.loads(req)
     req[OPERATION]['text'] = 42
     rep = sdk_sign_and_send_prepared_request(looper, sdk_wallet_trustee, sdk_pool_handle, json.dumps(req))
     with pytest.raises(RequestNackedException):
         sdk_get_and_check_replies(looper, [rep])
 
 
-def test_send_valid_txn_author_agreement_without_enough_privileges_fails(looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_steward):
-    req = gen_random_txn_author_agreement(sdk_wallet_steward[1])
-    rep = sdk_sign_and_send_prepared_request(looper, sdk_wallet_steward, sdk_pool_handle, json.dumps(req))
+def test_send_valid_txn_author_agreement_without_enough_privileges_fails(looper, txnPoolNodeSet, sdk_pool_handle,
+                                                                         sdk_wallet_steward):
+    req = looper.loop.run_until_complete(prepare_txn_author_agreement(sdk_wallet_steward[1]))
+
+    rep = sdk_sign_and_send_prepared_request(looper, sdk_wallet_steward, sdk_pool_handle, req)
     with pytest.raises(RequestRejectedException):
         sdk_get_and_check_replies(looper, [rep])
 
 
-def test_send_different_txn_author_agreement_with_same_version_fails(looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_trustee):
+def test_send_different_txn_author_agreement_with_same_version_fails(looper, txnPoolNodeSet, sdk_pool_handle,
+                                                                     sdk_wallet_trustee):
     # Send original txn
-    req = gen_random_txn_author_agreement(sdk_wallet_trustee[1])
+    req = looper.loop.run_until_complete(prepare_txn_author_agreement(sdk_wallet_trustee[1]))
+
+    req = json.loads(req)
     old_version = req[OPERATION][TXN_AUTHOR_AGREEMENT_VERSION]
     rep = sdk_sign_and_send_prepared_request(looper, sdk_wallet_trustee, sdk_pool_handle, json.dumps(req))
     sdk_get_and_check_replies(looper, [rep])
 
     # Send new txn with old version
-    req = gen_random_txn_author_agreement(sdk_wallet_trustee[1])
+    req = looper.loop.run_until_complete(prepare_txn_author_agreement(sdk_wallet_trustee[1]))
+    req = json.loads(req)
+
     req[OPERATION][TXN_AUTHOR_AGREEMENT_VERSION] = old_version
     rep = sdk_sign_and_send_prepared_request(looper, sdk_wallet_trustee, sdk_pool_handle, json.dumps(req))
     with pytest.raises(RequestRejectedException):
