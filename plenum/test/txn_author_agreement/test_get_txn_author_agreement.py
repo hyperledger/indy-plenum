2c6d67bb5c9c2c16c4381e342709377b3cd40c38
Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>
2019-05-16 16:17:47 +0300

    INDY-2067: Improve GET_TAA tests
    Signed-off-by: Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>

--- plenum/test/txn_author_agreement/test_get_txn_author_agreement.py
+++ plenum/test/txn_author_agreement/test_get_txn_author_agreement.py
@@ -150,7 +150,8 @@ def test_get_txn_author_agreement_doesnt_return_taa_for_nonexistent_digest(loope
     check_state_proof(result, '2:d:{}'.format(invalid_digest), None)
 
 
-def test_get_txn_author_agreement_can_return_taa_for_old_ts(looper, nodeSetWithTaa,
+# TODO: Change to nodeSetWithTaa when SDK will support state proofs with user-defined timestamps
+def test_get_txn_author_agreement_can_return_taa_for_old_ts(looper, nodeSetWithTaaAlwaysResponding,
                                                             sdk_pool_handle, sdk_wallet_client):
     reply = sdk_get_txn_author_agreement(looper, sdk_pool_handle, sdk_wallet_client,
                                          timestamp=TIMESTAMP_V2 - 2)[1]
@@ -162,7 +163,8 @@ def test_get_txn_author_agreement_can_return_taa_for_old_ts(looper, nodeSetWithT
     check_state_proof(result, '2:latest', DIGEST_V1)
 
 
-def test_get_txn_author_agreement_can_return_taa_for_fresh_ts(looper, nodeSetWithTaa,
+# TODO: Change to nodeSetWithTaa when SDK will support state proofs with user-defined timestamps
+def test_get_txn_author_agreement_can_return_taa_for_fresh_ts(looper, nodeSetWithTaaAlwaysResponding,
                                                               sdk_pool_handle, sdk_wallet_client):
     reply = sdk_get_txn_author_agreement(looper, sdk_pool_handle, sdk_wallet_client,
                                          timestamp=TIMESTAMP_V2 + 2)[1]
@@ -174,7 +176,7 @@ def test_get_txn_author_agreement_can_return_taa_for_fresh_ts(looper, nodeSetWit
     check_state_proof(result, '2:latest', DIGEST_V2)
 
 
-# TODO: For some reason SDK doesn't accept state proof in this case
+# TODO: Change to nodeSetWithTaa when SDK will support state proofs with user-defined timestamps
 def test_get_txn_author_agreement_doesnt_return_taa_when_it_didnt_exist(looper, nodeSetWithTaaAlwaysResponding,
                                                                         sdk_pool_handle, sdk_wallet_client):
     reply = sdk_get_txn_author_agreement(looper, sdk_pool_handle, sdk_wallet_client,
