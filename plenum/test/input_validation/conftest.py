828fb8a50d2e65bff718e600906ee0ce99f63f25
Andrey Kononykhin <andkononykhin@gmail.com>
2019-05-16 20:15:17 +0300

    fixes imports
    Signed-off-by: Andrey Kononykhin <andkononykhin@gmail.com>

--- plenum/test/input_validation/conftest.py
+++ plenum/test/input_validation/conftest.py
@@ -6,6 +6,10 @@ from plenum.common.util import get_utc_epoch
 from plenum.common.messages.fields import TimestampField
 from plenum.common.types import f
 
+# TODO external fixtures imports
+from plenum.test.txn_author_agreement.conftest import (
+    random_taa, get_txn_author_agreement, aml_request_kwargs
+)
 from plenum.test.txn_author_agreement.acceptance.conftest import (
     taa_acceptance, taa_digest, taa_acceptance_mechanism, taa_acceptance_time
 )
