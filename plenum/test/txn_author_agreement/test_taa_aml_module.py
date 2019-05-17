396af7bd01c1b6a9356f26ff094e51adab741f33
Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>
2019-05-17 13:53:34 +0300

    Fix failing test
    Signed-off-by: Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>

--- plenum/test/txn_author_agreement/test_taa_aml_module.py
+++ plenum/test/txn_author_agreement/test_taa_aml_module.py
@@ -8,7 +8,7 @@ def test_taa_acceptance_static_validation(config_req_handler, taa_aml_request):
     taa_aml_request.operation[AML] = {}
     with pytest.raises(InvalidClientRequest) as e:
         config_req_handler.doStaticValidation(taa_aml_request)
-    assert e.match('TAA AML request must contain at least one acceptance mechanism')
+    assert e.match('TXN_AUTHOR_AGREEMENT_AML request must contain at least one acceptance mechanism')
 
 
 def test_taa_acceptance_dynamic_validation(config_req_handler, taa_aml_request):
