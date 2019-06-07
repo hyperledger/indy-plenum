570e18ef912bcf6c4794f30549eb14049e37ff16
ArtObr <artemobruchnikov@gmail.com>
2019-06-07 16:18:09 +0300

    INDY-2108: Batch hanlder
    Signed-off-by: ArtObr <artemobruchnikov@gmail.com>

---
+++ plenum/test/common/test_batch_handler.py
@@ -0,0 +1,47 @@
+import pytest
+import time
+
+from storage.kv_store_rocksdb import KeyValueStorageRocksdb
+from state.pruning_state import PruningState
+from ledger.compact_merkle_tree import CompactMerkleTree
+from plenum.common.ledger import Ledger
+from plenum.server.batch_handlers.batch_request_handler import BatchRequestHandler
+from plenum.server.batch_handlers.three_pc_batch import ThreePcBatch
+
+from plenum.test.common.test_database_manager import database_manager as db
+from state.test.conftest import tempdir
+
+LEDGER_ID = 1
+FIXED_HASH = 'CMEcrTyug8SYVkLtR8qgjqx7NVEQ4xXuAvaJDDPhDu8n'
+
+
+@pytest.fixture(scope='function')
+def database_manager(db, tdir_for_func, tempdir):
+    db.register_new_database(LEDGER_ID, Ledger(CompactMerkleTree(), dataDir=tdir_for_func), PruningState(
+        KeyValueStorageRocksdb(tempdir, 'kv1')))
+    return db
+
+
+@pytest.fixture(scope='function')
+def batch_handler(database_manager):
+    return BatchRequestHandler(database_manager, LEDGER_ID)
+
+
+@pytest.fixture(scope='function')
+def three_pc_batch(batch_handler):
+    # Constant root hash is one which will be formed after applying txn
+    return ThreePcBatch(LEDGER_ID, 0, 0, 1, time.time(),
+                        batch_handler.state.headHash,
+                        FIXED_HASH,
+                        ['a', 'b', 'c'], ['d1', 'd2', 'd3'])
+
+
+def test_batch_handler_commit(batch_handler: BatchRequestHandler, three_pc_batch):
+    assert len(batch_handler.ledger) == 0
+
+    batch_handler.ledger.appendTxns([{'txnMetadata': {'seqNo': 1}}])
+    batch_handler.commit_batch(three_pc_batch)
+
+    assert len(batch_handler.ledger) == 1
+
+    batch_handler._check_consistency_after_commit(FIXED_HASH)
