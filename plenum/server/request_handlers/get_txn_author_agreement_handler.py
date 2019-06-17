3234b2f4d909283568f0e8fdc8100b68a2246c84
Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
2019-06-17 12:40:29 +0300

    [INDY-1861] remove unused imports
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>

--- plenum/server/request_handlers/get_txn_author_agreement_handler.py
+++ plenum/server/request_handlers/get_txn_author_agreement_handler.py
@@ -1,21 +1,13 @@
-from _sha256 import sha256
-from functools import lru_cache
-
-from common.serializers.serialization import pool_state_serializer, config_state_serializer
-from plenum.common.constants import POOL_LEDGER_ID, NODE, DATA, BLS_KEY, \
-    BLS_KEY_PROOF, TARGET_NYM, DOMAIN_LEDGER_ID, NODE_IP, \
-    NODE_PORT, CLIENT_IP, CLIENT_PORT, ALIAS, TXN_AUTHOR_AGREEMENT, CONFIG_LEDGER_ID, GET_TXN_AUTHOR_AGREEMENT_VERSION, \
-    GET_TXN_AUTHOR_AGREEMENT_DIGEST, GET_TXN_AUTHOR_AGREEMENT_TIMESTAMP, TXN_AUTHOR_AGREEMENT_VERSION, \
-    TXN_AUTHOR_AGREEMENT_TEXT, GET_TXN_AUTHOR_AGREEMENT, TXN_TIME
-from plenum.common.exceptions import InvalidClientRequest, UnauthorizedClientRequest
+from common.serializers.serialization import config_state_serializer
+from plenum.common.constants import CONFIG_LEDGER_ID, GET_TXN_AUTHOR_AGREEMENT_VERSION, \
+    GET_TXN_AUTHOR_AGREEMENT_DIGEST, GET_TXN_AUTHOR_AGREEMENT_TIMESTAMP, \
+    GET_TXN_AUTHOR_AGREEMENT
+from plenum.common.exceptions import InvalidClientRequest
 from plenum.common.request import Request
-from plenum.common.txn_util import get_payload_data, get_from, get_seq_no, get_txn_time
-from plenum.common.types import f
 from plenum.server.database_manager import DatabaseManager
 from plenum.server.request_handlers.handler_interfaces.read_request_handler import ReadRequestHandler
-from plenum.server.request_handlers.handler_interfaces.write_request_handler import WriteRequestHandler
 from plenum.server.request_handlers.static_taa_helper import StaticTAAHelper
-from plenum.server.request_handlers.utils import is_steward, encode_state_value, decode_state_value
+from plenum.server.request_handlers.utils import decode_state_value
 
 
 class GetTxnAuthorAgreementHandler(ReadRequestHandler):
