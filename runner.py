82567f4395eb35a7136d5321da8e4fd0242fa9d4
Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
2019-07-18 18:34:44 +0300

    [INDY-2136] get back of runner.py
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>

--- runner.py
+++ runner.py
@@ -21,13 +21,10 @@ def run(pytest, output_file, repeatUntilFailure, testDir, test_slice):
         log("Is going to repeat the test suite until failure")
     log("Preparing test suite with {}".format(pytest))
     testListFile = "test_list.txt"
-    # collect_status = os.system('{} --collect-only {} > {}'.format(pytest, testDir, testListFile))
-    result = subprocess.Popen('{} --collect-only {} > {}'.format(pytest, testDir, testListFile),
-                              stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE, shell=True)
-    if result.returncode != 0:
-        log("STDERR: Test suit preparation error {}".format(result.stderr.read()))
-        log("STDOUT: Test suit preparation error {}".format(result.stdout.read()))
+    collect_status = os.system('{} --collect-only {} > {}'.format(pytest, testDir, testListFile))
+
+    if collect_status != 0:
+        log("Test suit preparation error {}".format(collect_status))
         return -1
 
     log("Reading collected modules file")
