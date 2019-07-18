019d8d58269587496c42d8fc908fb098d78e00c3
toktar <renata.toktar@dsr-corporation.com>
2019-07-18 15:53:52 +0300

    INDY-2112: add test_catchup_with_reask_cp
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- plenum/test/node_catchup/test_catchup_reasking.py
+++ plenum/test/node_catchup/test_catchup_reasking.py
@@ -1,25 +1,12 @@
 import pytest
 
-from plenum.common.config_helper import PNodeConfigHelper
-from plenum.common.constants import AUDIT_LEDGER_ID, LEDGER_STATUS, COMMIT, CONSISTENCY_PROOF
-from plenum.common.messages.node_messages import LedgerStatus, MessageRep, ConsistencyProof
-from plenum.server.catchup.node_leecher_service import NodeLeecherService
-from plenum.test.delayers import delay_3pc, lsDelay, msg_rep_delay, msg_req_delay, cpDelay
-from plenum.test.helper import sdk_send_random_and_check, max_3pc_batch_limits, assertExp, MockTimer
-from plenum.test.node_catchup.helper import ensure_all_nodes_have_same_data, waitNodeDataEquality
-from plenum.test.pool_transactions.helper import disconnect_node_and_ensure_disconnected
-from plenum.test.stasher import delay_rules_without_processing, delay_rules
-from plenum.test.test_node import checkNodesConnected, TestNode
+from plenum.common.constants import LEDGER_STATUS, COMMIT
+from plenum.common.messages.node_messages import MessageRep, ConsistencyProof
+from plenum.test.delayers import delay_3pc, lsDelay, msg_rep_delay, cpDelay
+from plenum.test.helper import sdk_send_random_and_check
+from plenum.test.node_catchup.helper import waitNodeDataEquality
+from plenum.test.stasher import delay_rules_without_processing
 from stp_core.loop.eventually import eventually
-
-#
-#
-# @pytest.fixture(scope="module")
-# def txnPoolNodeSet(txnPoolNodeSet):
-#     for node in txnPoolNodeSet:
-#         node.timer = MockTimer(int(node.timer.get_current_time()))
-#     return txnPoolNodeSet
-from stp_core.types import HA
 
 
 @pytest.fixture(scope="module")
@@ -38,37 +25,74 @@ def test_catchup_with_reask_ls(txnPoolNodeSet,
                                sdk_wallet_steward,
                                tconf,
                                tdir,
-                               allPluginsPath,
-                               monkeypatch):
+                               allPluginsPath):
     '''
     Start a catchup
     Delay MessageqResp with LedgerStatuses twice
     Check that the catchup finished
     '''
-    node_to_disconnect = txnPoolNodeSet[-1]
+    lagged_node = txnPoolNodeSet[-1]
     sdk_send_random_and_check(looper, txnPoolNodeSet,
                               sdk_pool_handle, sdk_wallet_steward, 5)
-    node_to_disconnect.nodeIbStasher.delay(msg_rep_delay(types_to_delay=[COMMIT]))
+    lagged_node.nodeIbStasher.delay(msg_rep_delay(types_to_delay=[COMMIT]))
 
-    with delay_rules_without_processing(node_to_disconnect.nodeIbStasher, delay_3pc(),
+    with delay_rules_without_processing(lagged_node.nodeIbStasher, delay_3pc(),
                                         msg_rep_delay(types_to_delay=[COMMIT])):
         sdk_send_random_and_check(looper, txnPoolNodeSet,
                                   sdk_pool_handle, sdk_wallet_steward,
                                   2)
-        node_to_disconnect.nodeIbStasher.drop_delayeds()
-    with delay_rules_without_processing(node_to_disconnect.nodeIbStasher,
+        lagged_node.nodeIbStasher.drop_delayeds()
+    with delay_rules_without_processing(lagged_node.nodeIbStasher,
                                         lsDelay(),
                                         msg_rep_delay(types_to_delay=[LEDGER_STATUS])):
-        node_to_disconnect.start_catchup()
+        lagged_node.start_catchup()
 
         def chk():
             resp_ls_count = 0
-            for msg in node_to_disconnect.nodeIbStasher.delayeds:
+            for msg in lagged_node.nodeIbStasher.delayeds:
                 if isinstance(msg.item[0], MessageRep) and msg.item[0].msg_type == LEDGER_STATUS:
                     resp_ls_count += 1
             assert resp_ls_count >= (len(txnPoolNodeSet) - 1) * 2
-            node_to_disconnect.nodeIbStasher.drop_delayeds()
+            lagged_node.nodeIbStasher.drop_delayeds()
 
         looper.run(eventually(chk))
-    waitNodeDataEquality(looper, node_to_disconnect, *txnPoolNodeSet,
+    waitNodeDataEquality(looper, lagged_node, *txnPoolNodeSet,
+                         exclude_from_check=['check_last_ordered_3pc_backup'])
+
+
+def test_catchup_with_reask_cp(txnPoolNodeSet,
+                               looper,
+                               sdk_pool_handle,
+                               sdk_wallet_steward,
+                               tconf,
+                               tdir,
+                               allPluginsPath):
+    '''
+    Start a catchup
+    Delay ConsistencyProofs twice
+    Check that the catchup finished
+    '''
+    lagged_node = txnPoolNodeSet[-1]
+    sdk_send_random_and_check(looper, txnPoolNodeSet,
+                              sdk_pool_handle, sdk_wallet_steward, 5)
+    with delay_rules_without_processing(lagged_node.nodeIbStasher, delay_3pc(),
+                                        msg_rep_delay(types_to_delay=[COMMIT])):
+        sdk_send_random_and_check(looper, txnPoolNodeSet,
+                                  sdk_pool_handle, sdk_wallet_steward,
+                                  2)
+        lagged_node.nodeIbStasher.drop_delayeds()
+
+    with delay_rules_without_processing(lagged_node.nodeIbStasher, cpDelay()):
+        lagged_node.start_catchup()
+
+        def chk():
+            cp_count = 0
+            for msg in lagged_node.nodeIbStasher.delayeds:
+                if isinstance(msg.item[0], ConsistencyProof):
+                    cp_count += 1
+            assert cp_count >= (len(txnPoolNodeSet) - 1) * 2
+            lagged_node.nodeIbStasher.drop_delayeds()
+
+        looper.run(eventually(chk))
+    waitNodeDataEquality(looper, lagged_node, *txnPoolNodeSet,
                          exclude_from_check=['check_last_ordered_3pc_backup'])
