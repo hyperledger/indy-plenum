bf62d71623f80350155c648ebbb69f48d34d80e5
toktar <renata.toktar@dsr-corporation.com>
2019-07-31 11:16:46 +0300

    INDY-2137: move test_consensus_dp_batches.py
    Signed-off-by: toktar <renata.toktar@dsr-corporation.com>

--- plenum/test/replica/test_consensus_dp_batches.py
+++ plenum/test/replica/test_consensus_dp_batches.py
@@ -1,12 +1,10 @@
-from plenum.test.pool_transactions.helper import sdk_add_new_nym_without_waiting
+from plenum.test.helper import sdk_send_random_request
 from stp_core.loop.eventually import eventually
-
 from plenum.test.delayers import ppDelay, pDelay
-
 from plenum.test.stasher import delay_rules
 
 
-def test_check_cdp_pp_storages(looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_steward):
+def test_check_cdp_pp_storages(looper, txnPoolNodeSet, sdk_pool_handle, sdk_wallet_client):
     def check_all_empty(replica):
         assert not bool(replica._consensus_data.preprepared)
         assert not bool(replica._consensus_data.prepared)
@@ -25,7 +23,7 @@ def test_check_cdp_pp_storages(looper, txnPoolNodeSet, sdk_pool_handle, sdk_wall
 
     with delay_rules(node_stashers, pDelay()):
         with delay_rules(node_stashers, ppDelay()):
-            sdk_add_new_nym_without_waiting(looper, sdk_pool_handle, sdk_wallet_steward)
+            sdk_send_random_request(looper, sdk_pool_handle, sdk_wallet_client)
             looper.run(eventually(operation_for_replicas, check_all_empty, txnPoolNodeSet[1:]))
             looper.run(eventually(operation_for_replicas, check_preprepared_not_empty, txnPoolNodeSet[0:1]))
         looper.run(eventually(operation_for_replicas, check_preprepared_not_empty, txnPoolNodeSet))
