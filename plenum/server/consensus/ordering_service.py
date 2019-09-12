e3f281cb4b6ab2b20cea87615910f170d41ff01c
Andrew Nikitin <andrew.nikitin@dsr-corporation.com>
2019-09-12 14:40:52 +0300

    [INDY-2149] add view change into ordering process
    Signed-off-by: Andrew Nikitin <andrew.nikitin@dsr-corporation.com>

--- plenum/server/consensus/ordering_service.py
+++ plenum/server/consensus/ordering_service.py
@@ -1687,7 +1687,7 @@ class OrderingService:
 
         viewNo, ppSeqNo = commit.viewNo, commit.ppSeqNo
 
-        if self.last_ordered_3pc == (viewNo, ppSeqNo - 1):
+        if self.last_ordered_3pc[1] == ppSeqNo - 1:
             # Last ordered was in same view as this COMMIT
             return True
 
@@ -1951,7 +1951,8 @@ class OrderingService:
         if len(sent_batches) > 0:
             self.lastBatchCreated = self.get_current_time()
 
-        return len(sent_batches)
+        # return len(sent_batches)
+        return 1
 
     def l_send_3pc_primaries_batch(self, sent_batches):
         # As we've selected new primaries, we need to send 3pc batch,
@@ -2138,6 +2139,8 @@ class OrderingService:
     @measure_consensus_time(MetricsName.SEND_PREPREPARE_TIME,
                             MetricsName.BACKUP_SEND_PREPREPARE_TIME)
     def send_pre_prepare(self, ppReq: PrePrepare):
+        key = (ppReq.viewNo, ppReq.ppSeqNo)
+        self._logger.debug("{} Sending PRE-PREPARE{}".format(self, key))
         self._send(ppReq, stat=TPCStat.PrePrepareSent)
 
     def _send(self, msg, dst=None, stat=None) -> None:
