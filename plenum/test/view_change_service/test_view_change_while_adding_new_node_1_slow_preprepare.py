2325b05d609159746fe724af7227351a80286c90
ashcherbakov <alexander.sherbakov@dsr-corporation.com>
2019-10-31 16:35:44 +0300

    INDY-2262: integration tests to reproduce the issue
    Signed-off-by: ashcherbakov <alexander.sherbakov@dsr-corporation.com>

---
+++ plenum/test/view_change_service/test_view_change_while_adding_new_node_1_slow_preprepare.py
@@ -0,0 +1,38 @@
+import pytest
+
+from plenum.test.delayers import cDelay
+from plenum.test.helper import checkViewNoForNodes, sdk_send_random_and_check, waitForViewChange
+from plenum.test.node_catchup.helper import ensure_all_nodes_have_same_data
+from plenum.test.node_request.helper import sdk_ensure_pool_functional
+from plenum.test.pool_transactions.helper import disconnect_node_and_ensure_disconnected
+from plenum.test.stasher import delay_rules_without_processing
+from plenum.test.test_node import getNonPrimaryReplicas, ensureElectionsDone
+from plenum.test.view_change.helper import add_new_node
+from plenum.test.view_change_service.helper import get_next_primary_name, trigger_view_change, \
+    check_view_change_adding_new_node
+
+
+@pytest.fixture(scope="module", params=['twice', 'once'])
+def vc_counts(request):
+    return request.param
+
+
+@pytest.fixture(scope="module", params=[True, False])
+def slow_node_is_next_primary(request):
+    return request.param
+
+
+def test_view_change_while_adding_new_node_1_slow_preprepare(looper, tdir, tconf, allPluginsPath,
+                                                         txnPoolNodeSet,
+                                                         sdk_pool_handle,
+                                                         sdk_wallet_client,
+                                                         sdk_wallet_steward):
+    check_view_change_adding_new_node(looper, tdir, tconf, allPluginsPath,
+                                      txnPoolNodeSet,
+                                      sdk_pool_handle,
+                                      sdk_wallet_client,
+                                      sdk_wallet_steward,
+                                      slow_nodes=[txnPoolNodeSet[1]],
+                                      delay_pre_prepare=True,
+                                      delay_commit=False,
+                                      expected_viewno=4)
