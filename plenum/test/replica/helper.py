8327041e761f204c20c0fb036fffed2c5c6b1c36
Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>
2019-11-05 16:21:31 +0300

    INDY-2267: Fix tests
    Signed-off-by: Sergey Khoroshavin <sergey.khoroshavin@dsr-corporation.com>

--- plenum/test/replica/helper.py
+++ plenum/test/replica/helper.py
@@ -1,6 +1,9 @@
+from plenum.common.messages.internal_messages import CatchupFinished
+
+
 def emulate_catchup(replica, ppSeqNo=100):
-    replica.on_catch_up_finished(last_caught_up_3PC=(replica.viewNo, ppSeqNo),
-                                 master_last_ordered_3PC=replica.last_ordered_3pc)
+    replica.internal_bus.send(CatchupFinished((replica.viewNo, ppSeqNo),
+                                              replica.last_ordered_3pc))
 
 
 def emulate_select_primaries(replica):
