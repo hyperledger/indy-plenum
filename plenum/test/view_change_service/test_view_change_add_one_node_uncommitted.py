025803cc63a379264ae5092a408c2c4b7310f49f
ashcherbakov <alexander.sherbakov@dsr-corporation.com>
2019-12-26 14:58:08 +0300

    INDY-2318: integration test cleanup
    Signed-off-by: ashcherbakov <alexander.sherbakov@dsr-corporation.com>

--- plenum/test/view_change_service/test_view_change_add_one_node_uncommitted.py
+++ plenum/test/view_change_service/test_view_change_add_one_node_uncommitted.py
@@ -27,27 +27,27 @@ def check_node_txn_propagated(nodes):
         assert len(master_ordering_service.requestQueues.get(POOL_LEDGER_ID, set())) > 0
 
 
-def test_view_change_add_one_node_uncommitted(looper, tdir, tconf, allPluginsPath,
-                                              txnPoolNodeSet,
-                                              sdk_pool_handle,
-                                              sdk_wallet_client,
-                                              sdk_wallet_steward):
-    # Pre-requisites: viewNo=2, Primary is Node3
+def test_view_change_add_one_node_uncommitted_by_next_primary(looper, tdir, tconf, allPluginsPath,
+                                                              txnPoolNodeSet,
+                                                              sdk_pool_handle,
+                                                              sdk_wallet_client,
+                                                              sdk_wallet_steward):
+    # 1. Pre-requisites: viewNo=2, Primary is Node3
     for viewNo in range(1, 3):
         trigger_view_change(txnPoolNodeSet)
         waitForViewChange(looper, txnPoolNodeSet, viewNo)
         ensureElectionsDone(looper, txnPoolNodeSet, customTimeout=30)
 
-    # Add Steward for new Node
+    # 2. Add Steward for new Node
     new_steward_wallet_handle = sdk_add_new_nym(looper,
                                                 sdk_pool_handle,
                                                 sdk_wallet_steward,
                                                 alias="testClientSteward" + randomString(3),
                                                 role=STEWARD_STRING)
 
-    # Send txn to add Node5
+    # 3. Send txn to add Node5
     # It will not be proposed and ordered by the current Primary, but will be proposed by the next one in the new view
-    # Make sure that the request is propagated by all nodes
+    # Make sure that the request is propagated by the next Primary
     old_state_root_hash = txnPoolNodeSet[0].stateRootHash(ledgerId=POOL_LEDGER_ID, isCommitted=False)
     primary_node = getPrimaryReplica(txnPoolNodeSet).node
     next_primary = txnPoolNodeSet[-1]
@@ -68,6 +68,7 @@ def test_view_change_add_one_node_uncommitted(looper, tdir, tconf, allPluginsPat
         looper.run(eventually(check_node_txn_propagated, [next_primary]))
         check_node_txn_not_applied(txnPoolNodeSet, old_state_root_hash)
 
+    # 4. Trigger view change to view
     # Make sure that only the next Primary (Node4) finishes View Change to view=3
     slow_nodes = txnPoolNodeSet[:3]
     fast_nodes = [next_primary]
@@ -85,7 +86,7 @@ def test_view_change_add_one_node_uncommitted(looper, tdir, tconf, allPluginsPat
         looper.run(eventually(check_node_txn_applied, fast_nodes, old_state_root_hash))
         check_node_txn_not_applied(slow_nodes, old_state_root_hash)
 
-    # Trigger view change to view=4, and make sure it's finished properly
+    # 5. Trigger view change to view=4, and make sure it's finished properly
     trigger_view_change(txnPoolNodeSet)
     waitForViewChange(looper, txnPoolNodeSet, 4)
     ensureElectionsDone(looper, txnPoolNodeSet, customTimeout=35)
