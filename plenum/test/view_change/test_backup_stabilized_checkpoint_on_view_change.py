c4990fe30a2fc2bda530a053233a6e70fe6f197c
ashcherbakov <alexander.sherbakov@dsr-corporation.com>
2020-01-15 21:02:49 +0300

    INDY-2322: fix more tests
    Signed-off-by: ashcherbakov <alexander.sherbakov@dsr-corporation.com>

--- plenum/test/view_change/test_backup_stabilized_checkpoint_on_view_change.py
+++ plenum/test/view_change/test_backup_stabilized_checkpoint_on_view_change.py
@@ -2,7 +2,7 @@ import pytest
 
 from plenum.test.delayers import ppDelay
 from plenum.test.helper import sdk_send_random_requests, assertExp
-from plenum.test.stasher import delay_rules_without_processing
+from plenum.test.stasher import delay_rules
 from plenum.test.test_node import ensureElectionsDone
 from plenum.test.view_change.helper import ensure_view_change
 from stp_core.loop.eventually import eventually
@@ -26,7 +26,7 @@ def test_backup_stabilized_checkpoint_on_view_change(looper,
     # Delta:1
     backup = txnPoolNodeSet[-1].replicas[1]
     count_of_replicas = len(txnPoolNodeSet[0].replicas)
-    with delay_rules_without_processing([n.nodeIbStasher for n in txnPoolNodeSet], ppDelay(instId=0)):
+    with delay_rules([n.nodeIbStasher for n in txnPoolNodeSet], ppDelay(instId=0)):
         sdk_send_random_requests(looper, sdk_pool_handle, sdk_wallet_client, REQ_COUNT)
         looper.run(eventually(lambda r: assertExp(r.last_ordered_3pc == (0, REQ_COUNT)), backup))
         # assert that all of requests are propagated
@@ -35,9 +35,9 @@ def test_backup_stabilized_checkpoint_on_view_change(looper,
                 assert req.forwardedTo == count_of_replicas
 
         ensure_view_change(looper, txnPoolNodeSet)
-        ensureElectionsDone(looper, txnPoolNodeSet)
+    ensureElectionsDone(looper, txnPoolNodeSet)
 
-        # check, that all requests was freed on backups
-        for n in txnPoolNodeSet:
-            for req in n.requests.values():
-                assert req.forwardedTo == count_of_replicas - 1
+    # check, that all requests have been freed on backups
+    for n in txnPoolNodeSet:
+        for req in n.requests.values():
+            assert req.forwardedTo == count_of_replicas - 1
