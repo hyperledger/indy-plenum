03356aab2f1669cf4709598dad82d69e8b3655ca
Ian Costanzo <ian@anon-solutions.ca>
2021-03-02 09:02:24 -0800

    Fix cd command
    Signed-off-by: Ian Costanzo <ian@anon-solutions.ca>

--- build-scripts/ubuntu-1604/build-3rd-parties.sh
+++ build-scripts/ubuntu-1604/build-3rd-parties.sh
@@ -80,7 +80,7 @@ function build_from_pypi {
 # TODO duplicates list from Jenkinsfile.cd
 
 SCRIPT_PATH="${BASH_SOURCE[0]}"
-pushd ${SCRIPT_PATH} >/dev/null
+pushd `dirname ${SCRIPT_PATH}` >/dev/null
 
 # Build rocksdb at first
 build_rocksdb_deb 5.8.8
