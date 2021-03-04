f1aeca8483b36f25092b491ac0873e61d37544df
Anton Denishchenko <anton.denishchenko@evernym.com>
2021-03-04 15:38:55 +0300

    FIx issue with logging debug
    Signed-off-by: Anton Denishchenko <anton.denishchenko@evernym.com>

--- plenum/test/client/test_send_from_wrong_socket.py
+++ plenum/test/client/test_send_from_wrong_socket.py
@@ -3,7 +3,9 @@ import zmq
 
 from plenum.test.client.helper import create_zmq_connection
 from plenum.test.logging.conftest import logsearch
+from stp_core.common.log import Logger
 from stp_core.loop.eventually import eventually
+import logging
 
 
 @pytest.fixture(params=[zmq.REQ])
@@ -24,6 +26,8 @@ def zmq_connection(test_node, request, looper):
 
 # 2021-02-25 16:02:05,446|ERROR|looper.py|Error while running coroutine eventually: AssertionError('assert []')
 def test_send_using_not_dealer_socket(zmq_connection, test_node, looper, sdk_wallet_client, logsearch):
+    default_log_level = logging.root.level
+    Logger.setLogLevel(logging.DEBUG)
     logs, _ = logsearch(files=['zstack.py'], msgs=['Got too many values for unpack'])
 
     def check_reply():
@@ -33,3 +37,4 @@ def test_send_using_not_dealer_socket(zmq_connection, test_node, looper, sdk_wal
     msg = "{ \"op\": \"LEDGER_STATUS\", \"txnSeqNo\": 0, \"merkleRoot\": \"\", \"ledgerId\": 0, \"ppSeqNo\": null, \"viewNo\": null, \"protocolVersion\": 2}"
     zmq_connection.send_string(msg)
     looper.run(eventually(check_reply))
+    Logger.setLogLevel(default_log_level)
