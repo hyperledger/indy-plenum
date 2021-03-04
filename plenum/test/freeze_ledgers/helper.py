c967a0e012b10c76ae007cd3048b797c293152a7
toktar <renata.toktar@evernym.com>
2021-03-04 12:53:57 +0300

    Fix sdk_get_frozen_ledgers for tests
    Signed-off-by: toktar <renata.toktar@evernym.com>

--- plenum/test/freeze_ledgers/helper.py
+++ plenum/test/freeze_ledgers/helper.py
@@ -15,6 +15,5 @@ def sdk_send_freeze_ledgers(looper, sdk_pool_handle, sdk_wallets, ledgers_ids: [
 
 def sdk_get_frozen_ledgers(looper, sdk_pool_handle, sdk_wallet):
     req = looper.loop.run_until_complete(build_get_frozen_ledgers_request(sdk_wallet[1]))
-    rep = sdk_sign_and_submit_req_obj(looper, sdk_pool_handle, sdk_wallet,
-                                      [sdk_json_to_request_object(json.loads(req))])
+    rep = sdk_sign_and_submit_req_obj(looper, sdk_pool_handle, sdk_wallet, sdk_json_to_request_object(json.loads(req)))
     return sdk_get_and_check_replies(looper, [rep])[0]
