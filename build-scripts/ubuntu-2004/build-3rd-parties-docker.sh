f826c1b6bec6146ff300c372904d44c2bb427dc2
pSchlarb <p.schlarb@esatus.com>
2022-02-24 12:21:44 +0000

    Build-indy-plenum-docker switched to the GHA Dockerfile
    Signed-off-by: pSchlarb <p.schlarb@esatus.com>

--- build-scripts/ubuntu-2004/build-3rd-parties-docker.sh
+++ build-scripts/ubuntu-2004/build-3rd-parties-docker.sh
@@ -3,8 +3,11 @@
 set -x
 set -e
 
+
+PKG_SOURCE_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../../
+
 if [ -z "$2" ]; then
-    CMD="/root/build-3rd-parties.sh /output"
+    CMD="/input/build-scripts/ubuntu-2004/build-3rd-parties.sh /output"
 else
     CMD="$2"
 fi
@@ -13,12 +16,13 @@ PKG_NAME=indy-plenum
 IMAGE_NAME="${PKG_NAME}-build-u2004"
 OUTPUT_VOLUME_NAME="${1:-"${PKG_NAME}-deb-u2004"}"
 
-docker build -t "${PKG_NAME}-build-u2004" -f Dockerfile .
+docker build -t "${PKG_NAME}-build-u2004" -f $PKG_SOURCE_PATH/.github/workflows/build/Dockerfile.ubuntu-2004 .
 docker volume create --name "${OUTPUT_VOLUME_NAME}"
 
 docker run \
     -i \
     --rm \
+    -v "${PKG_SOURCE_PATH}:/input" \
     -v "${OUTPUT_VOLUME_NAME}:/output" \
     "${IMAGE_NAME}" \
     $CMD
